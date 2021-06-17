1a37ca76a5534302e4845c71c71bbedc2e32be1b
jdbocarsly <jdbocarsly@gmail.com>
2021-06-17 11:09:50 +0100

    refactor of editpage into a more flexible, modular editor

---
+++ webapp/src/views/SampleEditPage.vue
@@ -0,0 +1,26 @@
+<template>
+
+<EditorBase :sample_id="sample_id">
+   <SampleInformation :sample_id="sample_id" />
+</EditorBase>
+   
+</template>
+
+<script>
+
+import EditorBase from "@/components/EditorBase"
+import SampleInformation from "@/components/SampleInformation"
+
+export default {
+   data() {
+      return {
+         sample_id: this.$route.params.id,
+      }
+   },
+   components: {
+      EditorBase,
+      SampleInformation
+   }
+}
+
+</script>
