fa42abb493591e6cfa4ba9fd3641a6aad4a4d8b5
thetenthbox <balansarang@gmail.com>
2021-06-21 12:42:00 +0530

    Added data block for electrochemistry analysis
    - Can perform dQ/dV, dV/dQ and plotting
    - Can filter cycles in the front end and set smoothing parameters
    
    Co-authored-by: Matthew Evans <git@ml-evs.science>
    Co-authored-by: Josh Bocarsly <jdbocarsly@gmail.com>

--- pydatalab/pydatalab/bokeh_plots.py
+++ pydatalab/pydatalab/bokeh_plots.py
@@ -1,12 +1,14 @@
-import pandas as pd
-
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 from bokeh.events import DoubleTap
-from bokeh.io import curdoc
-from bokeh.layouts import column
-from bokeh.models.callbacks import CustomJS
-from bokeh.models.widgets import Select, TextInput
-from bokeh.plotting import ColumnDataSource, figure, show
-from bokeh.themes import Theme, built_in_themes
+from bokeh.layouts import column, gridplot
+from bokeh.models import CustomJS, HoverTool
+from bokeh.models.widgets import Select
+from bokeh.plotting import ColumnDataSource, figure
+from bokeh.themes import Theme
+from scipy.signal import find_peaks
+from bokeh.models import CrosshairTool
 
 FONTSIZE = "14pt"
 TYPEFACE = "Helvetica"  # "Lato"
@@ -51,35 +53,27 @@ style = {
 mytheme = Theme(json=style)
 
 
-def simple_bokeh_plot(xy_filename, x_label=None, y_label=None):
-
-    df = pd.read_csv(xy_filename, sep="\s+", names=["x_col", "y_col"])
-    source = ColumnDataSource(df)
-    # source = ColumnDataSource({'x_col': x, 'y_col': y})
-
-    kw = dict()
-    p = figure(sizing_mode="scale_width", aspect_ratio=1.5, tools=TOOLS, **kw)
-
-    p.xaxis.axis_label = x_label
-    p.yaxis.axis_label = y_label
-
-    # apply a theme. for some reason, this isn't carrying over
-    # to components() calls, so use components(theme=mytheme)
-    curdoc().theme = mytheme
-
-    p.circle("x_col", "y_col", source=source)
-    p.toolbar.logo = "grey"
-    p.js_on_event(DoubleTap, CustomJS(args=dict(p=p), code="p.reset.emit()"))
-    # show(p)
-    return p
-
-
 def selectable_axes_plot(df, x_options, y_options, x_default=None, y_default=None, **kwargs):
+    """
+    Creates bokeh layout with selectable axis.
+
+    Args:
+        df: Input df from echem_block.py
+        x_options: Selectable x-axis options
+        y_options: Selectable y-axis options
+        x_default: Default x-axis that is printed at start, None specified means first value in x_options list will be chosen
+        y_default: Default y-axis that is printed at start, None specified means first value in y_options list will be chosen
+
+    Returns:
+        Bokeh layout
+    """
     source = ColumnDataSource(df)
 
     if not x_default:
         x_default = x_options[0]
         y_default = y_options[0]
+
+    # colormapper = LinearColorMapper(palette="Plasma256",low=df['colour'].min(), high=df['colour'].max())
 
     code_x = """
       var column = cb_obj.value;
@@ -120,5 +114,349 @@ def selectable_axes_plot(df, x_options, y_options, x_default=None, y_default=Non
     return layout
 
 
-if __name__ == "__main__":
-    simple_bokeh_plot()
+def selectable_axes_plot_colours(
+    df, x_options, y_options, x_default="Capacity", y_default="Voltage", **kwargs
+):
+    """
+    Creates bokeh layout with selectable axis.
+
+    Args:
+        df: Input df from echem_block.py
+        x_options: Selectable x-axis options
+        y_options: Selectable y-axis options
+        x_default: Default x-axis that is printed at start, Default set to Capacity
+        y_default: Default y-axis that is printed at start, Default set to Voltage
+
+    Returns:
+        Bokeh layout
+    """
+    source = ColumnDataSource(df)
+
+    if not x_default:
+        x_default = x_options[0]
+        y_default = y_options[0]
+
+    p = figure(
+        sizing_mode="scale_width",
+        aspect_ratio=1.5,
+        x_axis_label=x_default,
+        y_axis_label=y_default,
+        tools=TOOLS,
+        **kwargs
+    )
+
+    # Need to group the df into groups of different half cycles and then plot a line through it. Plotting groups of full cycles means the point will
+    # jump from one end to the other when charge --> discharge
+
+    grouped_by_halfcycle = df.groupby("half cycle")
+
+    # We need to colour lines by full-cycles in a gradient
+    cmap = plt.get_cmap("plasma")
+    full_cycle_group = df["full cycle"].unique()
+    myList = sorted(full_cycle_group)
+    length = len(myList)
+    numberList = np.linspace(0, 1, length)
+    # Since we are plotting by half cycle, colour is assigned by half cycle, even though we need full cycle
+    # to get around this, we will assign each pair of half cycle for each full cycle the same cmap value
+    newList = []
+    for i in numberList:
+        newList.extend([i, i])
+
+    counter = 0
+    lines = []
+    for name, group in grouped_by_halfcycle:
+        group = group.copy()
+        val = newList[counter]
+        line = p.line(
+            x=x_default,
+            y=y_default,
+            source=group,
+            line_color=matplotlib.colors.rgb2hex(cmap(val)),
+        )
+        lines.append(line)
+        counter = counter + 1
+
+    callback_x = CustomJS(
+        args=dict(lines=lines, xaxis=p.xaxis[0]),
+        code="""
+            var column = cb_obj.value;
+            console.log(column)
+            for (let line of lines) {
+                line.glyph.x = { field: column };
+            }
+            xaxis.axis_label = column;
+        """,
+    )
+    callback_y = CustomJS(
+        args=dict(lines=lines, yaxis=p.yaxis[0]),
+        code="""
+            var column = cb_obj.value;
+            console.log(column)
+            for (let line of lines) {
+                line.glyph.y = { field: column };
+            }
+            yaxis.axis_label = column;
+        """,
+    )
+
+    # Add list boxes for selecting which columns to plot on the x and y axis
+    xaxis_select = Select(title="X axis:", value=x_default, options=x_options)
+    xaxis_select.js_on_change("value", callback_x)
+
+    yaxis_select = Select(title="Y axis:", value=y_default, options=y_options)
+    yaxis_select.js_on_change("value", callback_y)
+
+    # hover = p.select(type=HoverTool)
+    hovertooltips = [
+        ("Cycle No.", "@{full cycle}"),
+    ]
+
+    p.add_tools(HoverTool(tooltips=hovertooltips))
+
+    p.js_on_event(DoubleTap, CustomJS(args=dict(p=p), code="p.reset.emit()"))
+    layout = column(p, xaxis_select, yaxis_select)
+    return layout
+
+
+def double_axes_plot(df, df2, x_options=["Capacity", "Time"], x_default="Capacity", **kwargs):
+    """
+    Creates bokeh layout with 2 plots - one is normal, one is dq/dv
+
+    Args:
+        df: Input df from echem_block.py to be used for first, normal plot
+        df2: Input df from echem_block.py with dq/dv values for second plot
+        x_options: Selectable x-axis options
+        y_options: Selectable y-axis options
+        x_default: Default x-axis that is printed at start, Default set to Capacity
+
+
+    Returns:
+        Bokeh layout
+    """
+    source1 = ColumnDataSource(df)
+    source2 = ColumnDataSource(df2)
+
+    # normal plot
+    p1 = figure(
+        aspect_ratio=1.5, x_axis_label=x_default, y_axis_label="Voltage", tools=TOOLS, **kwargs
+    )
+    # circle1 = p1.circle(x='Capacity', y=y_default, source=source1 )
+
+    # Need to group the df into groups of different half cycles and then plot a line through it. Plotting groups of full cycles means the point will
+    # jump from one end to the other when charge --> discharge
+
+    half_cycle_group = df.groupby("half cycle")
+
+    # We need to colour lines by full-cycles in a gradient
+    cmap = plt.get_cmap("plasma")
+    full_cycle_group = df["full cycle"].unique()
+    myList = sorted(full_cycle_group)
+    length = len(myList)
+    numberList = np.linspace(0, 1, length)
+    # Since we are plotting by half cycle, colour is assigned by half cycle, even though we need full cycle
+    # to get around this, we will assign each pair of half cycle for each full cycle the same cmap value
+    newList = []
+    for i in numberList:
+        newList.extend([i, i])
+
+    counter = 0
+    lines = []
+    for name, group in half_cycle_group:
+        val = newList[counter]
+        line = p1.line(
+            x=x_default,
+            y="Voltage",
+            source=group,
+            line_color=matplotlib.colors.rgb2hex(cmap(val)),
+        )
+        lines.append(line)
+        counter = counter + 1
+
+    # dqdv plot
+    p2 = figure(
+        aspect_ratio=1.5,
+        x_axis_label='dq/dv',
+        y_axis_label="Voltage",
+        tools=TOOLS,
+        y_range=p1.y_range,
+        **kwargs
+    )
+
+    callback_x = CustomJS(
+        args=dict(lines=lines, xaxis=p1.xaxis[0]),
+        code="""
+            var column = cb_obj.value;
+            console.log(column)
+            for (let line of lines) {
+                line.glyph.x = { field: column };
+            }
+            xaxis.axis_label = column;
+        """,
+    )
+    xaxis_select = Select(title="X axis:", value=x_default, options=x_options)
+    xaxis_select.js_on_change("value", callback_x)
+
+    # circle2 = p2.circle(x='dqdv', y=y_default, source=source2)
+
+    grouped2 = df2.groupby("half cycle")
+
+    counter2 = 0
+    # print(len(grouped))
+    for name, group in grouped2:
+        val2 = newList[counter2]
+        circle2 = p2.line(
+            x="dqdv",
+            y="Voltage",
+            source=group,
+            line_color=matplotlib.colors.rgb2hex(cmap(val2)),
+        )
+        counter2 = counter2 + 1
+
+    # hover = p.select(type=HoverTool)
+    hovertooltips = [
+        ("Cycle No.", "@{full cycle}"),
+        ("Half-cycle", "@{half cycle}")
+    ]
+
+    p1.add_tools(HoverTool(tooltips=hovertooltips))
+    p2.add_tools(HoverTool(tooltips=hovertooltips))
+
+    crosshair = CrosshairTool(dimensions="width")
+    p1.add_tools(HoverTool(tooltips=hovertooltips))
+    p2.add_tools(HoverTool(tooltips=hovertooltips))
+    p1.add_tools(crosshair)
+    p2.add_tools(crosshair)
+
+    p1.js_on_event(DoubleTap, CustomJS(args=dict(p=p1), code="p.reset.emit()"))
+    p2.js_on_event(DoubleTap, CustomJS(args=dict(p=p2), code="p.reset.emit()"))
+    layout = gridplot(
+        [[p1, p2], [xaxis_select]], sizing_mode="scale_width", toolbar_location="below"
+    )
+    return layout
+
+
+def double_axes_plot_dvdq(df, dvdq_df, x_default="Capacity", **kwargs):
+    """
+    Creates bokeh layout with 2 plots - one is normal, one is dq/dv
+
+    Args:
+        df: Input df from echem_block.py to be used for first, normal plot
+        dvdq_df: Input df from echem_block.py with dv/dq values for second plot
+        x_default: Default x-axis that is printed at start, Default set to Capacity
+
+
+    Returns:
+        Bokeh layout
+    """
+    source1 = ColumnDataSource(df)
+    source2 = ColumnDataSource(dvdq_df)
+
+    # colormapper = LinearColorMapper(palette="Plasma256",low=df['colour'].min(), high=df['colour'].max())
+
+    code_x = """
+      var column = cb_obj.value;
+      circle1.glyph.x.field = column;
+      source.change.emit();
+      xaxis.axis_label = column;
+      """
+    code_y = """
+      var column = cb_obj.value;
+      circle1.glyph.y.field = column;
+      source.change.emit();
+      yaxis.axis_label = column;
+      """
+
+    # normal plot
+    p1 = figure(
+        aspect_ratio=1.5, x_axis_label=x_default, y_axis_label="Voltage", tools=TOOLS, **kwargs
+    )
+    # circle1 = p1.circle(x='Capacity', y=y_default, source=source1 )
+
+    # Need to group the df into groups of different half cycles and then plot a line through it. Plotting groups of full cycles means the point will
+    # jump from one end to the other when charge --> discharge
+
+    half_cycle_group = df.groupby("half cycle")
+
+    # We need to colour lines by full-cycles in a gradient
+    cmap = plt.get_cmap("plasma")
+    full_cycle_group = df["full cycle"].unique()
+    myList = sorted(full_cycle_group)
+    length = len(myList)
+    numberList = np.linspace(0, 1, length)
+    # Since we are plotting by half cycle, colour is assigned by half cycle, even though we need full cycle
+    # to get around this, we will assign each pair of half cycle for each full cycle the same cmap value
+    newList = []
+    for i in numberList:
+        newList.extend([i, i])
+
+    counter = 0
+    for name, group in half_cycle_group:
+        val = newList[counter]
+        circle1 = p1.line(
+            x=x_default,
+            y="Voltage",
+            source=group,
+            line_color=matplotlib.colors.rgb2hex(cmap(val)),
+        )
+        counter = counter + 1
+
+    # dvdq plot
+    p2 = figure(
+        aspect_ratio=1.5,
+        x_axis_label=x_default,
+        y_axis_label="dv/dq",
+        tools=TOOLS,
+        x_range=p1.x_range,
+        **kwargs
+    )
+    # circle2 = p2.circle(x='dqdv', y=y_default, source=source2)
+
+    cmap = plt.get_cmap("plasma")
+
+    grouped2 = dvdq_df.groupby("half cycle")
+
+    counter2 = 0
+    # print(len(grouped))
+    for name, group in grouped2:
+        val2 = newList[counter2]
+        circle2 = p2.line(
+            x="Voltage",
+            y="dqdv",
+            source=group,
+            line_color=matplotlib.colors.rgb2hex(cmap(val2)),
+        )
+        counter2 = counter2 + 1
+
+        # Check if half cycle or not
+        if group["dqdv"].mean() > 0:
+            my_dqdv_array = np.array(group["dqdv"])
+
+            peaks, _ = find_peaks(my_dqdv_array, prominence=5)
+
+            my_peaks_df = group.iloc[peaks]
+
+            circle3 = p2.circle(x="Voltage", y="dqdv", source=my_peaks_df)
+        else:
+            my_dqdv_array = np.array(-group["dqdv"])
+
+            peaks, _ = find_peaks(my_dqdv_array, prominence=5)
+
+            my_peaks_df = group.iloc[peaks]
+
+            circle3 = p2.circle(x="Voltage", y="dqdv", source=my_peaks_df)
+
+    # hover = p.select(type=HoverTool)
+    hovertooltips = [
+        ("Cycle No.", "@{full cycle}"),
+        ("Half-cycle", "@{half cycle}")
+    ]
+
+    crosshair = CrosshairTool(dimensions="height")
+    p1.add_tools(HoverTool(tooltips=hovertooltips))
+    p2.add_tools(HoverTool(tooltips=hovertooltips))
+    p1.add_tools(crosshair)
+    p2.add_tools(crosshair)
+    p1.js_on_event(DoubleTap, CustomJS(args=dict(p=p1), code="p.reset.emit()"))
+    p2.js_on_event(DoubleTap, CustomJS(args=dict(p=p2), code="p.reset.emit()"))
+    layout = gridplot([[p1, p2]], sizing_mode="scale_width", toolbar_location="below")
+    return layout
--- webapp/src/components/datablocks/CycleBlock.vue
+++ webapp/src/components/datablocks/CycleBlock.vue
@@ -12,18 +12,52 @@
 		<div class="form-row col-md-4 col-lg-5 mt-2">
 			<div class="input-group form-inline">
 				<label class="mr-2"><b>Cycle number:</b></label>
-				<input type="text" class="form-control" v-model.number="cycle_number" @input="updateBlock">
+				<input type="text" class="form-control" 
+				v-model="cycle_number" 
+				@keydown.enter="updateBlock2"
+				@blur="updateBlock2">
 			</div>
+			
 		<div v-if="cycle_num_error" class="alert alert-warning">{{ cycle_num_error }}</div>
+		<!-- Insert another div here, if the filled value is a string or something -->
 		</div>
-		<div class="row">
+		<div class="button-box">
+				<input type="button"  class="btn btn-dark" value="Plot!" @click="updateBlock2" >
+				<input type="button" class="btn btn-dark" id='norm' value="Normal analysis!" @click="normal_modeselect(); color_select()">
+				<input type="button" class="btn btn-dark"  id ='dqdv' value="dq/dv analysis!" @click="dqdv_modeselect(); color_select() ">
+				<input type="button" class="btn btn-dark"  id='dvdq' value="dV/dq analysis!" @click="dvdq_modeselect(); color_select()">
+		</div>
+		<div class="slider-block">
+			<div class="slider" >
+				<input type="range" v-model="s_spline" id="s_spline" name="s_spline" min="1" max="10" step="0.2">
+				<label  for="volume" > <span @mouseenter="exp1 = true" @mouseleave="exp1 = false" >Spline fit:</span> {{- s_spline }}</label>
+			</div>
+			<div class="slider">
+				<input type="range" v-model="win_size_1" id="win_size_1" name="win_size_1" min="501" max="1501">
+				<label for="volume" > <span  @mouseenter="exp2 = true" @mouseleave="exp2 = false" >Window Size 1:</span>  {{ win_size_1 }}</label>
+			</div>
+			<div class="explaination" v-show="exp1" @mouseover="exp1 = true" @mouseleave="exp1 = false"> 
+
+			<p>Spline fit: determines how close the spline fits to the real data - less negative value results in a smoother fit with less details preserved</p>
+			</div>
+			<div class="explaination" v-show="exp2"  @mouseover="exp1 = true" @mouseleave="exp1 = false">
+			<p>Window Size: Range of data to be smoothed by Savitzky-Golay filter</p>
+			</div>
+
+		</div>
+		
+
+		<div class="row" id='plotarea'>
 			<div class="col-xl-8 col-lg-9 col-md-11 mx-auto">
 				<BokehPlot :bokehPlotData="bokehPlotData" />
 				<!-- <div v-if="!bokehPlotData" class="alert alert-danger">No bokeh Plot Data is loaded</div> -->
 			</div>
 		</div>
+
 	</DataBlockBase>
+
 </template>
+
 
 
 <script>
@@ -35,10 +69,14 @@ import BokehPlot from "@/components/BokehPlot"
 import {updateBlockFromServer} from "@/server_fetch_utils.js"
 import {createComputedSetterForBlockField} from "@/field_utils.js"
 
+
 export default {
 	data() {
 		return {
 			cycle_num_error: "",
+			cycle_number: "",
+			exp1 : false,
+			exp2 : false,
 		}
 	},
 	props: {
@@ -53,24 +91,210 @@ export default {
 			return this.$store.state.all_sample_data[this.sample_id]["blocks_obj"][this.block_id].number_of_cycles
 		},
 		file_id: createComputedSetterForBlockField("file_id"),
-		cycle_number: createComputedSetterForBlockField("cyclenumber"),
+		all_cycles: createComputedSetterForBlockField("cyclenumber"),
+		p_spline: createComputedSetterForBlockField("p_spline"),
+		s_spline: createComputedSetterForBlockField("s_spline"),
+		win_size_1: createComputedSetterForBlockField("win_size_1"),
+		win_size_2: createComputedSetterForBlockField("win_size_2"),
+		dqdv_mode: createComputedSetterForBlockField("plotmode-dqdv"),
+		dvdq_mode: createComputedSetterForBlockField("plotmode-dvdq"),
+		
+		
 	},
 	methods: {
-		updateBlock() {
-			if (this.cycle_number === parseInt(this.cycle_number, 10)) {
-				this.cycle_num_error = ""
-				updateBlockFromServer(this.sample_id, this.block_id, 
-					this.$store.state.all_sample_data[this.sample_id]["blocks_obj"][this.block_id])
+		parseCycleNumber(cycle_number) {
+			let cycle_string = cycle_number.replace(/\s/g, '')
+			var cycle_regex = /^(\d+(-\d+)?,)*(\d+(-\d+)?)$/g
+			if (cycle_number.match(/^ *$/) !== null){
+				this.cycle_num_error = "Plotting all cycles!"
+				return false
 			}
+			else if (!cycle_regex.test(cycle_string)) {
+				this.cycle_num_error = "Please enter a valid input!"
+				return false
+			}
+			let cycle_string_sections = cycle_string.split(',')
+			var all_cycles = []
+			for (const section of cycle_string_sections) {
+				let split_section = section.split('-')
+				if (split_section.length == 1) {
+					all_cycles.push(parseInt(split_section[0]))
+				} else {
+					let upper_range = parseInt(split_section[1])
+					let lower_range = parseInt(split_section[0])
+					for (let j = Math.min(lower_range, upper_range); j <= Math.max(lower_range, upper_range); j++) {
+						all_cycles.push(j)
+					}
+				}
+			}
+			
+			return all_cycles
+		},
+		updateBlock2() {
+			this.all_cycles = this.parseCycleNumber(this.cycle_number)
+			// if (!this.all_cycles) {
+			// 	return
+			// }
+			console.log('parsing')
+			this.cycle_num_error = this.all_cycles
+			updateBlockFromServer(this.sample_id, this.block_id, 
+				this.$store.state.all_sample_data[this.sample_id]["blocks_obj"][this.block_id])
+		},
+
+		dqdv_modeselect(){
+			
+			
+			//Default value for dqdv mode is False, following lines reverses boolean and shows/removes the dqdv slider section accordingly
+			this.dqdv_mode = !(this.dqdv_mode)
+
+			//if dqdv mode is activated, then dvdq mode HAS to be false
+			if (this.dqdv_mode && this.dvdq_mode){       // When calling the dqdv button, we know user wants dqdv. If both dqdv and dvdq are true, set dvdq to false
+				this.dvdq_mode = !(this.dvdq_mode)
+			}
+
+			console.log(this.dvdq_mode)
+			console.log(this.dqdv_mode)
+			if (this.dqdv_mode){
+				document.getElementsByClassName("slider-block")[0].style.display = "block"
+			}else{
+				document.getElementsByClassName("slider-block")[0].style.display = "none"
+			}
+
+
+			
+		},
+		dvdq_modeselect(){
+			//Default value for dqdv mode is False, following lines reverses boolean and shows/removes the dqdv slider section accordingly
+			this.dvdq_mode = !(this.dvdq_mode)
+
+			//if dqdv mode is activated, then dvdq mode HAS to be false
+			if (this.dvdq_mode && this.dqdv_mode){
+				this.dqdv_mode = !(this.dqdv_mode)
+			}
+			console.log(this.dvdq_mode)
+			console.log(this.dqdv_mode)
+			if (this.dvdq_mode){
+				document.getElementsByClassName("slider-block")[0].style.display = "block"
+			}else{
+				document.getElementsByClassName("slider-block")[0].style.display = "none"
+			}
+		},
+		normal_modeselect(){
+			
+			if (this.dqdv_mode){
+				this.dqdv_mode = !(this.dqdv_mode)
+			}
+			if (this.dvdq_mode){
+				this.dvdq_mode = !(this.dvdq_mode)
+			}
+			console.log(this.dvdq_mode)
+			console.log(this.dqdv_mode)
+		
+			document.getElementsByClassName("slider-block")[0].style.display = "none"
+			
+		},
+		color_select(){
+			var dqdv = document.getElementById('dqdv');
+			var dvdq = document.getElementById('dvdq');
+			var norm = document.getElementById('norm');
+
+			if (this.dqdv_mode){
+				
+				dqdv.style.color = 'green';
+				dvdq.style.color = 'white';
+				norm.style.color = 'white';
+			} 
+			else if (this.dvdq_mode){
+				
+				dqdv.style.color = 'white';
+				dvdq.style.color = 'green';
+				norm.style.color = 'white';
+			} 
 			else {
-				this.cycle_num_error="Please enter a number!"
-			}
-		}
+				
+				dqdv.style.color = 'white';
+				dvdq.style.color = 'white';
+				norm.style.color = 'green';
+			} 
+		},
+   
+
 	},
 	components: {
 		DataBlockBase,
 		FileSelectDropdown,
-		BokehPlot
+		BokehPlot,
 	},	
 }
 </script>
+
+<style scoped>
+
+.button-box {
+	display: block;
+	width: 100%;
+	margin: 10px 0 10px 0;
+	
+}
+
+
+.button-box input{
+	margin-right: 10px;
+}
+.slider {
+	display: inline-block;
+	width: 50%;
+}
+
+.slider label{
+	display: block;
+
+}
+
+.slider span{
+	border-bottom: 2px dotted #0c5460;
+	padding-bottom: 1px;
+}
+
+.slider-block{
+	margin-top: 20px;
+	margin-bottom: 20px;
+	display: none;
+
+}
+.mx-auto {
+	margin-left: 0 !important;
+	margin-right: 0 !important;
+	max-width: 100%;
+}
+
+#plotarea {
+	max-width: 100% !important;
+	display: block !important;
+}
+
+.explaination{
+	color: #0c5460;
+    background-color: #d1ecf1;
+    border-color: #bee5eb;
+	position: relative;
+    padding: 0.75rem 1rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+}
+
+
+
+.explaination p{
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-size: 15px;
+}
+
+
+
+
+
+
+
+</style>
