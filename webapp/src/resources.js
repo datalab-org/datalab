75a5b971c7ae3dbe4e9526d95d493b46ac143046
jdbocarsly <jdbocarsly@gmail.com>
2021-07-06 21:56:31 +0100

    Commented out import of SynthesisBlock until synthesis block funtionality can be fully added

--- webapp/src/resources.js
+++ webapp/src/resources.js
@@ -3,7 +3,7 @@ import DataBlockBase from "@/components/datablocks/DataBlockBase"
 import ImageBlock from "@/components/datablocks/ImageBlock"
 import XRDBlock from "@/components/datablocks/XRDBlock"
 import CycleBlock from "@/components/datablocks/CycleBlock"
-import SynthesisBlock from "@/components/datablocks/SynthesisBlock"
+// import SynthesisBlock from "@/components/datablocks/SynthesisBlock"
 
 export const blockKinds = {
    test: { description:"Test Block", component: DataBlockBase },
