f6c5136d8291c318babd91627a80ec94ec74938d
Matthew Evans <git@ml-evs.science>
2021-07-11 19:21:38 +0100

    Config tweaks:
    - Updated lock and reqs files
    - Updated Pipfile to properly pin Bokeh version
    - Renamed server dockerfile
    - Added explicit flake8 error run
    - Switch to only pipenv in CI and elsewhere

--- pydatalab/setup.py
+++ pydatalab/setup.py
@@ -16,37 +16,10 @@ with open(module_dir.joinpath("pydatalab/__init__.py")) as version_file:
         raise RuntimeError(f"Could not determine package version from {version_file.name} !")
 
 
-def strip_reqs(fname: Union[str, Path]) -> List[str]:
-    """Extract dependencies from a requirements.txt file that contains
-    #-delimited comments and any pip-specific arguments.
-
-    Parameters:
-        fname: The filename to read.
-
-    Returns:
-        Each requirements line found in the file.
-
-    """
-    with open(fname, "r") as f:
-        reqs = [
-            line.strip()
-            for line in f.readlines()
-            if line.strip() and not line.strip()[0] in ("#", "-")
-        ]
-    return reqs
-
-
-requirements = strip_reqs("./requirements.txt")
-dev_requirements = strip_reqs("./requirements-dev.txt")
-
 setup(
     name="pydatalab",
     version=VERSION,
     url="https://github.com/the-grey-group/datalabvue",
     include_package_data=True,
     packages=find_packages(),
-    install_requires=requirements,
-    extras_require={
-        "dev": dev_requirements,
-    },
 )
