077aca05f3148368c159b8c360855aaacfbd5b08
Matthew Evans <git@ml-evs.science>
2021-08-18 17:26:28 +0100

    Added config module

--- pydatalab/pydatalab/logger.py
+++ pydatalab/pydatalab/logger.py
@@ -2,14 +2,12 @@ import logging
 
 
 def setup_log():
-    # from pydatalab.config import CONFIG
+    from pydatalab.config import CONFIG
 
     logging.basicConfig()
     logger = logging.getLogger("pydatalab")
     log_level = logging.INFO
-    # if CONFIG.debug:
-    # replace this once config is merged
-    if True:
+    if CONFIG.DEBUG:
         log_level = logging.DEBUG
     logger.setLevel(log_level)
     return logger
