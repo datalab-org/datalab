2d7fe8d783f03c73d568297002720fcaf36aed71
Matthew Evans <git@ml-evs.science>
2021-08-18 17:33:01 +0100

    Added tests for all samples and file routes

---
+++ pydatalab/tests/routers/conftest.py
@@ -0,0 +1,41 @@
+from unittest.mock import patch
+
+import mongomock
+import pytest
+
+import pydatalab.mongo
+from pydatalab.main import create_app
+from pydatalab.models import Sample
+
+
+class PyMongoMock(mongomock.MongoClient):
+    def init_app(self, _):
+        return super().__init__()
+
+
+# Must remain as `db` so that calls to flask_mongo.db remain correct
+TEST_DATABASE_NAME = "db"
+MONGO_URI = f"mongodb://localhost:27017/{TEST_DATABASE_NAME}"
+
+
+@pytest.fixture(scope="session")
+def client():
+    with patch.object(
+        pydatalab.mongo,
+        "flask_mongo",
+        PyMongoMock(connectTimeoutMS=100, serverSelectionTimeoutMS=100),
+    ):
+        app = create_app({"TESTING": True, "MONGO_URI": MONGO_URI})
+
+        with app.test_client() as cli:
+            yield cli
+
+
+@pytest.fixture(scope="module", name="default_sample")
+def fixture_default_sample():
+    return Sample(**{"sample_id": "12345", "name": "other_sample", "date": "1970-02-01"})
+
+
+@pytest.fixture(scope="module", name="default_sample_dict")
+def fixture_default_sample_dict(default_sample):
+    return default_sample.dict(exclude_unset=True)
