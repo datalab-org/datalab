328c3d9eee3ff4c7d3754f296199ba88534ab562
Matthew Evans <git@ml-evs.science>
2021-08-18 17:27:46 +0100

    Add some type-hints and tidied blocks and bokeh_plots

--- pydatalab/pydatalab/blocks/__init__.py
+++ pydatalab/pydatalab/blocks/__init__.py
@@ -1,8 +1,10 @@
+from typing import Dict, Sequence, Type
+
 from pydatalab.blocks.blocks import CommentBlock, DataBlock, ImageBlock, XRDBlock
 from pydatalab.blocks.echem_block import CycleBlock
 
-BLOCKS = (DataBlock, CommentBlock, ImageBlock, XRDBlock, CycleBlock)
-BLOCK_KINDS = {block.blocktype: block for block in BLOCKS}
+BLOCKS: Sequence[Type[DataBlock]] = (DataBlock, CommentBlock, ImageBlock, XRDBlock, CycleBlock)
+BLOCK_KINDS: Dict[str, Type[DataBlock]] = {block.blocktype: block for block in BLOCKS}
 BLOCK_KINDS["test"] = DataBlock
 
 __all__ = (
