3304cec9ef78f23e85bf8a6820157a1b346c1bf3
Matthew Evans <git@ml-evs.science>
2021-08-18 17:26:28 +0100

    Added config module

---
+++ pydatalab/tests/test_config.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from pydatalab.config import CONFIG
+
+
+def test_default_settings():
+    assert CONFIG.SECRET_KEY == "dummy key"
+    assert CONFIG.MONGO_URI == "mongodb://localhost:27017/datalabvue"
+    assert CONFIG.UPLOAD_PATH == "uploads"
+    assert Path(CONFIG.FILE_DIRECTORY).name == "files"
+
+
+def test_update_settings():
+    new_settings = {
+        "secret_key": "new_secret",
+        "mongo_uri": "mongodb://test",
+        "new_key": "some new data",
+    }
+    CONFIG.update(new_settings)
+
+    assert CONFIG.SECRET_KEY == new_settings["secret_key"]
+    assert CONFIG.MONGO_URI == new_settings["mongo_uri"]
+    assert CONFIG.NEW_KEY == new_settings["new_key"]
+    assert CONFIG.UPLOAD_PATH == "uploads"
+    assert Path(CONFIG.FILE_DIRECTORY).name == "files"
