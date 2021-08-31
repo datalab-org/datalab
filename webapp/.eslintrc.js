0574b42c9358881643680991a111c9ee84ce7a6d
jdbocarsly <jdbocarsly@gmail.com>
2021-08-31 12:16:12 +0100

    adding in first cypress tests (currently fail)

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     parser: "babel-eslint",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    //"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
   overrides: [
---
+++ webapp/cypress/integration/sampleTablePage.spec.js
@@ -0,0 +1,41 @@
+let consoleSpy; // keeps track of every time an error is written to the console
+Cypress.on("window:before:load", (win) => {
+	consoleSpy = cy.spy(win.console, "error");
+});
+
+describe("Sample table page", () => {
+	beforeEach(() => {
+		cy.visit("/");
+	});
+
+	// afterEach( () => {
+	// 	cy.expect(consoleSpy).not.to.be.called
+	// });
+
+	it("Loads the main page without any errors", () => {
+		cy.findByText("About").should("exist");
+		cy.findByText("Samples").should("exist");
+		cy.findByText("Add a sample").should("exist");
+		cy.findByText("# of blocks").should("exist");
+
+		// Ensure no error messages or console errors. The wait is necessary so that 
+		// the assertion does not run before the server has had 
+		// time to respond. 
+		// Can we wait for the server response instead of hard-coding 
+		// a wait time in ms?
+		cy.wait(1000).then((x) => {
+			cy.contains("Server Error. Sample list not retreived.").should("not.exist")
+			expect(consoleSpy).not.to.be.called;
+		});
+	});
+
+	it("Adds a valid sample", () => {
+		cy.findByText("Add a sample").click();
+		cy.findByText("Add new sample").should("exist");
+		cy.findByLabelText("Sample ID:").type("12345678910");
+		cy.findByLabelText("Date Created:").type("1990-01-07");
+
+		cy.findByLabelText("Sample Name:").type("This is a sample name");
+		cy.findByText("Submit").click();
+	});
+});
