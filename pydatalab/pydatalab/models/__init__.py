3224a7472aece632919d7533cb3c14392625d650
jdbocarsly <jdbocarsly@gmail.com>
2021-10-08 11:18:42 +0100

    changes to starting materials model and import script

--- pydatalab/pydatalab/models/__init__.py
+++ pydatalab/pydatalab/models/__init__.py
@@ -1,7 +1,5 @@
 from pydatalab.models.files import File
 from pydatalab.models.samples import Sample
+from pydatalab.models.starting_materials import StartingMaterial
 
-__all__ = (
-    "File",
-    "Sample",
-)
+__all__ = ("File", "Sample", "StartingMaterial")
--- pydatalab/pydatalab/models/samples.py
+++ pydatalab/pydatalab/models/samples.py
@@ -1,17 +1,27 @@
+import datetime
 from typing import Optional
 
 from pydantic import Field
 
-from pydatalab.models.items import Item
+from pydatalab.models.items import Item, validator
 
 
 class Sample(Item):
     """A model for representing an experimental sample."""
 
+    type: str = Field("samples", const="samples", pattern="^samples$")
+
     sample_id: str = Field(description="A machine-readable UUID for the sample.")
 
-    type: str = Field("samples", const="samples", pattern="^samples$")
+    date: datetime.datetime = Field(description="The creation timestamp of the item.")
 
     chemform: Optional[str] = Field(
         description="A string representation of the chemical formula associated with this sample."
     )
+
+    @validator("date", pre=True)
+    def cast_to_datetime(cls, v):
+        if isinstance(v, str):
+            v = datetime.datetime.fromisoformat(v)
+
+        return v
