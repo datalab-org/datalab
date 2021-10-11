8c40c1c98a998ec97e51e155811a91bd99a2f37d
jdbocarsly <jdbocarsly@gmail.com>
2021-10-11 22:04:37 +0100

    fix lint error (unused imports in setupTests.js)

--- webapp/tests/setupTests.js
+++ webapp/tests/setupTests.js
@@ -1,15 +1,3 @@
 import fetchMock from "jest-fetch-mock";
-import { API_URL } from "@/resources.js";
-import { config } from "@vue/test-utils";
 
 fetchMock.enableMocks();
-
-// config.mocks["$API_URL"] = API_URL;
-// config.mocks["$filters"] = {
-//   IsoDatetimeToDate(isodatetime) {
-//     if (isodatetime) {
-//       return isodatetime.substring(0, 10);
-//     }
-//     return isodatetime; // if isodatetime is null or empty, don't do anything
-//   },
-// };
