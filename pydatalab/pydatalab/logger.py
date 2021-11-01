0850bc3179719777662f53d7eb62104df05320bf
Matthew Evans <git@ml-evs.science>
2021-11-01 10:40:35 +0000

    Add stdout logger

--- pydatalab/pydatalab/logger.py
+++ pydatalab/pydatalab/logger.py
@@ -1,16 +1,29 @@
 import logging
+import sys
 
 
 def setup_log():
+    """Creates a logger for pydatalab with a simple
+    stdout output.
+
+    Verbosity is set in the config file via
+    the DEBUG option.
+
+    """
     from pydatalab.config import CONFIG
 
     logging.basicConfig()
     logger = logging.getLogger("pydatalab")
     log_level = logging.INFO
+    stdout_handler = logging.StreamHandler(sys.stdout)
     if CONFIG.DEBUG:
         log_level = logging.DEBUG
     logger.setLevel(log_level)
+    stdout_handler.setLevel(log_level)
+    stdout_handler.setFormatter("%(asctime)s - %(name)s | %(levelname)8s: %(message)s")
+    logger.addHandler(stdout_handler)
     return logger
 
 
+"""The main logging object to be imported from elsewhere in the package."""
 LOGGER = setup_log()
