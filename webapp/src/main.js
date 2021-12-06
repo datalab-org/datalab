48cc54e6bce0bf9a8e8596d5e5a68cb63a241113
Matthew Evans <git@ml-evs.science>
2021-12-06 23:40:02 +0000

    Make use of new remote filesystem API in app, plus some tweaks

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -70,7 +70,7 @@ import Editor from "@tinymce/tinymce-vue";
 import store from "./store";
 
 // css for vue-select
-import "../node_modules/vue-select/dist/vue-select.css";
+import "vue-select/dist/vue-select.css";
 
 const app = createApp(App);
 
