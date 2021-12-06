039aa8b94b3fa08b43265e4cc7fbfcf8db5ff18b
Matthew Evans <git@ml-evs.science>
2021-12-06 23:35:29 +0000

    Rework remote filesystems endpoints to use optional caching

--- pydatalab/pydatalab/routes/remotes.py
+++ pydatalab/pydatalab/routes/remotes.py
@@ -1,52 +1,40 @@
-import datetime
 from typing import Callable, Dict
 
-from flask import jsonify
+from flask import jsonify, request
 
-from pydatalab import remote_filesystems
-from pydatalab.logger import LOGGER
+from pydatalab.config import CONFIG
+from pydatalab.remote_filesystems import get_directory_structures
 
 
 def list_remote_directories():
-    # all_directory_structures = remote_filesystems.get_all_directory_structures()
-    all_directory_structures = remote_filesystems.get_all_directory_structures()
-    return jsonify(all_directory_structures), 200
+    """Returns the most recent directory structures from the server.
+
+    If the cache is missing or is older than some configured time,
+    then it will be reconstructed.
+
+    """
+    invalidate_cache = None
+    if "invalidate_cache" in request.args:
+        invalidate_cache = request.args["invalidate_cache"]
+        if invalidate_cache not in ("1", "0"):
+            return jsonify({"error": "invalidate_cache must be 0 or 1"}), 400
+        invalidate_cache = bool(int(invalidate_cache))
+
+    all_directory_structures = get_directory_structures(
+        CONFIG.REMOTE_FILESYSTEMS, invalidate_cache=invalidate_cache
+    )
+
+    oldest_update = min(d["last_updated"] for d in all_directory_structures)
+    response = {}
+    response["data"] = all_directory_structures
+    response["meta"] = {}
+    response["meta"]["oldest_cache_update"] = oldest_update.isoformat()
+    return jsonify(response), 200
 
 
 list_remote_directories.methods = ("GET",)  # type: ignore
 
 
-def list_remote_directories_cached():
-    """return the most recent cached remote directory tree, without actually tree-ing the remote directory"""
-    all_directory_structures = remote_filesystems.get_cached_directory_structures()
-    last_update_datetimes = [
-        datetime.datetime.fromisoformat(d["last_updated"])
-        for d in all_directory_structures
-        if d["last_updated"]
-    ]
-
-    seconds_since_last_update = None
-    if last_update_datetimes:
-        LOGGER.debug("Last update from cache: %s", last_update_datetimes)
-        seconds_since_last_update = (
-            datetime.datetime.now() - min(last_update_datetimes)
-        ).total_seconds()
-
-    return (
-        jsonify(
-            {
-                "cached_dir_structures": all_directory_structures,
-                "seconds_since_last_update": seconds_since_last_update,
-                "ncached_not_found": len(all_directory_structures) - len(last_update_datetimes),
-            }
-        ),
-        200,
-    )
-
-
-list_remote_directories_cached.methods = ("GET",)  # type: ignore
-
 ENDPOINTS: Dict[str, Callable] = {
     "/list-remote-directories/": list_remote_directories,
-    "/list-remote-directories-cached/": list_remote_directories_cached,
 }
