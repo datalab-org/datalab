8ffcc0f943d32e4265bc2afd78ff67c0f9474aff
Matthew Evans <git@ml-evs.science>
2022-09-20 18:38:57 +0100

    Dynamically populate field names to use for FTS in index script

--- pydatalab/pydatalab/models/__init__.py
+++ pydatalab/pydatalab/models/__init__.py
@@ -1,7 +1,11 @@
+from typing import Dict
+
+from pydantic import BaseModel
+
 from pydatalab.models.files import File
 from pydatalab.models.samples import Sample
 from pydatalab.models.starting_materials import StartingMaterial
 
-ITEM_MODELS = {"samples": Sample, "starting_materials": StartingMaterial}
+ITEM_MODELS: Dict[str, BaseModel] = {"samples": Sample, "starting_materials": StartingMaterial}
 
 __all__ = ("File", "Sample", "StartingMaterial", "ITEM_MODELS")
