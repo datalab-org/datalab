debe70742bcb69f1f6a4444c8c61e5b015a59885
Matthew Evans <git@ml-evs.science>
2022-09-20 18:38:57 +0100

    Dynamically populate field names to use for FTS in index script

--- pydatalab/scripts/create_mongo_indices.py
+++ pydatalab/scripts/create_mongo_indices.py
@@ -1,10 +1,19 @@
-from pymongo import MongoClient
+from pymongo import TEXT, MongoClient
 
 from pydatalab.config import CONFIG
+from pydatalab.models import ITEM_MODELS
 
-client = MongoClient(CONFIG.MONGO_URI)
+client: MongoClient = MongoClient(CONFIG.MONGO_URI)
 
 db = client.datalabvue
 
-response = db.items.create_index([("$**", "text")])
+fts_fields = set()
+for model in ITEM_MODELS:
+    schema = ITEM_MODELS[model].schema()
+    for f in schema["properties"]:
+        if schema["properties"][f]["type"] == "string":
+            fts_fields.add(f)
+
+response = db.items.create_index([(k, TEXT) for k in fts_fields], name="items_fts")
 response = db.items.create_index("type")
+response = db.items.create_index("item_id", unique=True)
