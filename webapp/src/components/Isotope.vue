b4a0e9efbebc4fabcf1fd60ff876773523e76629
jdbocarsly <jdbocarsly@gmail.com>
2022-09-22 19:47:53 +0100

    Add component for formatting isotopes with proper superscript

---
+++ webapp/src/components/Isotope.vue
@@ -0,0 +1,20 @@
+<template>
+  <span v-if="isotopeString">
+    <sup>{{ isotopeFormat[1] }}</sup
+    >{{ isotopeFormat[2] }}
+  </span>
+  <!-- <span v-else><i>no isotope found</i></span> -->
+</template>
+
+<script>
+export default {
+  props: { isotopeString: String },
+  computed: {
+    isotopeFormat() {
+      const re = /(\d+)([A-z][a-z]?)/;
+      var match = this.isotopeString.match(re);
+      return match ? match : [null, null, this.isotopeString];
+    },
+  },
+};
+</script>
