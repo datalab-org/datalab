d67d4d6734712e64bc8cca53558c8c7b35b8ae7e
Matthew Evans <git@ml-evs.science>
2022-11-26 15:14:19 +0000

    Add login management and user querying to Sample and StartingMaterial pages
    - Update e2e test snapshot

--- webapp/src/views/StartingMaterials.vue
+++ webapp/src/views/StartingMaterials.vue
@@ -3,6 +3,13 @@
     <router-link to="/about">About</router-link> |
     <router-link to="/samples">Samples</router-link> |
     <router-link to="/starting-materials">Starting Materials</router-link>
+    <div v-if="currentUser != null" class="btn mx-auto normal">
+      <b>{{ currentUser }}</b> <i>({{ currentUserID }})</i>
+      <a :href="this.api_url + '/logout'" class="btn-link mx-auto text-primary"> (Logout)</a>
+    </div>
+    <div v-else class="btn btn-default mx-auto">
+      <a :href="this.api_url + '/login/github'">Login</a>
+    </div>
   </div>
   <div id="tableContainer" class="container">
     <div class="row">
@@ -15,10 +22,29 @@
 
 <script>
 import StartingMaterialTable from "@/components/StartingMaterialTable.vue";
+import { getUserInfo } from "@/server_fetch_utils.js";
+import { API_URL } from "@/resources.js";
 
 export default {
   components: {
     StartingMaterialTable,
+  },
+  data() {
+    return {
+      currentUser: null,
+      currentUserID: null,
+      api_url: API_URL,
+    };
+  },
+  methods: {
+    async getUser() {
+      let user = await getUserInfo();
+      this.currentUser = user.display_name;
+      this.currentUserID = user.immutable_id;
+    },
+  },
+  mounted() {
+    this.getUser();
   },
 };
 </script>
