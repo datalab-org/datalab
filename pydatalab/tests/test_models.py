46b449a83961b4696cc20618d5cfe61661e61736
Matthew Evans <git@ml-evs.science>
2023-01-20 18:24:44 +0000

    Add test for schema generation

--- pydatalab/tests/test_models.py
+++ pydatalab/tests/test_models.py
@@ -1,6 +1,7 @@
 import pydantic
 import pytest
 
+from pydatalab.models import ITEM_MODELS
 from pydatalab.models.relationships import (
     KnownType,
     RelationshipType,
@@ -8,20 +9,29 @@ from pydatalab.models.relationships import (
 )
 
 
+def test_generate_schemas():
+    """Test that all item model schemas can be generated."""
+    for type in ITEM_MODELS:
+        assert ITEM_MODELS[type].schema()
+
+
 def test_relationship_with_custom_type():
     """Test that a relationship with a custom type can be created."""
     relationship = TypedRelationship(
         relation=RelationshipType.OTHER,
-        type=KnownType.SAMPLE,
+        type=KnownType.SAMPLES,
         item_id="1234",
         description="This is a relationship",
     )
     assert relationship.relation == RelationshipType.OTHER
-    assert relationship.type == KnownType.SAMPLE
+    assert relationship.type == KnownType.SAMPLES
     assert relationship.item_id == "1234"
     assert relationship.description == "This is a relationship"
 
     with pytest.raises(pydantic.ValidationError):
         relationship = TypedRelationship(
-            relation=RelationshipType.OTHER, type=KnownType.SAMPLE, item_id="1234", description=None
+            relation=RelationshipType.OTHER,
+            type=KnownType.SAMPLES,
+            item_id="1234",
+            description=None,
         )
