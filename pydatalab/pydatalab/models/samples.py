cf2477e6df724a3011a7d14bd50f4ab7452d0ce2
Matthew Evans <git@ml-evs.science>
2023-01-21 18:32:51 +0000

    Remove synthesis models for now

--- pydatalab/pydatalab/models/samples.py
+++ pydatalab/pydatalab/models/samples.py
@@ -1,9 +1,9 @@
-from typing import Dict, List, Optional
+from typing import List, Optional
 
 from pydantic import Field
 
 from pydatalab.models.items import Item
-from pydatalab.models.utils import JSON_ENCODERS, Mass
+from pydatalab.models.utils import JSON_ENCODERS
 
 
 class Sample(Item):
@@ -20,14 +20,6 @@ class Sample(Item):
     )
 
     synthesis_description: Optional[str] = Field(description="Details of the sample synthesis")
-
-    synthesis_quantities: Optional[Dict[str, Mass]] = Field(
-        description="The quantites of each constituent used in the synthesis, keyed by their item_id."
-    )
-
-    synthesis_yield: Optional[Mass] = Field(
-        description="The mass of the sample produced by the synthesis recipe with the given quantities."
-    )
 
     class Config:
         allow_arbitrary_types = True
