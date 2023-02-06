a3048eface15878baef3a14cfb8b5a128a2b5b12
Matthew Evans <git@ml-evs.science>
2023-02-06 17:41:46 +0000

    More about page tweaks

--- webapp/src/views/About.vue
+++ webapp/src/views/About.vue
@@ -4,11 +4,14 @@
     <div class="row">
       <div class="col-sm-8 mx-auto">
         <h2>About datalab</h2>
-        Datalab is a place to store experimental data and the connections between them. Datalab
-        development occurs on GitHub at
-        <a href="https://github.com/the-grey-group/datalabvue/">
-          <font-awesome-icon icon="github" />the-grey-group/datalabvue</a
-        >. Datalab was primarily developed by:
+        <p>Datalab is a place to store experimental data and the connections between them.</p>
+
+        <p>
+          Datalab development occurs on GitHub at
+          <a href="https://github.com/the-grey-group/datalabvue/"> the-grey-group/datalabvue</a>.
+        </p>
+
+        Datalab was primarily developed by:
         <ul>
           <li>Joshua Bocarsly (Department of Chemistry, University of Cambridge)</li>
           <li>Matthew Evans (MODL-IMCN, UCLouvain & Matgenix)</li>
@@ -19,7 +22,8 @@
         <a href="https://doi.org/10.3030/957189">10.3030/957189</a>), the
         <a href="https://www.big-map.eu"
           >Battery Interface Genome - Materials Acceleration Platform (BIG-MAP)</a
-        >, as an external stakeholder project.
+        >
+        , as an external stakeholder project.
 
         <div align="center">
           <img src="https://avatars.githubusercontent.com/u/75324577" width="100" />
