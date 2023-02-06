45c85daf5349c6d525848e5bb92825bce1233314
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2023-02-06 17:53:37 +0000

    Tweaks to about page and README formatting (#281)
    * About page tweaks
    
    * More about page tweaks

--- webapp/src/views/About.vue
+++ webapp/src/views/About.vue
@@ -4,24 +4,30 @@
     <div class="row">
       <div class="col-sm-8 mx-auto">
         <h2>About datalab</h2>
-        Datalab is a place to store experimental data and the connections between them. Datalab
-        development occurs on GitHub at
-        <a href="https://github.com/the-grey-group/datalabvue/">
-          <font-awesome-icon icon="github" />the-grey-group/datalabvue</a
-        >. Datalab was primarily developed by:
+        <p>Datalab is a place to store experimental data and the connections between them.</p>
 
+        <p>
+          Datalab development occurs on GitHub at
+          <a href="https://github.com/the-grey-group/datalabvue/"> the-grey-group/datalabvue</a>.
+        </p>
+
+        Datalab was primarily developed by:
         <ul>
           <li>Joshua Bocarsly (Department of Chemistry, University of Cambridge)</li>
           <li>Matthew Evans (MODL-IMCN, UCLouvain & Matgenix)</li>
         </ul>
 
         This project has received funding from the European Union's Horizon 2020 research and
-        innovation programme under grant agreement 957189 (<a href="https://doi.org/10.3030/957189"
-          ><font-awesome-icon icon="doi" />10.3030/957189]</a
-        >, the
+        innovation programme under grant agreement 957189 (DOI:
+        <a href="https://doi.org/10.3030/957189">10.3030/957189</a>), the
         <a href="https://www.big-map.eu"
           >Battery Interface Genome - Materials Acceleration Platform (BIG-MAP)</a
-        >, as an external stakeholder project.
+        >
+        , as an external stakeholder project.
+
+        <div align="center">
+          <img src="https://avatars.githubusercontent.com/u/75324577" width="100" />
+        </div>
 
         <!-- <tiny-mce-inline /> -->
       </div>
