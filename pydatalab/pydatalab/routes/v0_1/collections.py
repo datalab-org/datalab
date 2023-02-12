ab971b094129b6109627834ba46e381a903a2d86
Matthew Evans <git@ml-evs.science>
2023-02-12 14:00:25 +0000

    Add save collection endpoint

--- pydatalab/pydatalab/routes/v0_1/collections.py
+++ pydatalab/pydatalab/routes/v0_1/collections.py
@@ -4,9 +4,10 @@ import json
 from flask import Blueprint, jsonify, request
 from flask_login import current_user
 from pydantic import ValidationError
-from pymongo.results import UpdateResult
+from pymongo.results import InsertOneResult, UpdateResult
 
 from pydatalab.config import CONFIG
+from pydatalab.logger import logged_route
 from pydatalab.models.collections import Collection
 from pydatalab.mongo import flask_mongo
 from pydatalab.routes.utils import get_default_permissions
@@ -59,7 +60,7 @@ def get_collection(collection_id):
 
     samples = get_samples_summary(
         match={
-            "relationships.type": "collection",
+            "relationships.type": "collections",
             "relationships.immutable_id": collection.immutable_id,
         }
     )
@@ -135,7 +136,7 @@ def create_collection():
             400,
         )
 
-    result = flask_mongo.db.collections.insert_one(data_model.dict())
+    result: InsertOneResult = flask_mongo.db.collections.insert_one(data_model.dict())
     if not result.acknowledged:
         return (
             dict(
@@ -146,6 +147,8 @@ def create_collection():
             ),
             400,
         )
+
+    immutable_id = result.inserted_id
 
     errors = []
     if starting_members:
@@ -158,11 +161,7 @@ def create_collection():
                 "item_id": {"$in": list(item_ids)},
                 **get_default_permissions(user_only=True),
             },
-            {
-                "$push": {
-                    "relationships": {"type": "collection", "immutable_id": data_model.immutable_id}
-                }
-            },
+            {"$push": {"relationships": {"type": "collections", "immutable_id": immutable_id}}},
         )
 
         data_model.num_items = results.modified_count
@@ -191,6 +190,74 @@ def create_collection():
         jsonify(response),
         201,  # 201: Created
     )
+
+
+@collection.route("/collections/<collection_id>", methods=["PATCH"])
+@logged_route
+def save_collection(collection_id):
+
+    request_json = request.get_json()  # noqa: F821 pylint: disable=undefined-variable
+    updated_data = request_json.get("data")
+
+    if not updated_data:
+        return (
+            jsonify(
+                status="error",
+                message=f"Unable to find any data in request to update {collection_id=} with.",
+            ),
+            204,  # 204: No content
+        )
+
+    # These keys should not be updated here and cannot be modified by the user through this endpoint
+    for k in ("_id", "file_ObjectIds", "creators", "creator_ids", "collection_id"):
+        if k in updated_data:
+            del updated_data[k]
+
+    updated_data["last_modified"] = datetime.datetime.now().isoformat()
+
+    collection = flask_mongo.db.collections.find_one(
+        {"collection_id": collection_id, **get_default_permissions(user_only=True)}
+    )
+
+    if not collection:
+        return (
+            jsonify(
+                status="error",
+                message=f"Unable to find item with appropriate permissions and {collection_id=}.",
+            ),
+            400,
+        )
+
+    collection.update(updated_data)
+
+    try:
+        collection = Collection(**collection).dict()
+    except ValidationError as exc:
+        return (
+            jsonify(
+                status="error",
+                message=f"Unable to update item {collection_id=} with new data {updated_data}",
+                output=str(exc),
+            ),
+            400,
+        )
+
+    result: UpdateResult = flask_mongo.db.collections.update_one(
+        {"collection_id": collection_id},
+        {"$set": collection},
+    )
+
+    if result.modified_count != 1:
+        return (
+            jsonify(
+                status="error",
+                message=f"Unable to update item {collection_id=} with new data {updated_data}",
+                output=result.raw_result,
+            ),
+            400,
+        )
+
+    return jsonify(status="success"), 200
 
 
 @collection.route("/collections/<collection_id>", methods=["DELETE"])
@@ -236,18 +303,21 @@ def search_collections():
 
     match_obj = {"$text": {"$search": query}, **get_default_permissions(user_only=True)}
 
-    cursor = flask_mongo.db.collections.aggregate(
-        [
-            {"$match": match_obj},
-            {"$sort": {"score": {"$meta": "textScore"}}},
-            {"$limit": nresults},
-            {
-                "$project": {
-                    "collection_id": 1,
-                    "title": 1,
-                }
-            },
-        ]
-    )
+    cursor = [
+        json.loads(Collection(**doc).json(exclude_unset=True))
+        for doc in flask_mongo.db.collections.aggregate(
+            [
+                {"$match": match_obj},
+                {"$sort": {"score": {"$meta": "textScore"}}},
+                {"$limit": nresults},
+                {
+                    "$project": {
+                        "collection_id": 1,
+                        "title": 1,
+                    }
+                },
+            ]
+        )
+    ]
 
     return jsonify({"status": "success", "collections": list(cursor)}), 200
--- pydatalab/pydatalab/routes/v0_1/items.py
+++ pydatalab/pydatalab/routes/v0_1/items.py
@@ -123,6 +123,11 @@ def get_samples_summary(match: Optional[Dict] = None):
             {
                 "$project": {
                     "_id": 0,
+                    "creators": 1,
+                    "item_id": 1,
+                    "name": 1,
+                    "chemform": 1,
+                    "type": 1,
                 }
             },
             {"$sort": {"_id": -1}},
@@ -566,7 +571,7 @@ def save_item():
     updated_data = request_json["data"]
 
     # These keys should not be updated here and cannot be modified by the user through this endpoint
-    for k in ("_id", "file_ObjectIds", "creators", "creator_ids"):
+    for k in ("_id", "file_ObjectIds", "creators", "creator_ids", "item_id"):
         if k in updated_data:
             del updated_data[k]
 
