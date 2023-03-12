7e9a1033255f751f4e2b58374e6f174a38be03fc
Matthew Evans <git@ml-evs.science>
2023-03-12 11:59:19 +0000

    Add explicit error handling for pydantic ValidationError

--- pydatalab/pydatalab/errors.py
+++ pydatalab/pydatalab/errors.py
@@ -1,7 +1,9 @@
 import traceback
+import os
 from typing import Any, Callable, Iterable, Tuple
 
 from flask import Response, jsonify
+from pydantic import ValidationError
 from werkzeug.exceptions import Forbidden, HTTPException
 
 from pydatalab.config import CONFIG
@@ -49,17 +51,26 @@ def render_unauthorised_user_template(exc: UserRegistrationForbidden) -> Tuple[R
     """Return a rich HTML page on user account creation failure."""
     return Response(response=exc.description), exc.code
 
+def handle_pydantic_validation_error(exc: ValidationError) -> Tuple[Response, int]:
+    """Handle pydantic validation errors separately from other exceptions.
+    These always come from malformed data, so should not necessarily trigger the
+    Flask debugger.
+    """
+    response = {
+        "title": exc.__class__.__name__,
+        "message": str(exc.args[:]) if exc.args else "",
+    }
+    return jsonify(response), 500
+
 
 def handle_generic_exception(exc: Exception) -> Tuple[Response, int]:
     """Return a specific error message and status code if the exception stores them."""
-    if CONFIG.DEBUG:
-        raise (exc)
+    if os.environ.get("FLASK_ENV") == "development":
+        raise exc
 
     response = {
         "title": exc.__class__.__name__,
-        "description": exc.args[0] if exc.args else "",
-        # Return the full traceback under the key 'detail':
-        "detail": "".join(traceback.format_exc()).replace("\n", "\\n"),
+        "message": str(exc.args) if exc.args else "",
     }
     return jsonify(response), 500
 
@@ -67,5 +78,6 @@ def handle_generic_exception(exc: Exception) -> Tuple[Response, int]:
 ERROR_HANDLERS: Iterable[Tuple[Any, Callable[[Any], Tuple[Response, int]]]] = [
     (UserRegistrationForbidden, render_unauthorised_user_template),
     (HTTPException, handle_http_exception),
+    (ValidationError, handle_pydantic_validation_error),
     (Exception, handle_generic_exception),
 ]
