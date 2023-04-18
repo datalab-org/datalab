b628ec2d62ca5fa2ae29190799425ad2026ba0fb
Joshua Bocarsly <jdbocarsly@users.noreply.github.com>
2023-04-18 15:30:46 +0100

    tweak the prompt and add the local item graph to the prompt

--- pydatalab/pydatalab/apps/chat/blocks.py
+++ pydatalab/pydatalab/apps/chat/blocks.py
@@ -9,16 +9,16 @@ __all__ = "ChatBlock"
 
 
 class ChatBlock(DataBlock):
-
     blocktype = "chat"
     description = "LLM Chat Block with contextual data (powered by GPT-3.5-turbo)"
     accepted_file_extensions: Sequence[str] = []
     defaults = {
-        "system_prompt": """You are C.L.A.I.R.E., an LLM interface for cybernetically enhanced materials chemistry in the group of Professor Dame Clare Grey FRS in the Department of Chemistry at the University of Cambridge.
+        "system_prompt": """You are a virtual assistant that helps materials chemists manage their experimental data. You are deployed in the group of Professor Clare Grey in the Department of Chemistry at the University of Cambridge.
 
-You have been embedded within the group's data management solution, datalab, where you have access to recorded data on samples, electrochemical cells and characterisation experiments (XRD, NMR etc.). You will be asked questions about the data and you are tasked with providing helpful responses to questions that researcher's may ask about their data. You are currently only available in a trial mode but one day we would like you to learn from the group wiki at https://wikis.ch.cam.ac.uk/grey/wiki/index.php/Main_Page to allow you to provide even more helpful answers!
+        You are embedded within the program datalab, where you have access to json that describes 'items'. These items may include experimental samples, starting materials, and devices (e.g. batteries made out of experimental samples and starting materials). Answer questions about the data as concisely as possible.
 
-We look forward to working with you!""",
+        Start the conversion with a friendly greeting introducing yourself.
+        """,
     }
     openai.api_key = os.environ.get("OPENAI_API_KEY")
 
@@ -27,9 +27,11 @@ We look forward to working with you!""",
         return (self.render,)
 
     def render(self):
+        from pydatalab.routes.v0_1.graphs import get_graph_cy_format
         from pydatalab.routes.v0_1.items import get_item_data
 
         item_info = get_item_data(self.data["item_id"], load_blocks=False).json
+        graph = get_graph_cy_format(self.data["item_id"])
 
         item_info["item_data"]["blocks_obj"] = []
 
@@ -41,7 +43,9 @@ We look forward to working with you!""",
                 },
                 {
                     "role": "system",
-                    "content": f"Below is the JSON API response for this sample, please start with a brief description of the experiment after a friendly welcome message of your choice. Please only reference the data in the JSON response, do not reference the data in the wiki or any other source. JSON data: {item_info}",
+                    "content": f"""The current item has item_id: {self.data['item_id']}. Here is a JSON that describes links between this sample and others: {graph}.
+
+                    Here is a json file that describes the current item and others that are linked to it: {item_info}""",
                 },
             ]
 
