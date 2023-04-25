3ee06880e1c720fc34a8c5b43b578bf718781776
jdbocarsly <jdbocarsly@gmail.com>
2023-04-25 15:04:19 +0100

    A few token tweaks
    - reduce token count of prompt and lower temperature of GPT
    
    - rename chat block menu id
    
    - bug fixes in block munging

--- pydatalab/pydatalab/apps/chat/blocks.py
+++ pydatalab/pydatalab/apps/chat/blocks.py
@@ -1,3 +1,4 @@
+import json
 import os
 from typing import Sequence
 
@@ -6,6 +7,7 @@ import openai
 from pydatalab.blocks.blocks import DataBlock
 from pydatalab.logger import LOGGER
 from pydatalab.models import ITEM_MODELS
+from pydatalab.utils import CustomJSONEncoder
 
 __all__ = "ChatBlock"
 
@@ -16,10 +18,8 @@ class ChatBlock(DataBlock):
     accepted_file_extensions: Sequence[str] = []
     defaults = {
         "system_prompt": """You are a virtual assistant that helps materials chemists manage their experimental data. You are deployed in the group of Professor Clare Grey in the Department of Chemistry at the University of Cambridge.
-
-        You are embedded within the program datalab, where you have access to JSON that describes 'items'. These items may include experimental samples, starting materials, and devices (e.g. batteries made out of experimental samples and starting materials). Answer questions about the data as concisely as possible.
-
-        Start the conversion with a friendly greeting introducing yourself.
+You are embedded within the program datalab, where you have access to JSON describing an ‘item’ with connections to other items. These items may include experimental samples, starting materials, and devices (e.g. battery cells made out of experimental samples and starting materials).
+ Answer questions in markdown. Specify the language for all markdown code blocks. Be as concise as possible. Start the conversion with a friendly greeting introducing yourself.
         """,
     }
     openai.api_key = os.environ.get("OPENAI_API_KEY")
@@ -29,18 +29,16 @@ class ChatBlock(DataBlock):
         return (self.render,)
 
     def render(self):
-        from pydatalab.routes.v0_1.graphs import get_graph_cy_format
         from pydatalab.routes.v0_1.items import get_item_data
 
         item_info = get_item_data(self.data["item_id"], load_blocks=False).json
-        graph = get_graph_cy_format(self.data["item_id"])
 
         model = ITEM_MODELS[item_info["item_data"]["type"]](**item_info["item_data"])
         if model.blocks_obj:
             model.blocks_obj = {
                 k: value for k, value in model.blocks_obj.items() if value["blocktype"] != "chat"
             }
-        item_info = model.dict(exclude_none=True, exclude_unset=True, exclude_defaults=True)
+        item_info = model.dict(exclude_none=True, exclude_unset=True)
         item_info["type"] = model.type
         # LOGGER.debug(item_info)
 
@@ -52,10 +50,13 @@ class ChatBlock(DataBlock):
             "display_order",
             "creator_ids",
             "refcode",
-            "last_modified" "revision",
+            "last_modified",
+            "revision",
             "revisions",
             "immutable_id",
+            "file_ObjectIds",
         ]
+
         for k in top_level_keys_to_remove:
             item_info.pop(k, None)
 
@@ -68,6 +69,30 @@ class ChatBlock(DataBlock):
 
         item_info = {k: value for k, value in item_info.items() if value}
 
+        for key in [
+            "synthesis_constituents",
+            "positive_electrode",
+            "negative_electrode",
+            "electrolyte",
+        ]:
+            if key in item_info:
+                for constituent in item_info[key]:
+                    LOGGER.debug("iterating through constituents:")
+                    LOGGER.debug(constituent)
+                    if "quantity" in constituent:
+                        constituent[
+                            "quantity"
+                        ] = f"{constituent.get('quantity', 'unknown')} {constituent.get('unit', '')}"
+                    constituent.pop("unit", None)
+
+        # Note manual replaces to help avoid escape sequences that take up extra tokens
+        item_info_json = (
+            json.dumps(item_info, cls=CustomJSONEncoder)
+            .replace('"', "'")
+            .replace(r"\'", "'")
+            .replace(r"\n", " ")
+        )
+
         if not self.data.get("messages"):
             self.data["messages"] = [
                 {
@@ -75,12 +100,13 @@ class ChatBlock(DataBlock):
                     "content": self.defaults["system_prompt"],
                 },
                 {
-                    "role": "system",
-                    "content": f"""The current item has item_id: {self.data['item_id']}. Here is a JSON that describes links between this sample and others: {graph}.
-
-                    Here is the JSON data for the current item and others that are linked to it: {item_info}""",
+                    "role": "user",
+                    "content": f"""Here is the JSON data for the current item: {item_info_json}""",
                 },
             ]
+        # for debugging:
+        # with open("test_item_info.json", "w") as f:
+        #     f.write(self.data["messages"][-1]["content"])
 
         if self.data.get("prompt"):
             self.data["messages"].append(
@@ -105,7 +131,7 @@ class ChatBlock(DataBlock):
             responses = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=self.data["messages"],
-                temperature=0.9,
+                temperature=0.2,
                 max_tokens=1024,
             )
         except openai.InvalidRequestError as exc:
