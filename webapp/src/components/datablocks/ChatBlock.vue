2f6d6360e9d37c8b785f58ac386d16346fabd8fe
jdbocarsly <jdbocarsly@gmail.com>
2023-04-26 15:45:53 +0100

    prompt tweak to get the assistant to make diagrams more readily
    properly register svg and mermaid code blocks regardless of capitalization
    
    re-raise errors after updatebblock failures so that they can be handled again if needed
    
    add regenerate button
    
    tweak block update error handling flow to properly mark when block is updating
    
    add ability to show/hide initial prompts

--- webapp/src/components/datablocks/ChatBlock.vue
+++ webapp/src/components/datablocks/ChatBlock.vue
@@ -2,9 +2,30 @@
   <!-- think about elegant two-way binding to DataBlockBase... or, just pass all the block data into
 DataBlockBase as a prop, and save from within DataBlockBase  -->
   <DataBlockBase :item_id="item_id" :block_id="block_id">
+    <div v-if="contextHidden" @click="contextHidden = !contextHidden" class="context-button">
+      [show prompts]
+    </div>
+    <div v-if="!contextHidden" @click="contextHidden = !contextHidden" class="context-button">
+      [hide prompts]
+    </div>
+
     <div class="row">
       <div id="chatWindowContainer" class="col-xl-9 col-lg-10 col-md-11 mx-auto">
-        <ChatWindow :chatMessages="messages" :isLoading="isLoading" />
+        <ChatWindow :chatMessages="messages.slice(contextHidden ? 2 : 0)" :isLoading="isLoading" />
+        <div class="d-flex justify-content-center">
+          <button
+            class="btn btn-default btn-sm regenerate-button"
+            @click="regenerateLastResponse"
+            :disabled="messages[messages.length - 1]['role'] != 'assistant'"
+          >
+            <font-awesome-icon
+              :icon="['fa', 'sync']"
+              class="pr-1 text-muted"
+              :spin="isRegenerating"
+            />
+            regenerate response
+          </button>
+        </div>
       </div>
     </div>
     <div class="input-group form-inline col-md-10 mx-auto align-items-end">
@@ -44,6 +65,8 @@ export default {
   data() {
     return {
       isLoading: false,
+      isRegenerating: false,
+      contextHidden: true,
     };
   },
   computed: {
@@ -70,11 +93,30 @@ export default {
       );
       this.isLoading = false;
     },
+    async regenerateLastResponse() {
+      this.isLoading = true;
+      this.isRegenerating = true;
+      const last_message = this.messages.pop();
+      await updateBlockFromServer(
+        this.item_id,
+        this.block_id,
+        this.$store.state.all_item_data[this.item_id]["blocks_obj"][this.block_id]
+      ).catch(() => {
+        this.messages.push(last_message);
+      });
+      this.isLoading = false;
+      this.isRegenerating = false;
+    },
   },
 };
 </script>
 
 <style scoped>
+.context-button {
+  cursor: pointer;
+  float: right;
+}
+
 .send-button {
   height: 2.5rem;
   border-radius: 2rem;
@@ -82,5 +124,10 @@ export default {
   left: -70px;
   top: -10px;
   z-index: 10;
+}
+
+.regenerate-button {
+  margin-top: -1rem;
+  margin-bottom: 1rem;
 }
 </style>
