3264bc5bcc6564d960e62984f419aa2e6dc64529
Matthew Evans <git@ml-evs.science>
2023-04-28 14:11:34 +0100

    Overlay spline fit over raw data in MassSpec block

--- pydatalab/pydatalab/apps/tga/blocks.py
+++ pydatalab/pydatalab/apps/tga/blocks.py
@@ -2,11 +2,13 @@ import os
 from pathlib import Path
 
 import bokeh
+import numpy as np
 from bokeh.layouts import gridplot
+from scipy.interpolate import splev, splrep
 
 from pydatalab.apps.tga.parsers import parse_ms_ascii
-from pydatalab.blocks import DataBlock
-from pydatalab.bokeh_plots import mytheme, selectable_axes_plot
+from pydatalab.blocks.blocks import DataBlock
+from pydatalab.bokeh_plots import selectable_axes_plot
 from pydatalab.file_utils import get_file_info_by_id
 from pydatalab.logger import LOGGER
 
@@ -27,6 +29,7 @@ class MassSpecBlock(DataBlock):
 
         if "file_id" not in self.data:
             LOGGER.warning("No file set in the DataBlock")
+            return
         else:
             file_info = get_file_info_by_id(self.data["file_id"], update_if_live=True)
             ext = os.path.splitext(file_info["location"].split("/")[-1])[-1].lower()
@@ -44,17 +47,40 @@ class MassSpecBlock(DataBlock):
         y_options = ["Partial Pressure [mbar]"]
 
         if ms_data:
+            for species in ms_data["data"]:
+                pressure = ms_data["data"][species]["Partial Pressure [mbar]"].to_numpy()
+                time = ms_data["data"][species]["Time Relative [s]"].to_numpy()
+
+                time = time[~np.isnan(time)]
+                pressure = pressure[~np.isnan(pressure)]
+
+                ms_data["data"][species]["Partial Pressure [mbar] (spline)"] = splev(
+                    time,
+                    splrep(
+                        time,
+                        pressure,
+                        xe=time[~np.isnan(time)].max(),
+                        xb=time[~np.isnan(time)].min(),
+                        s=10,
+                    ),
+                )
+
             if grid_plot:
                 plots = []
-                for species in ms_data["data"]:
+                for ind, species in enumerate(ms_data["data"]):
                     plots.append(
                         selectable_axes_plot(
                             {species: ms_data["data"][species]},
                             x_options=x_options,
                             y_options=y_options,
+                            y_default=[
+                                "Partial Pressure [mbar] (spline)",
+                                "Partial Pressure [mbar]",
+                            ],
                             plot_line=True,
                             plot_points=False,
-                            y_axis_type="log",
+                            plot_title=species,
+                            plot_index=ind,
                         )
                     )
                 # construct 3xN grid of all species
@@ -72,4 +98,4 @@ class MassSpecBlock(DataBlock):
                     y_axis_type="log",
                 )
 
-            self.data["bokeh_plot_data"] = bokeh.embed.json_item(p, theme=mytheme)
+            self.data["bokeh_plot_data"] = bokeh.embed.json_item(p)
