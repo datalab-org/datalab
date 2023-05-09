36316f366fe21b857c976bb9daa76fafc74ce228
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2023-05-09 17:52:12 +0100

    Reworking XRD functionality into standalone submodule (#338)
    * WIP work on XRD schema
    
    * Refactor XRD block around new schema
    
    * Add simple tests for xrd block pattern loader
    
    * Minor tidying of XRD utils

--- pydatalab/pydatalab/blocks/blocks.py
+++ pydatalab/pydatalab/blocks/blocks.py
@@ -3,20 +3,17 @@ import io
 import os
 import random
 import zipfile
-from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple
+from typing import Any, Callable, Dict, Optional, Sequence
 
 import bokeh.embed
-import numpy as np
 import pandas as pd
 from bson import ObjectId
 from PIL import Image
-from scipy.signal import medfilt
 
-from pydatalab import nmr_utils, xrd_utils
+from pydatalab import nmr_utils
 from pydatalab.bokeh_plots import mytheme, selectable_axes_plot
 from pydatalab.file_utils import get_file_info_by_id
 from pydatalab.logger import LOGGER
-from pydatalab.mongo import flask_mongo
 
 __all__ = ("generate_random_id", "DataBlock")
 
@@ -271,132 +268,3 @@ class NMRBlock(DataBlock):
         bokeh_layout.children[0].x_range.flipped = True  # flip x axis, per NMR convention
 
         self.data["bokeh_plot_data"] = bokeh.embed.json_item(bokeh_layout, theme=mytheme)
-
-
-class XRDBlock(DataBlock):
-    blocktype = "xrd"
-    description = "Powder XRD"
-    accepted_file_extensions = (".xrdml", ".xy", ".dat", ".xye")
-
-    defaults = {"wavelength": 1.54060}
-
-    @property
-    def plot_functions(self):
-        return (self.generate_xrd_plot,)
-
-    def load_pattern(self, location: str) -> Tuple[pd.DataFrame, List[str]]:
-
-        ext = os.path.splitext(location.split("/")[-1])[-1].lower()
-
-        if ext == ".xrdml":
-            df = xrd_utils.parse_xrdml(location)
-
-        elif ext == ".xy":
-            df = pd.read_csv(location, sep=r"\s+", names=["twotheta", "intensity"])
-
-        else:
-            df = pd.read_csv(location, sep=r"\s+", names=["twotheta", "intensity", "error"])
-
-        df = df.rename(columns={"twotheta": "2θ (°)"})
-        try:
-            wavelength = float(self.data["wavelength"])
-
-            df["Q (Å⁻¹)"] = 4 * np.pi / wavelength * np.sin(np.deg2rad(df["2θ (°)"]) / 2)
-            df["d (Å)"] = 2 * np.pi / df["Q (Å⁻¹)"]
-
-        # if no wavelength (or invalid wavelength) is passed, don't convert to Q and d
-        except (ValueError, ZeroDivisionError):
-            pass
-
-        df["sqrt(intensity)"] = np.sqrt(df["intensity"])
-        df["log(intensity)"] = np.log10(df["intensity"])
-        df["normalized intensity"] = df["intensity"] / np.max(df["intensity"])
-        polyfit_deg = 15
-        polyfit_baseline = np.poly1d(
-            np.polyfit(df["2θ (°)"], df["normalized intensity"], deg=polyfit_deg)
-        )(df["2θ (°)"])
-        df["intensity - polyfit baseline"] = df["normalized intensity"] - polyfit_baseline
-        df["intensity - polyfit baseline"] /= np.max(df["intensity - polyfit baseline"])
-        df[f"baseline (`numpy.polyfit`, deg={polyfit_deg})"] = polyfit_baseline / np.max(
-            df["intensity - polyfit baseline"]
-        )
-
-        kernel_size = 101
-        median_baseline = medfilt(df["normalized intensity"], kernel_size=kernel_size)
-        df["intensity - median baseline"] = df["normalized intensity"] - median_baseline
-        df["intensity - median baseline"] /= np.max(df["intensity - median baseline"])
-        df[
-            f"baseline (`scipy.signal.medfilt`, kernel_size={kernel_size})"
-        ] = median_baseline / np.max(df["intensity - median baseline"])
-
-        df.index.name = location.split("/")[-1]
-
-        y_options = [
-            "normalized intensity",
-            "intensity",
-            "sqrt(intensity)",
-            "log(intensity)",
-            "intensity - median baseline",
-            f"baseline (`scipy.signal.medfilt`, kernel_size={kernel_size})",
-            "intensity - polyfit baseline",
-            f"baseline (`numpy.polyfit`, deg={polyfit_deg})",
-        ]
-
-        return df, y_options
-
-    def generate_xrd_plot(self):
-        file_info = None
-        all_files = None
-        pattern_dfs = None
-
-        if "file_id" not in self.data:
-            # If no file set, try to plot them all
-
-            item_info = flask_mongo.db.items.find_one(
-                {"item_id": self.data["item_id"]},
-            )
-
-            all_files = [
-                d
-                for d in [
-                    get_file_info_by_id(f, update_if_live=False)
-                    for f in item_info["file_ObjectIds"]
-                ]
-                if any(d["name"].lower().endswith(ext) for ext in self.accepted_file_extensions)
-            ]
-
-            if not all_files:
-                LOGGER.warning(
-                    "XRDBlock.generate_xrd_plot(): Unsupported file extension (must be .xrdml or .xy)"
-                )
-                raise RuntimeError("XRDBlock.generate_xrd_plot(): No file set in DataBlock")
-
-            pattern_dfs = []
-            for f in all_files:
-                pattern_df, y_options = self.load_pattern(f["location"])
-                pattern_dfs.append(pattern_df)
-
-        else:
-            file_info = get_file_info_by_id(self.data["file_id"], update_if_live=True)
-            ext = os.path.splitext(file_info["location"].split("/")[-1])[-1].lower()
-            if ext not in self.accepted_file_extensions:
-                raise RuntimeError(
-                    "XRDBlock.generate_xrd_plot(): Unsupported file extension (must be one of %s), not %s",
-                    self.accepted_file_extensions,
-                    ext,
-                )
-
-            pattern_dfs, y_options = self.load_pattern(file_info["location"])
-            pattern_dfs = [pattern_dfs]
-
-        if pattern_dfs:
-            p = selectable_axes_plot(
-                pattern_dfs,
-                x_options=["2θ (°)", "Q (Å⁻¹)", "d (Å)"],
-                y_options=y_options,
-                plot_line=True,
-                plot_points=True,
-                point_size=3,
-            )
-
-            self.data["bokeh_plot_data"] = bokeh.embed.json_item(p, theme=mytheme)
