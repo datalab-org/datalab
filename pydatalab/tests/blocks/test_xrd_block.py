aa5e5e20b65ef8aca306dbd7d0f712f3070137f7
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2023-05-20 11:30:28 +0100

    Fix XRD plot wavelength interaction (#389)
    * Fix XRDBlock wavelength interaction
    
    * Add simple xrd plot test

--- pydatalab/tests/blocks/test_xrd_block.py
+++ pydatalab/tests/blocks/test_xrd_block.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pytest
 
 from pydatalab.apps.xrd.blocks import XRDBlock
+from pydatalab.bokeh_plots import selectable_axes_plot
 
 
 @pytest.fixture
@@ -14,3 +15,17 @@ def test_load(data_files):
     for f in data_files:
         df, y_options = XRDBlock.load_pattern(f)
         assert all(y in df.columns for y in y_options)
+
+
+def test_plot(data_files):
+    f = next(data_files)
+    df, y_options = XRDBlock.load_pattern(f)
+    p = selectable_axes_plot(
+        [df],
+        x_options=["2θ (°)", "Q (Å⁻¹)", "d (Å)"],
+        y_options=y_options,
+        plot_line=True,
+        plot_points=True,
+        point_size=3,
+    )
+    assert p
