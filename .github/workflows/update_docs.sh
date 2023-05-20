8d1819c056dfa1e120cf2d01e479c4c4fa54d34c
Matthew Evans <git@ml-evs.science>
2023-05-20 11:23:51 +0000

    Use GitHub actions to automate release workflow

---
+++ .github/workflows/update_docs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+echo -e "\n-o- Setting commit user -o-"
+git config --global user.email "${GIT_USER_EMAIL}"
+git config --global user.name "${GIT_USER_NAME}"
+
+echo "\n-o- Update version -o-"
+cd pydatalab
+pipenv run invoke dev.set-version -v ${GITHUB_REF#refs/tags/}
+
+echo "\n-o- Commit updates - Changelog -o-"
+git add pydatalab/__init__.py
+git commit -m "Release ${GITHUB_REF#refs/tags/}"
+
+echo -e "\n-o- Update version tag -o-"
+git tag -af ${GITHUB_REF#refs/tags/}
