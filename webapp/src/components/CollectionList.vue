4f0b67640dfad6fde03444906660088daf6badd0
Matthew Evans <git@ml-evs.science>
2023-05-26 11:52:58 +0100

    Add UI components for formatting collection names

---
+++ webapp/src/components/CollectionList.vue
@@ -0,0 +1,27 @@
+<template>
+  <span v-for="collection in collections" :key="collection.collection_id">
+    <FormattedCollectionName
+      :collection_id="collection.collection_id"
+      enableClick
+      enableModifiedClick
+    />
+  </span>
+</template>
+
+<script>
+import FormattedCollectionName from "@/components/FormattedCollectionName.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    collections: {
+      type: Array,
+    },
+  },
+  components: {
+    FormattedCollectionName,
+  },
+};
+</script>
---
+++ webapp/src/components/FormattedCollectionName.vue
@@ -0,0 +1,72 @@
+<template>
+  <span
+    class="badge badge-light mr-2"
+    :class="{ clickable: enableClick || enableModifiedClick }"
+    :style="{ backgroundColor: badgeColor }"
+    @click.exact="enableClick ? openEditPageInNewTab() : null"
+    @click.meta.stop="enableModifiedClick ? openEditPageInNewTab() : null"
+    @click.ctrl.stop="enableModifiedClick ? openEditPageInNewTab() : null"
+  >
+    {{ collection_id }}
+  </span>
+  {{ shortenedName }}
+</template>
+
+<script>
+import { itemTypes } from "@/resources.js";
+
+export default {
+  data() {
+    return {
+      itemType: "collections",
+    };
+  },
+  props: {
+    collection_id: String,
+    title: {
+      type: String,
+      default: "",
+    },
+    enableClick: {
+      type: Boolean,
+      default: false,
+    },
+    enableModifiedClick: {
+      type: Boolean,
+      default: false,
+    },
+    maxLength: {
+      type: Number,
+      default: NaN,
+    },
+  },
+  computed: {
+    badgeColor() {
+      return itemTypes[this.itemType]?.lightColor || "LightGrey";
+    },
+    shortenedName() {
+      if (this.maxLength && this.maxLength < this.name.length) {
+        return this.title.substring(0, this.maxLength) + "...";
+      } else {
+        return this.title;
+      }
+    },
+  },
+  methods: {
+    openEditPageInNewTab() {
+      this.$emit("collectionIdClicked");
+      window.open(`/collections/${this.collection_id}`, "_blank");
+    },
+  },
+  emits: ["collectionIdClicked"],
+};
+</script>
+
+<style scoped>
+.clickable {
+  cursor: pointer;
+}
+.badge {
+  color: black;
+}
+</style>
