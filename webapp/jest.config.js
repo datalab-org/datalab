606fb4f7e64b91d9989ba63f439cadbc9a9f3bbb
Matthew Evans <git@ml-evs.science>
2023-05-26 12:08:35 +0100

    Update dependencies: remove jest and tweak babel config for dev
    - Add cypress and log dirs to gitignore
    
    - Refactor babel to only strip console in production

--- webapp/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
-  setupFiles: ["<rootDir>/tests/setupTests.js"],
-  transform: {
-    "^.+\\.vue$": "vue-jest",
-    "^.+\\.js$": "babel-jest",
-  },
-  transformIgnorePatterns: ["<rootDir>/node_modules/(?!vue-select)"],
-};
