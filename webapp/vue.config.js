67af0cf1d4bb1493372d7461ef289949b898f6ba
Matthew Evans <git@ml-evs.science>
2023-08-16 12:00:48 +0100

    Add polyfill for stream

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   transpileDependencies: ["mermaid"],
   configureWebpack: (config) => {
-    config.resolve.fallback = { crypto: require.resolve("crypto-browserify") };
+    config.resolve.fallback = {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+    };
     config.externals = {
       ...config.externals,
       bokeh: "Bokeh",
