bb6fd8f73afc47ce1ed0207427ae9e70d4f0e129
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2023-08-16 17:06:46 +0100

    Update vue/cli to v5 (#432)
    * Update vue/cli
    
    * yarn upgrade
    
    * Disable vue/multi-word-component-names linter rule
    
    * Update polyfill for crypto in webpack5
    
    * Add polyfill for stream

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -5,11 +5,12 @@ module.exports = {
   },
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "@babel/eslint-parser",
   },
   rules: {
     //"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
   },
   overrides: [
     {
