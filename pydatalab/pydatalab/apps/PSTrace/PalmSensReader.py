8ab3361b44fe17edfacb1bbfc3029dfe6ca3441b
Gabriela Horwitz <ga.horwitz@gmail.com>
2023-10-30 12:18:35 +0000

    Palmsense reader code now can load the data and separate in different df according to keyword Impedance

--- pydatalab/pydatalab/apps/PSTrace/PalmSensReader.py
+++ pydatalab/pydatalab/apps/PSTrace/PalmSensReader.py
@@ -16,10 +16,26 @@ from io import StringIO
 #pd.options.display.max_rows = 10
 #pd.options.display.max_columns = 10
 #pd.options.display.float_format = "{:.1f}".format
+<<<<<<< HEAD
 
 
 filename = "PalmSense_test_datalab_shorter.csv"
 file_encoding = 'utf-16 LE'
+=======
+
+
+filename = "PalmSense_test_datalab_shorter.csv"
+file_encoding = 'utf-16 LE'
+
+def getdata():
+    """Loads all the experimental data to a dataframe per excel sheet"""
+            # set file_encoding to the file encoding (utf8, latin1, etc.)
+    #df = pd.read_csv(file, header = 2, encoding = file_encoding, index_col = False, names=range(25))
+
+    with open(filename, 'r', encoding = file_encoding) as temp_f:
+        # get No of columns in each line
+        col_count = [ len(l.split(",")) for l in temp_f.readlines() ]
+>>>>>>> cc22c82 (Palmsense reader code now can load the data and separate in different df according to keyword Impedance)
 
 
 def getdata(filename, file_encoding='utf-16 LE', verbose = False):
@@ -44,6 +60,7 @@ def getdata(filename, file_encoding='utf-16 LE', verbose = False):
     # Generate column names (names will be 0, 1, 2, ..., maximum columns - 1)
     column_names = [i for i in range(0, max(col_count))]
 
+<<<<<<< HEAD
     # Read CSV file into a DataFrame
     df = pd.read_csv(filename, header=None, names=column_names, encoding=file_encoding)
 
@@ -73,6 +90,40 @@ def getdata(filename, file_encoding='utf-16 LE', verbose = False):
 
 
 
+=======
+    ### Read csv
+    df = pd.read_csv(filename, header=None, names=column_names,encoding = file_encoding)
+
+#    print(dfhead)
+  #  print (df.head(n=10))
+
+    df.to_csv("test.csv")
+
+    
+    # Keyword to split on
+    keyword = 'Impedance'
+    
+    # Find the locations of the keyword in any column
+    mask = df.apply(lambda row: row.astype(str).str.contains(keyword), axis=1)
+    mask['Any'] = mask.any(axis=1)
+    groups = mask["Any"].cumsum()
+
+    # Splitting the DataFrame based on the keyword occurrences
+    split_dfs = {group: df[group == groups] for group in groups.unique()}
+    
+    # Display the split DataFrames
+    for key, split_df in split_dfs.items():
+        print(f"DataFrame for '{keyword}' occurrence {key}:")
+        print(split_df)
+        print("\n")
+
+
+
+
+
+    
+    return df
+>>>>>>> cc22c82 (Palmsense reader code now can load the data and separate in different df according to keyword Impedance)
 
 def main():
     """ Main program """
