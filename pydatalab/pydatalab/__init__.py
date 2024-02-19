6c9c9ce0defe35fff7157f5a0bcc2b635a7aed9a
Matthew Evans <git@ml-evs.science>
2024-02-19 12:44:18 +0000

    Bind mount git repo info on docker build to enable versions data to be passed through

--- pydatalab/pydatalab/__init__.py
+++ pydatalab/pydatalab/__init__.py
@@ -1,3 +1,6 @@
-from importlib.metadata import version
+from importlib.metadata import PackageNotFoundError, version
 
-__version__ = version("pydatalab")
+try:
+    __version__ = version("pydatalab")
+except PackageNotFoundError:
+    __version__ = "develop"
