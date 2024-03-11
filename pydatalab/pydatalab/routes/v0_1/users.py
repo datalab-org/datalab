b284751b21b91000d27820cc65e414735c42477d
Benjamin Charmes <107116804+BenjaminCharmes@users.noreply.github.com>
2024-03-11 14:18:13 +0000

    Update pydatalab/pydatalab/routes/v0_1/users.py
    Co-authored-by: Matthew Evans <7916000+ml-evs@users.noreply.github.com>
    
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci

--- pydatalab/pydatalab/routes/v0_1/users.py
+++ pydatalab/pydatalab/routes/v0_1/users.py
@@ -11,6 +11,13 @@ def save_user(user_id):
     user_name = request.json.get("data", {}).get("user_name")
 
     user_id_object = ObjectId(user_id)
-    flask_mongo.db.users.update_one({"_id": user_id_object}, {"$set": {"display_name": user_name}})
+    update_result = flask_mongo.db.users.update_one(
+        {"_id": user_id_object}, {"$set": {"display_name": user_name}}
+    )
+    if update_result.nmodified != 1:
+        return jsonify(
+            status="error",
+            detail="User does not have the appropriate permissions to update the given user ID.",
+        ), 403
 
     return jsonify(status="success"), 200
