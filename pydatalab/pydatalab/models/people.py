a0790d0b66872544dfcd3a4dd594a32111dd38ba
Benjamin CHARMES <benjamin.charmes@gmail.com>
2024-03-13 17:30:06 +0000

    Added validation in save_user

--- pydatalab/pydatalab/models/people.py
+++ pydatalab/pydatalab/models/people.py
@@ -122,7 +122,7 @@ class Person(Entry):
 
     @validator("display_name")
     def validate_display_name_length(cls, v):
-        """Validate that the display name."""
+        """Validate the display name."""
         if len(v) > 150:
             raise ValueError(
                 "Display name must be at most 150 characters long.")
@@ -131,9 +131,9 @@ class Person(Entry):
     @validator("contact_email")
     def validate_contact_email_format(cls, v):
         """Validate that the contact email has a valid email format."""
-        email_regex = re.compile(r"[^@]+@[^@]+\.[^@]+")
-        if v is not None:
-            not email_regex.match(EmailStr(v))
+        if not validate_email(v):
+            raise ValueError(
+                "Invalid email format for contact email.")
         return v
 
     @staticmethod
