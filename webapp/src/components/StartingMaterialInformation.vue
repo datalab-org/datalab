22cb8f03c24019c51fac95705b3fdf3d77c62fc6
jdbocarsly <jdbocarsly@gmail.com>
2024-03-18 16:59:25 -0500

    add fields for CAS and GHS codes to starting material edit page and the pydantic model, and rearrange layout of fields on the page
    add description and examples for GHS codes to starting_materials model

--- webapp/src/components/StartingMaterialInformation.vue
+++ webapp/src/components/StartingMaterialInformation.vue
@@ -2,22 +2,37 @@
   <div class="container-lg px-5 px-lg-0">
     <!-- Sample information -->
     <div id="starting-material-information" class="form-row">
-      <div class="form-group col-md-2 col-sm-3 col-sm-4">
+      <div class="form-group col-md-2 col-sm-3 col-6">
         <label for="item_id">Refcode</label>
         <div><FormattedRefcode :refcode="Refcode" /></div>
       </div>
-      <div class="form-group col-md-2 col-sm-3 col-sm-4">
+      <div class="form-group col-md-2 col-sm-3 col-6">
         <label for="item_id">Item ID</label>
         <StyledInput id="item_id" readonly :modelValue="ItemID" />
       </div>
-      <div class="form-group col-lg-7 col-md-8 col-sm-8">
+      <div class="form-group col-lg-7 col-md-8 col-sm-6">
         <label for="name">Name</label>
         <StyledInput id="name" v-model="Name" :readonly="!isEditable" />
       </div>
     </div>
     <div class="form-row">
-      <div class="form-group col-md-3 col-sm-4">
-        <label for="date-acquired" class="mr-2">Date acquired</label>
+      <div class="form-group col-lg-3 col-sm-4">
+        <label for="chemform">Chemical formula</label>
+        <ChemFormulaInput v-if="isEditable" id="chemform" v-model="ChemForm" />
+        <ChemicalFormula v-if="!isEditable" id="chemform" :formula="ChemForm" />
+      </div>
+      <div class="form-group col-lg-3 col-sm-4">
+        <label for="supplier">Supplier</label>
+        <StyledInput id="supplier" v-model="Supplier" :readonly="!isEditable" />
+      </div>
+      <div class="form-group col-lg-3 col-sm-4">
+        <label for="purity">Chemical purity</label>
+        <StyledInput id="purity" v-model="ChemicalPurity" :readonly="!isEditable" />
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-lg-3 col-sm-4">
+        <label for="date-acquired">Date acquired</label>
         <StyledInput
           id="date-acquired"
           type="date"
@@ -25,31 +40,27 @@
           :readonly="!isEditable"
         />
       </div>
-      <div class="form-group col-md-3 col-sm-4">
-        <label for="date-opened" class="mr-2">Date opened</label>
+      <div class="form-group col-lg-3 col-sm-4">
+        <label for="date-opened">Date opened</label>
         <StyledInput id="date-opened" type="date" v-model="DateOpened" :readonly="!isEditable" />
       </div>
-      <div class="form-group col-md-6 col-sm-4">
-        <label for="location" class="mr-2">Location</label>
+      <div class="form-group col-lg-3 col-sm-4">
+        <label for="location">Location</label>
         <StyledInput id="location" v-model="Location" :readonly="!isEditable" />
       </div>
     </div>
+
     <div class="form-row">
-      <div class="col-md-3">
+      <div class="form-group col-lg-3 col-sm-4">
+        <label for="cas">CAS</label>
+        <StyledInput id="cas" v-model="CAS" :readonly="!isEditable" />
+      </div>
+      <div class="form-group col-lg-3 col-sm-4">
+        <label for="hazards">GHS Hazard Codes</label>
+        <StyledInput id="hazards" v-model="GHS" :readonly="!isEditable" />
+      </div>
+      <div class="col-lg-3 col-sm-4">
         <ToggleableCollectionFormGroup v-model="Collections" />
-      </div>
-      <div class="form-group col-md-3">
-        <label for="chemform" class="mr-2">Chemical formula</label>
-        <ChemFormulaInput v-if="isEditable" id="chemform" v-model="ChemForm" />
-        <ChemicalFormula v-if="!isEditable" id="chemform" :formula="ChemForm" />
-      </div>
-      <div class="form-group col-md-3">
-        <label for="supplier" class="mr-2">Supplier</label>
-        <StyledInput id="supplier" v-model="Supplier" :readonly="!isEditable" />
-      </div>
-      <div class="form-group col-md-3">
-        <label for="purity" class="mr-2">Chemical purity</label>
-        <StyledInput id="purity" v-model="ChemicalPurity" :readonly="!isEditable" />
       </div>
     </div>
 
@@ -94,6 +105,8 @@ export default {
     },
     ItemID: createComputedSetterForItemField("item_id"),
     Name: createComputedSetterForItemField("name"),
+    CAS: createComputedSetterForItemField("CAS"),
+    GHS: createComputedSetterForItemField("GHS_codes"),
     DateAcquired: createComputedSetterForItemField("date_acquired"),
     DateOpened: createComputedSetterForItemField("date_opened"),
     ChemForm: createComputedSetterForItemField("chemform"),
