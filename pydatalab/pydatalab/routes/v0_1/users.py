eff7c7d668dbe61d8c69cd94ff1e8070481b0f55
Benjamin CHARMES <benjamin.charmes@gmail.com>
2024-03-19 14:39:15 +0000

    Update method for processing data to update user

--- pydatalab/pydatalab/routes/v0_1/users.py
+++ pydatalab/pydatalab/routes/v0_1/users.py
@@ -11,8 +11,8 @@ user = Blueprint("users", __name__)
 def save_user(user_id):
     request_json = request.get_json()
 
-    display_name = request_json.get("display_name")
-    contact_email = request_json.get("contact_email")
+    display_name = request_json.get("display_name", False)
+    contact_email = request_json.get("contact_email", False)
 
     if not current_user.is_authenticated:
         return jsonify(status="error"), 401
@@ -20,9 +20,22 @@ def save_user(user_id):
     if current_user.id != user_id and current_user.role != "admin":
         return jsonify(status="error"), 403
 
+    update = {}
+
+    if contact_email or contact_email in (None, ""):
+        if contact_email == "":
+            contact_email = None
+        update["contact_email"] = contact_email
+
+    if display_name:
+        update["display_name"] = display_name
+
+    if not update:
+        return jsonify(status="success", detail="No update was performed."), 200
+
     update_result = flask_mongo.db.users.update_one(
         {"_id": ObjectId(user_id)},
-        {"$set": {"display_name": display_name, "contact_email": contact_email}},
+        {"$set": update}
     )
 
     if update_result.modified_count != 1:
