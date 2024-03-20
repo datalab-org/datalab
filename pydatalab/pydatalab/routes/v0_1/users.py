8fe0e184e86aba940ced6c763108121b9b6779de
Benjamin CHARMES <benjamin.charmes@gmail.com>
2024-03-20 15:32:45 +0000

    Readd change
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci

--- pydatalab/pydatalab/routes/v0_1/users.py
+++ pydatalab/pydatalab/routes/v0_1/users.py
@@ -13,8 +13,8 @@ user = Blueprint("users", __name__)
 def save_user(user_id):
     request_json = request.get_json()
 
-    display_name = request_json.get("display_name")
-    contact_email = request_json.get("contact_email")
+    display_name = request_json.get("display_name", False)
+    contact_email = request_json.get("contact_email", False)
 
     if not current_user.is_authenticated:
         return jsonify(status="error"), 401
@@ -22,20 +22,23 @@ def save_user(user_id):
     if current_user.id != user_id and current_user.role != "admin":
         return jsonify(status="error"), 403
 
-    update_data = {"display_name": DisplayName(display_name)}
+    update = {}
 
-    if contact_email == "":
-        contact_email = None
+    if display_name:
+        update["display_name"] = DisplayName(display_name)
 
-    update_data["contact_email"] = EmailStr(contact_email) if contact_email is not None else None
+    if contact_email or contact_email in (None, ""):
+        if contact_email == "":
+            contact_email = None
+        update["contact_email"] = EmailStr(contact_email)
 
-    try:
-        update_result = flask_mongo.db.users.update_one(
-            {"_id": ObjectId(user_id)},
-            {"$set": update_data},
-        )
-    except Exception as e:
-        return jsonify(status="error", detail=str(e)), 500
+    if not update:
+        return jsonify(status="success", detail="No update was performed."), 200
+
+    update_result = flask_mongo.db.users.update_one({"_id": ObjectId(user_id)}, {"$set": update})
+
+    if update_result.matched_count != 1:
+        return jsonify(status="error", detail="Unable to update user."), 400
 
     if update_result.modified_count != 1:
         return jsonify(
