c903bdd49fc01dc23c20ff8d67835fa24ab8f171
jdbocarsly <jdbocarsly@gmail.com>
2024-03-26 17:23:46 -0500

    add python tests for creating sample and starting material with automatic ID

--- pydatalab/tests/server/test_startingmaterials.py
+++ pydatalab/tests/server/test_startingmaterials.py
@@ -45,6 +45,40 @@ def test_get_item_data(client, default_starting_material_dict):
         assert response.json["item_data"][key] == v
 
 
+@pytest.mark.dependency(depends=["test_new_starting_material", "test_get_item_data"])
+def test_new_sample_with_automatically_generated_id(client):
+    new_starting_material_data = {
+        "name": "sample with random id",
+        "date": datetime.datetime.fromisoformat("1995-03-02"),
+        "date_opened": datetime.datetime.fromisoformat("2001-12-31"),
+        "chemform": "NiO",
+        "type": "starting_materials",
+        "CAS": "1313-99-1",
+    }
+
+    request_json = dict(
+        new_sample_data=new_starting_material_data,
+        generate_id_automatically=True,
+    )
+
+    response = client.post("/new-sample/", json=request_json)
+    # Test that 201: Created is emitted
+    assert response.status_code == 201, response.json
+    assert response.json["status"] == "success"
+    created_item_id = response.json["item_id"]
+    assert created_item_id
+
+    response = client.get(f"/get-item-data/{created_item_id}")
+    assert response.status_code == 200
+    assert response.json["status"] == "success"
+    assert response.json["item_data"]["refcode"].split(":")[1] == created_item_id
+
+    for key in new_starting_material_data.keys():
+        if isinstance(v := new_starting_material_data[key], datetime.datetime):
+            v = v.isoformat()
+        assert response.json["item_data"][key] == v
+
+
 @pytest.mark.dependency(depends=["test_new_starting_material"])
 def test_new_starting_material_collision(client, default_starting_material_dict):
     # Try to do the same thing again, expecting an ID collision
