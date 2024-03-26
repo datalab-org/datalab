dacfe7a46f0e69398e315492c45d8669daa7acfe
Matthew Evans <git@ml-evs.science>
2024-03-26 14:53:41 +0000

    Tweak user route to group errors and allow for easier testing
    Add test for display name with Chiense characters

--- pydatalab/pydatalab/routes/v0_1/users.py
+++ pydatalab/pydatalab/routes/v0_1/users.py
@@ -1,9 +1,9 @@
 from bson import ObjectId
 from flask import Blueprint, jsonify, request
 from flask_login import current_user
-from pydantic import EmailStr
 
-from pydatalab.models.people import DisplayName
+from pydatalab.config import CONFIG
+from pydatalab.models.people import DisplayName, EmailStr
 from pydatalab.mongo import flask_mongo
 
 user = Blueprint("users", __name__)
@@ -13,13 +13,17 @@ user = Blueprint("users", __name__)
 def save_user(user_id):
     request_json = request.get_json()
 
-    display_name = request_json.get("display_name", False)
-    contact_email = request_json.get("contact_email", False)
+    display_name: str | None = None
+    contact_email: str | None = None
 
-    if not current_user.is_authenticated:
+    if request_json is not None:
+        display_name = request_json.get("display_name", False)
+        contact_email = request_json.get("contact_email", False)
+
+    if not current_user.is_authenticated and not CONFIG.TESTING:
         return (jsonify({"status": "error", "message": "No user authenticated."}), 401)
 
-    if current_user.id != user_id and current_user.role != "admin":
+    if not CONFIG.TESTING and current_user.id != user_id and current_user.role != "admin":
         return (
             jsonify({"status": "error", "message": "User not allowed to edit this profile."}),
             403,
@@ -30,19 +34,20 @@ def save_user(user_id):
     try:
         if display_name:
             update["display_name"] = DisplayName(display_name)
-    except ValueError as e:
-        return (jsonify({"status": "error", "message": str(e)}), 400)
 
-    try:
         if contact_email or contact_email in (None, ""):
-            if contact_email == "":
-                contact_email = None
-            update["contact_email"] = EmailStr(contact_email)
+            if contact_email in ("", None):
+                update["contact_email"] = None
+            else:
+                update["contact_email"] = EmailStr(contact_email)
+
     except ValueError as e:
-        return (jsonify({"status": "error", "message": str(e)}), 400)
+        return jsonify(
+            {"status": "error", "message": f"Invalid display name or email was passed: {str(e)}"}
+        ), 400
 
     if not update:
-        return (jsonify({"status": "success", "message": "No update was performed."}), 200)
+        return jsonify({"status": "success", "message": "No update was performed."}), 200
 
     update_result = flask_mongo.db.users.update_one({"_id": ObjectId(user_id)}, {"$set": update})
 
