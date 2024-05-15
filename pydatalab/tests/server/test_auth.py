010713035b35dd1f51a9124b0926d11be38bf044
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2024-05-15 18:10:01 +0100

    Make account status on registration be configurable by provider (#733)

--- pydatalab/tests/server/test_auth.py
+++ pydatalab/tests/server/test_auth.py
@@ -7,8 +7,8 @@ def test_allow_emails():
     assert _check_email_domain("test@example.org", ["example.org", "example2.org"])
     assert _check_email_domain("test@subdomain.example.org", ["example.org", "example2.org"])
     assert _check_email_domain("test@subdomain.example.org", ["subdomain.example.org"])
-    assert _check_email_domain("test@example2.org", [])
-    assert not _check_email_domain("test@example2.org", None)
+    assert not _check_email_domain("test@example2.org", [])
+    assert _check_email_domain("test@example2.org", None)
     assert not _check_email_domain("test@example.org", ["subdomain.example.org"])
     assert not _check_email_domain("test@example2.org", ["example.org"])
 
