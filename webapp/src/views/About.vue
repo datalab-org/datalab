275a842276bb33ff6511dcc45e6423da863768c9
Matthew Evans <git@ml-evs.science>
2024-05-31 14:01:57 +0100

    Bump version numbers

--- webapp/src/views/About.vue
+++ webapp/src/views/About.vue
@@ -34,7 +34,7 @@
             </tr>
             <tr>
               <td>App version</td>
-              <td><code>0.3.2</code></td>
+              <td><code>0.4.0-rc.4</code></td>
             </tr>
           </table>
         </div>
