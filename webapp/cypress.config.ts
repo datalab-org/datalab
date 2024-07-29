04130eea2becdc43ffd359f4801dfb95e506bf88
Matthew Evans <git@ml-evs.science>
2024-07-29 17:53:03 +0100

    Try to share same cloud project between e2e and component

--- webapp/cypress.config.ts
+++ webapp/cypress.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "4kqx5i",
   e2e: {
     baseUrl: "http://localhost:8080",
     apiUrl: "http://localhost:5001",
     experimentalMemoryManagement: true,
-    projectId: "4kqx5i",
     defaultCommandTimeout: 10000,
   },
   component: {
-    projectId: "4kqx5i",
     devServer: {
       framework: "vue-cli",
       bundler: "webpack",
