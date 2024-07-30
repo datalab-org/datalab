b595315b61f7727e0e78bd101b699b8547284236
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2024-07-30 22:25:04 +0100

    Re-enable Firefox e2e tests (#711)

--- webapp/cypress/support/e2e.js
+++ webapp/cypress/support/e2e.js
@@ -15,3 +15,10 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
+
+// eslint-disable-next-line no-unused-vars
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
