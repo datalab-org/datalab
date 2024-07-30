be5166b49c7ee1f9c3f830291bca15ba1c3d5bd3
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2024-07-30 11:07:37 +0100

    Add simple component test for `ChemFormInput` (#743)
    * Add simple compenent test for `ChemFormInput`
    
    * Attempt to run component tests in CI
    
    * Add cypress bundled stuff
    
    * Tweak cypress again
    
    * Typo
    
    * Try to fix cypress component testing
    
    * Temps commit to fix cypress component testing
    
    * Fix cypress component testing
    
    * Try to share same cloud project between e2e and component
    
    * Reintroduce cypress plugin stub
    
    * Set test with dummy data for now
    
    * Fix cypress component testing
    
    ---------
    
    Co-authored-by: Benjamin CHARMES <benjamin.charmes@gmail.com>

--- webapp/cypress.config.ts
+++ webapp/cypress.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "4kqx5i",
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
-    },
     baseUrl: "http://localhost:8080",
     apiUrl: "http://localhost:5001",
     experimentalMemoryManagement: true,
-    projectId: "4kqx5i",
     defaultCommandTimeout: 10000,
+  },
+  component: {
+    devServer: {
+      framework: "vue-cli",
+      bundler: "webpack",
+    },
   },
 });
