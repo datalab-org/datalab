baff6537664e92197399a2dc4ddba0c54b0f5e3f
Matthew Evans <git@ml-evs.science>
2024-08-20 10:38:51 +0100

    Fix setuptools-scm docker/CI behaviour and add simple test for version number
    - Mount .git directory one level higher in docker build so that version gets picked up
    
    - Mount .git directory in dev api container
    
    - More version tweaks
    
    - Set fetch-depth to 0 for pytest

--- pydatalab/tests/test_version.py
+++ pydatalab/tests/test_version.py
@@ -2,3 +2,5 @@ def test_version():
     from pydatalab import __version__
 
     assert isinstance(__version__, str)
+    assert int(__version__.split(".")[0]) == 0
+    assert int(__version__.split(".")[1]) >= 4
