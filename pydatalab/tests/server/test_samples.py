fd579bb71c3b99315bc138060c47a06604b252f0
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2024-10-14 15:14:28 +0100

    Ensure that new collection `immutable_id` is returned upon creation (#925)
    * Add test that new collection ID is returned upon creation
    
    * Make sure that new collection immutable_id is returned upon creation
    
    * Fix typo
    
    * Remove unecessary test case

--- pydatalab/tests/server/test_samples.py
+++ pydatalab/tests/server/test_samples.py
@@ -485,6 +485,7 @@ def test_create_collections(client, default_collection):
     assert response.json["data"]["collection_id"] == "test_collection"
     assert response.json["data"]["title"] == "My Test Collection"
     assert response.json["data"]["num_items"] == 0
+    assert response.json["data"]["immutable_id"]
 
     response = client.get("/collections")
     assert response.status_code == 200
