c38c81dc77fb57cf6d480d04846a370417367a9c
Matthew Evans <git@ml-evs.science>
2024-10-18 00:42:35 +0100

    Rework of the app file store to be scoped per item

--- webapp/src/file_upload.js
+++ webapp/src/file_upload.js
@@ -12,7 +12,6 @@ import { API_URL, UPPY_MAX_NUMBER_OF_FILES, UPPY_MAX_TOTAL_FILE_SIZE } from "@/r
 // file-upload loaded
 
 export default function setupUppy(item_id, trigger_selector, reactive_file_list) {
-  console.log("setupUppy called with: " + trigger_selector);
   var uppy = new Uppy({
     restrictions: {
       // Somewhat arbitrary restrictions that prevent numbers that would break the server in one go -- the API should also refuse files when 'full'
@@ -26,36 +25,21 @@ export default function setupUppy(item_id, trigger_selector, reactive_file_list)
       inline: false,
       trigger: trigger_selector,
       close_after_finish: true,
-      metaFields: [
-        {
-          id: "blahblah",
-          name: "Blah",
-          placeholder: "Metadata can be added here",
-        },
-      ],
     })
     .use(Webcam, { target: Dashboard })
     .use(XHRUpload, {
-      //someday, try to upgrade this to Tus
+      //someday, try to upgrade this to Tus for resumable uploads
       headers: headers,
       endpoint: `${API_URL}/upload-file/`,
       FormData: true, // send as form
       fieldName: "files[]", // default, think about whether or not to change this
       showProgressDetails: true,
       withCredentials: true,
-      // getResponseData(responseText, response) {
-      // 	console.log("Uppy response text:")
-      // 	console.log(responseText)
-      // 	console.log("Uppy response:")
-      // 	console.log(response)
-      // }
     });
 
   uppy.on("file-added", (file) => {
-    console.log("searching for matching files for: " + file.name);
     var matching_file_id = null;
     for (const file_id in reactive_file_list) {
-      // console.log(`evaluating ${reactive_file_list[file_id].name} vs ${file.name}`)
       if (reactive_file_list[file_id].name == file.name) {
         alert(
           "A file with this name already exists in the sample. If you upload this, it will be duplicated on the current item.",
@@ -71,21 +55,13 @@ export default function setupUppy(item_id, trigger_selector, reactive_file_list)
   });
 
   uppy.on("complete", function (result) {
-    console.log("Upload complete! We’ve uploaded these files:", result.successful);
-    console.log("There were errors with these files:", result.failed);
     result.successful.forEach(function (f) {
-      console.log("file upload response:");
-      console.log(f.response);
       var response_body = f.response.body;
-      store.commit("updateFiles", {
-        [response_body.file_id]: response_body.file_information,
-      });
-      console.log("Added file to store. Response_body:");
-      console.log(response_body);
       if (!response_body.is_update) {
         store.commit("addFileToSample", {
           item_id: item_id,
           file_id: response_body.file_id,
+          file_info: response_body.file_information,
         });
       }
     });
