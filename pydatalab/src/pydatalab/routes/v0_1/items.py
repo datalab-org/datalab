5d422be0f6db1fadc5cc1eb6439276f0a3561153
Matthew Evans <git@ml-evs.science>
2024-11-07 10:33:56 +0000

    Implement rudimentary ngram-based search with item updates and add tests

--- pydatalab/src/pydatalab/routes/v0_1/items.py
+++ pydatalab/src/pydatalab/routes/v0_1/items.py
@@ -16,7 +16,7 @@ from pydatalab.models.items import Item
 from pydatalab.models.people import Person
 from pydatalab.models.relationships import RelationshipType
 from pydatalab.models.utils import generate_unique_refcode
-from pydatalab.mongo import flask_mongo
+from pydatalab.mongo import ITEM_FTS_FIELDS, _generate_item_ngrams, flask_mongo
 from pydatalab.permissions import PUBLIC_USER_ID, active_users_or_get_only, get_default_permissions
 
 ITEMS = Blueprint("items", __name__)
@@ -304,23 +304,33 @@ def search_items_ngram():
         types = types.split(",")
 
     # split search string into trigrams
+    query = query.lower()
     if len(query) < 3:
         trigrams = [query]
-    trigrams = [query[i:i+3] for i in range(len(query)-2)]
+    trigrams = [query[i : i + 3] for i in range(len(query) - 2)]
 
     match_obj = {
-        "_fts_trigrams": {"$in": trigrams},
+        "_fts_ngrams": {"$in": trigrams},
         **get_default_permissions(user_only=False),
     }
 
     if types is not None:
         match_obj["type"] = {"$in": types}
 
-    cursor = flask_mongo.db.items.aggregate(
+    cursor = flask_mongo.db.items_fts.aggregate(
         [
             {"$match": match_obj},
-            {"$sort": {"score": {"$meta": "textScore"}}},
             {"$limit": nresults},
+            {
+                "$lookup": {
+                    "from": "items",
+                    "localField": "_id",
+                    "foreignField": "_id",
+                    "as": "items",
+                }
+            },
+            {"$unwind": "$items"},
+            {"$replaceRoot": {"newRoot": {"$mergeObjects": ["$items"]}}},
             {
                 "$project": {
                     "_id": 0,
@@ -397,7 +407,7 @@ def _create_sample(
             dict(
                 status="error",
                 messages=f"""Request to create item with generate_id_automatically = true is incompatible with the provided item data,
-                which has an item_id included (provided id: {sample_dict['item_id']}")""",
+                which has an item_id included (provided id: {sample_dict["item_id"]}")""",
             ),
             400,
         )
@@ -564,6 +574,16 @@ def _create_sample(
             ),
             400,
         )
+
+    # Update ngram index, if configured
+    ngrams = _generate_item_ngrams(
+        flask_mongo.db.items.find_one(result.inserted_id), ITEM_FTS_FIELDS
+    )
+    flask_mongo.db.items_fts.update_one(
+        {"_id": result.inserted_id},
+        {"$set": {"type": data_model.type, "_fts_ngrams": list(ngrams)}},
+        upsert=True,
+    )
 
     sample_list_entry = {
         "refcode": data_model.refcode,
@@ -759,11 +779,12 @@ def delete_sample():
     request_json = request.get_json()  # noqa: F821 pylint: disable=undefined-variable
     item_id = request_json["item_id"]
 
-    result = flask_mongo.db.items.delete_one(
-        {"item_id": item_id, **get_default_permissions(user_only=True, deleting=True)}
+    deleted_doc = flask_mongo.db.items.find_one_and_delete(
+        {"item_id": item_id, **get_default_permissions(user_only=True, deleting=True)},
+        projection={"_id": 1},
     )
 
-    if result.deleted_count != 1:
+    if deleted_doc is None:
         return (
             jsonify(
                 {
@@ -773,6 +794,10 @@ def delete_sample():
             ),
             401,
         )
+
+    # Update ngram index, if configured
+    flask_mongo.db.items_fts.delete_one({"_id": deleted_doc["_id"]})
+
     return (
         jsonify(
             {
@@ -1021,20 +1046,27 @@ def save_item():
     item.pop("collections")
     item.pop("creators")
 
-    result = flask_mongo.db.items.update_one(
+    updated_doc = flask_mongo.db.items.find_one_and_update(
         {"item_id": item_id},
         {"$set": item},
     )
 
-    if result.matched_count != 1:
+    if updated_doc is None:
         return (
             jsonify(
                 status="error",
                 message=f"{item_id} item update failed. no subdocument matched",
-                output=result.raw_result,
             ),
             400,
         )
+
+    # Update ngram index, if configured
+    ngrams = _generate_item_ngrams(updated_doc, ITEM_FTS_FIELDS)
+    flask_mongo.db.items_fts.update_one(
+        {"_id": updated_doc["_id"]},
+        {"$set": {"type": updated_doc["type"], "_fts_ngrams": list(ngrams)}},
+        upsert=True,
+    )
 
     return jsonify(status="success", last_modified=updated_data["last_modified"]), 200
 
--- pydatalab/tests/server/test_ngram_fts.py
+++ pydatalab/tests/server/test_ngram_fts.py
@@ -3,7 +3,7 @@ the n-gram FTS.
 
 """
 
-from pydatalab.mongo import _generate_item_ngrams, _generate_ngrams
+from pydatalab.mongo import _generate_item_ngrams, _generate_ngrams, create_ngram_item_index
 
 
 def test_ngram_single_field():
@@ -49,3 +49,57 @@ def test_ngram_single_field():
 def test_ngram_item():
     item = {"refcode": "ABCDEF"}
     assert _generate_item_ngrams(item, {"refcode"}, n=3) == {"abc": 1, "bcd": 1, "cde": 1, "def": 1}
+
+
+def test_ngram_fts_route(client, default_sample_dict, real_mongo_client, database):
+    default_sample_dict["item_id"] = "ABCDEF"
+    response = client.post("/new-sample/", json=default_sample_dict)
+    assert response.status_code == 201
+
+    # Check that creating the ngram index with existing items works
+    create_ngram_item_index(real_mongo_client, background=False, filter_top_ngrams=None)
+
+    doc = database.items_fts.find_one({})
+    ngrams = set(doc["_fts_ngrams"])
+    for ng in ["abc", "bcd", "cde", "def", "sam", "ple"]:
+        assert ng in ngrams
+    assert doc["type"] == "samples"
+
+    query_strings = ("ABC", "ABCDEF", "abcd", "cdef")
+
+    for q in query_strings:
+        response = client.get(f"/search-items-ngram/?query={q}&types=samples")
+        assert response.status_code == 200
+        assert response.json["status"] == "success"
+        assert len(response.json["items"]) == 1
+        assert response.json["items"][0]["item_id"] == "ABCDEF"
+
+    # Check that new items are added to the ngram index
+    default_sample_dict["item_id"] = "ABCDEF2"
+    response = client.post("/new-sample/", json=default_sample_dict)
+    assert response.status_code == 201
+
+    for q in query_strings:
+        response = client.get(f"/search-items-ngram/?query={q}&types=samples")
+        assert response.status_code == 200
+        assert response.json["status"] == "success"
+        assert len(response.json["items"]) == 2
+        assert response.json["items"][0]["item_id"] == "ABCDEF"
+        assert response.json["items"][1]["item_id"] == "ABCDEF2"
+
+    # Check that updates are reflected in the ngram index
+    # This test also makes sure that the string 'test' is not picked up from the refcode,
+    # which has an explicit carve out
+    default_sample_dict["description"] = "test string with punctuation"
+    update_req = {"item_id": "ABCDEF2", "data": default_sample_dict}
+    response = client.post("/save-item/", json=update_req)
+    assert response.status_code == 200
+
+    query_strings = ("test", "punctuation")
+
+    for q in query_strings:
+        response = client.get(f"/search-items-ngram/?query={q}&types=samples")
+        assert response.status_code == 200
+        assert response.json["status"] == "success"
+        assert len(response.json["items"]) == 1
+        assert response.json["items"][0]["item_id"] == "ABCDEF2"
