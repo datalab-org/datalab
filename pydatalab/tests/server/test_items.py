769bc6fea709e2696ceba1c2f9cc5abff39854c8
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2024-11-10 16:38:55 +0100

    Fix and refactor FTS field generation (#998)
    * Tag `immutable_id` as having JSONSchema string format "uuid"
    
    * Better extract and test relevant fields for FTS index
    
    * Resolve field names for FTS index without aliases

--- pydatalab/tests/server/test_items.py
+++ pydatalab/tests/server/test_items.py
@@ -18,3 +18,11 @@ def test_single_item_endpoints(client, inserted_default_items):
         assert response.json["status"] == "success"
         assert response.json["item_id"] == item.item_id
         assert response.json["item_data"]["item_id"] == item.item_id
+
+
+def test_fts_fields():
+    """Test non-exhaustively that certain fields make it into the fts index."""
+    from pydatalab.mongo import ITEMS_FTS_FIELDS
+
+    fields = ("item_id", "name", "description", "refcode", "synthesis_description", "supplier")
+    assert all(field in ITEMS_FTS_FIELDS for field in fields)
