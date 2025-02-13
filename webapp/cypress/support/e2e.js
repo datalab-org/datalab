e893628af23e80a9884f7dd1e9b727346afa0b86
Matthew Evans <git@ml-evs.science>
2025-02-13 13:56:11 +0000

    Make cypress support import more explicit

--- webapp/cypress/support/e2e.js
+++ webapp/cypress/support/e2e.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import "./commands";
+import "./commands.js";
 
 // eslint-disable-next-line no-unused-vars
 Cypress.on("uncaught:exception", (err, runnable) => {
