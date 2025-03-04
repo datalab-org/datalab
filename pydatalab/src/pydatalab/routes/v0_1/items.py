15b1cdd70d102bb33bd328425b05b291386406ab
Matthew Evans <git@ml-evs.science>
2025-03-04 17:31:40 +0000

    Run latest linter and update schemas

--- pydatalab/src/pydatalab/routes/v0_1/items.py
+++ pydatalab/src/pydatalab/routes/v0_1/items.py
@@ -15,7 +15,7 @@ from pydatalab.models import ITEM_MODELS
 from pydatalab.models.items import Item
 from pydatalab.models.people import Person
 from pydatalab.models.relationships import RelationshipType
-from pydatalab.models.utils import generate_unique_refcode, ItemStatus, EquipmentStatus
+from pydatalab.models.utils import EquipmentStatus, ItemStatus, generate_unique_refcode
 from pydatalab.mongo import ITEMS_FTS_FIELDS, flask_mongo
 from pydatalab.permissions import PUBLIC_USER_ID, active_users_or_get_only, get_default_permissions
 
@@ -1045,7 +1045,7 @@ def get_item_status_options():
     return jsonify(status_options)
 
 
-@ITEMS.route('/equipment_status_options', methods=['GET'])
+@ITEMS.route("/equipment_status_options", methods=["GET"])
 def get_equipment_status_options():
     status_options = [status.value for status in EquipmentStatus]
     return jsonify(status_options)
