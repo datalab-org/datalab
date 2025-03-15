af35b55bb4714c6f5c251dee01a2f1cfff7e813f
Ben Smith <42773657+be-smith@users.noreply.github.com>
2025-03-15 14:02:14 +0000

    added ftir block and associated tests (#1061)
    * added ftir block and associated tests
    
    * Fixed tests
    
    * fixed string formatting to double speechmarks not apostrophes
    
    * fixed tests
    
    * linting fixes
    
    * Changed functions to methods of FTIRBlock and improved tests

--- pydatalab/src/pydatalab/blocks/__init__.py
+++ pydatalab/src/pydatalab/blocks/__init__.py
@@ -4,6 +4,7 @@ from typing import Dict, Sequence, Type
 from pydatalab.apps.chat.blocks import ChatBlock
 from pydatalab.apps.echem import CycleBlock
 from pydatalab.apps.eis import EISBlock
+from pydatalab.apps.ftir import FTIRBlock
 from pydatalab.apps.nmr import NMRBlock
 from pydatalab.apps.raman import RamanBlock
 from pydatalab.apps.tga import MassSpecBlock
@@ -23,6 +24,7 @@ BLOCKS: Sequence[Type["DataBlock"]] = (
     ChatBlock,
     EISBlock,
     TabularDataBlock,
+    FTIRBlock,
 )
 
 BLOCK_TYPES: Dict[str, Type["DataBlock"]] = {block.blocktype: block for block in BLOCKS}
@@ -39,6 +41,7 @@ __all__ = (
     "RamanBlock",
     "MassSpecBlock",
     "TabularDataBlock",
+    "FTIRBlock",
     "BLOCK_TYPES",
     "BLOCKS",
 )
