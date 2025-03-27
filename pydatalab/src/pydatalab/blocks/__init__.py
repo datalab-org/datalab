2f7ffc9e0ac0947475c38d29bf41ac5341707563
Matthew Evans <git@ml-evs.science>
2025-03-27 15:59:09 +0100

    Add block plugin loading and test
    - Add datalab_app_plugin_insitu pinned to v0.1.1

--- pydatalab/src/pydatalab/blocks/__init__.py
+++ pydatalab/src/pydatalab/blocks/__init__.py
@@ -1,8 +1,3 @@
-from typing import Dict, Sequence, Type
-
-from datalab_app_plugin_insitu.blocks import InsituBlock
-
-# These app imports will be replaced by dynamic plugins in a future version
 from pydatalab.apps.chat.blocks import ChatBlock
 from pydatalab.apps.echem import CycleBlock
 from pydatalab.apps.eis import EISBlock
@@ -14,7 +9,7 @@ from pydatalab.apps.xrd import XRDBlock
 from pydatalab.blocks.base import DataBlock
 from pydatalab.blocks.common import CommentBlock, MediaBlock, NotSupportedBlock, TabularDataBlock
 
-BLOCKS: Sequence[Type["DataBlock"]] = (
+BLOCKS: list[type["DataBlock"]] = [
     CommentBlock,
     MediaBlock,
     XRDBlock,
@@ -27,9 +22,33 @@ BLOCKS: Sequence[Type["DataBlock"]] = (
     EISBlock,
     TabularDataBlock,
     FTIRBlock,
-)
+]
 
-BLOCK_TYPES: Dict[str, Type["DataBlock"]] = {block.blocktype: block for block in BLOCKS}
+BLOCK_TYPES: dict[str, type["DataBlock"]] = {block.blocktype: block for block in BLOCKS}
+
+
+def load_block_plugins():
+    """Search through any registered entrypoints at 'pydatalab.apps.plugins'
+    and load them as DataBlock subclasses.
+    """
+    import pkg_resources
+
+    block_plugins: dict[str, type[DataBlock]] = {}
+    for entry_point in pkg_resources.iter_entry_points("pydatalab.apps.plugins"):
+        block = entry_point.load()
+
+        if not issubclass(block, DataBlock):
+            raise ValueError(f"Plugin {block} must be a subclass of DataBlock")
+
+        if block.blocktype in BLOCK_TYPES:
+            raise ValueError(f"Block type {block.blocktype} is already defined")
+
+        BLOCK_TYPES[block.blocktype] = block
+        BLOCKS.append(block)
+        block_plugins[block.blocktype] = block
+
+    return block_plugins
+
 
 __all__ = (
     "CommentBlock",
@@ -46,4 +65,5 @@ __all__ = (
     "FTIRBlock",
     "BLOCK_TYPES",
     "BLOCKS",
+    "load_block_plugins",
 )
---
+++ pydatalab/tests/apps/test_plugins.py
@@ -0,0 +1,10 @@
+def test_load_plugins():
+    from datalab_app_plugin_insitu import InsituBlock
+
+    from pydatalab.blocks import BLOCK_TYPES, BLOCKS, load_block_plugins
+
+    plugins = load_block_plugins()
+    assert plugins["insitu-nmr"] == InsituBlock
+    assert isinstance(BLOCK_TYPES["insitu-nmr"], type)
+    assert BLOCK_TYPES["insitu-nmr"] == InsituBlock
+    assert InsituBlock in BLOCKS
