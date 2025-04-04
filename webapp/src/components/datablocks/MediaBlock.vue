29d0117f6fc109f1a04fb3497f2266bae744e70b
Josh Bocarsly <32345545+jdbocarsly@users.noreply.github.com>
2025-04-04 18:25:45 -0500

    Make media block resizable (#1099)
    * fix typo in MediaBlock so that images have a sensible max-height
    
    * make image size draggable by the user (will not persist)

--- webapp/src/components/datablocks/MediaBlock.vue
+++ webapp/src/components/datablocks/MediaBlock.vue
@@ -8,8 +8,15 @@
       class="mb-3"
       update-block-on-change
     />
-    <img v-if="isPhoto" data-testid="media-block-img" :src="media_url" class="img-fluid mx-auto" />
-    <video v-if="isVideo" :src="media_url" controls class="mx-auto" />
+    <div class="wrapper">
+      <img
+        v-if="isPhoto"
+        data-testid="media-block-img"
+        :src="media_url"
+        class="img-fluid mx-auto"
+      />
+      <video v-if="isVideo" :src="media_url" controls class="mx-auto" />
+    </div>
   </DataBlockBase>
 </template>
 
@@ -77,9 +84,17 @@ export default {
 </script>
 
 <style scoped>
-image,
+img,
 video {
-  display: block;
-  max-height: 600px;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
+.wrapper {
+  resize: both;
+  height: 600px;
+  overflow: auto;
+  display: inline-block;
 }
 </style>
