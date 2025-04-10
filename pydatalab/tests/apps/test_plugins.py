ee3119da2d32366f5a6bf3b4981506ee5c2c047f
Matthew Evans <git@ml-evs.science>
2025-04-10 15:43:29 +0100

    Fix import order in test so that it works more uniformly

--- pydatalab/tests/apps/test_plugins.py
+++ pydatalab/tests/apps/test_plugins.py
@@ -1,9 +1,9 @@
 def test_load_plugins():
-    from datalab_app_plugin_insitu import InsituBlock
-
     from pydatalab.blocks import BLOCK_TYPES, BLOCKS, load_block_plugins
 
     plugins = load_block_plugins()
+
+    from datalab_app_plugin_insitu import InsituBlock
 
     assert plugins["insitu-nmr"] == InsituBlock
     assert isinstance(BLOCK_TYPES["insitu-nmr"], type)
