821a53692f4fa753821ff64f52a2916c9a03b7ee
Matthew Evans <git@ml-evs.science>
2025-04-10 15:48:25 +0100

    Reorder import in plugin load test to avoid failures when test is run in isolation

--- pydatalab/tests/apps/test_plugins.py
+++ pydatalab/tests/apps/test_plugins.py
@@ -1,9 +1,11 @@
+# Load block types first to avoid circular dependency issues
+from pydatalab.blocks import BLOCK_TYPES, BLOCKS, load_block_plugins
+
+
 def test_load_plugins():
-    from pydatalab.blocks import BLOCK_TYPES, BLOCKS, load_block_plugins
+    from datalab_app_plugin_insitu import InsituBlock
 
     plugins = load_block_plugins()
-
-    from datalab_app_plugin_insitu import InsituBlock
 
     assert plugins["insitu-nmr"] == InsituBlock
     assert isinstance(BLOCK_TYPES["insitu-nmr"], type)
