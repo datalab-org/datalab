f7b09d0405c909fa886ba9b03ead5cbef76bed71
Benjamin CHARMES <benjamin.charmes@gmail.com>
2025-04-11 17:17:20 +0200

    Add dropdown to display/hide columns in Datatable

--- webapp/src/components/DynamicDataTable.vue
+++ webapp/src/components/DynamicDataTable.vue
@@ -37,7 +37,10 @@
           :filters="filters"
           :editable-inventory="editable_inventory"
           :show-buttons="showButtons"
+          :available-columns="availableColumns"
+          :selected-columns="selectedColumns"
           @update:filters="updateFilters"
+          @update:selected-columns="onToggleColumns"
           @open-create-item-modal="createItemModalIsOpen = true"
           @open-batch-create-item-modal="batchCreateItemModalIsOpen = true"
           @open-qr-scanner-modal="qrScannerModalIsOpen = true"
@@ -46,6 +49,7 @@
           @open-add-to-collection-modal="addToCollectionModalIsOpen = true"
           @delete-selected-items="deleteSelectedItems"
         />
+        <div></div>
       </template>
       <template #empty> No data found. </template>
       <template #loading> Loading data. Please wait. </template>
@@ -54,7 +58,7 @@
 
       <!-- <Column expander style="width: 5rem" /> -->
       <Column
-        v-for="column in columns"
+        v-for="column in selectedColumns"
         :key="column.field"
         :field="column.field"
         sortable
@@ -337,6 +341,7 @@ export default {
       },
       filteredData: [],
       allowedTypes: INVENTORY_TABLE_TYPES,
+      selectedColumns: [],
     };
   },
   computed: {
@@ -391,9 +396,13 @@ export default {
     isAllSelected() {
       return this.itemsSelected.length === this.data.length;
     },
+    availableColumns() {
+      return this.columns.map((col) => ({ ...col }));
+    },
   },
   created() {
     this.editable_inventory = EDITABLE_INVENTORY;
+    this.selectedColumns = [...this.availableColumns];
 
     FilterService.register("exactCollectionMatch", (value, filterValue) => {
       if (!filterValue || !value) return true;
@@ -656,6 +665,11 @@ export default {
       this.updatePage(event.page);
       this.updateRows(event.rows);
       this.allSelected = this.checkAllSelected();
+    },
+    onToggleColumns(value) {
+      this.$nextTick(() => {
+        this.selectedColumns = [...value];
+      });
     },
   },
 };
