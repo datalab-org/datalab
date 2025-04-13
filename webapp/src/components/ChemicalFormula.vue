b386ed46e624a5d7e3582628c262114b72ccd319
Matthew Evans <git@ml-evs.science>
2025-04-13 10:48:57 +0100

    Use exhaustive element list for regexp matching

--- webapp/src/components/ChemicalFormula.vue
+++ webapp/src/components/ChemicalFormula.vue
@@ -27,10 +27,14 @@ export default {
       if (!this.formula) {
         return this.formula;
       }
-      // For now, just regexp for simple formulas and subscript the numbers in them
-      const chemicalFormulaRegex = /([A-Z][a-z]{0,2})|([0-9.]+)/g;
+      // From an LLM, needs checking...
+      const elementSymbols =
+        "H|He|Li|Be|B|C|N|O|F|Ne|Na|Mg|Al|Si|P|S|Cl|Ar|K|Ca|Sc|Ti|V|Cr|Mn|Fe|Co|Ni|Cu|Zn|Ga|Ge|As|Se|Br|Kr|Rb|Sr|Y|Zr|Nb|Mo|Tc|Ru|Rh|Pd|Ag|Cd|In|Sn|Sb|Te|I|Xe|Cs|Ba|La|Ce|Pr|Nd|Pm|Sm|Eu|Gd|Tb|Dy|Ho|Er|Tm|Yb|Lu|Hf|Ta|W|Re|Os|Ir|Pt|Au|Hg|Tl|Pb|Bi|Po|At|Rn|Fr|Ra|Ac|Th|Pa|U|Np|Pu|Am|Cm|Bk|Cf|Es|Fm|Md|No|Lr|Rf|Db|Sg|Bh|Hs|Mt|Ds|Rg|Cn|Nh|Fl|Mc|Lv|Ts|Og";
+      // Create a regex that matches either element symbols or sequences of digits/periods
+      const chemicalFormulaRegexFull = new RegExp(`^(${elementSymbols})|(\\d+\\.?\\d*)+$`, "g");
+      const chemicalFormulaRegex = new RegExp(`(${elementSymbols})|(\\d+\\.?\\d*)`, "g");
 
-      if (this.formula.match(chemicalFormulaRegex)) {
+      if (this.formula.matchAll(chemicalFormulaRegexFull)) {
         return this.formula.replace(chemicalFormulaRegex, (match, element, number) => {
           if (element) {
             // If it's an element, return it unchanged
