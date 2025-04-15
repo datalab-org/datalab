dfc6e0cc15ffab3c3093ef4234716571540c0d82
Matthew Evans <git@ml-evs.science>
2025-04-15 19:15:23 +0100

    Expand NMR block tests

--- pydatalab/tests/apps/test_nmr.py
+++ pydatalab/tests/apps/test_nmr.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pytest
 
+from pydatalab.apps.nmr.blocks import NMRBlock
 from pydatalab.apps.nmr.utils import read_bruker_1d
 
 
@@ -13,21 +14,33 @@ def _extract_example(filename, dir):
 
 
 @pytest.fixture(scope="function")
-def nmr_1d_solution_example(tmpdir):
-    zip_path = Path(__file__).parent.parent.parent / "example_data" / "NMR" / "1.zip"
-    return _extract_example(zip_path, tmpdir)
+def nmr_1d_solution_path():
+    yield Path(__file__).parent.parent.parent / "example_data" / "NMR" / "1.zip"
 
 
 @pytest.fixture(scope="function")
-def nmr_1d_solid_example(tmpdir):
-    zip_path = Path(__file__).parent.parent.parent / "example_data" / "NMR" / "71.zip"
-    return _extract_example(zip_path, tmpdir)
+def nmr_1d_solution_example(tmpdir, nmr_1d_solution_path):
+    return _extract_example(nmr_1d_solution_path, tmpdir)
 
 
 @pytest.fixture(scope="function")
-def nmr_2d_matpass_example(tmpdir):
-    zip_path = Path(__file__).parent.parent.parent / "example_data" / "NMR" / "72.zip"
-    return _extract_example(zip_path, tmpdir)
+def nmr_1d_solid_path():
+    yield Path(__file__).parent.parent.parent / "example_data" / "NMR" / "71.zip"
+
+
+@pytest.fixture(scope="function")
+def nmr_1d_solid_example(tmpdir, nmr_1d_solid_path):
+    return _extract_example(nmr_1d_solid_path, tmpdir)
+
+
+@pytest.fixture(scope="function")
+def nmr_2d_matpass_path():
+    yield Path(__file__).parent.parent.parent / "example_data" / "NMR" / "72.zip"
+
+
+@pytest.fixture(scope="function")
+def nmr_2d_matpass_example(tmpdir, nmr_2d_matpass_path):
+    return _extract_example(nmr_2d_matpass_path, tmpdir)
 
 
 def test_bruker_reader_solution(nmr_1d_solution_example):
@@ -52,3 +65,28 @@ def test_bruker_reader_2D(nmr_2d_matpass_example):
     assert a_dic
     assert topspin_title
     assert shape == (8, 4096)
+
+
+def test_nmr_block(nmr_1d_solution_path, nmr_1d_solid_path, nmr_2d_matpass_path):
+    block = NMRBlock(item_id="nmr-block")
+    block.read_bruker_nmr_data(nmr_1d_solid_path)
+    assert block.data["metadata"]["topspin_title"].split("\n")[0] == "7Li 40 kHz 40 C hahn-echo"
+    block.generate_nmr_plot(parse=False)
+    plot = block.data["bokeh_plot_data"]
+    assert plot is not None
+
+    block = NMRBlock(item_id="nmr-block")
+    block.read_bruker_nmr_data(nmr_1d_solution_path)
+    assert block.data["metadata"]["topspin_title"].split("\n")[0] == "31P reference, 85% H3PO4"
+    block.generate_nmr_plot(parse=False)
+    plot = block.data["bokeh_plot_data"]
+    assert plot is not None
+
+    block = NMRBlock(item_id="nmr-block")
+    block.read_bruker_nmr_data(nmr_2d_matpass_path)
+    assert block.data["metadata"]["topspin_title"].split("\n")[0] == "7Li 40kHz 40 C MATPASS"
+    # catch warning about processed data
+    with pytest.warns(UserWarning, match="processed data"):
+        block.generate_nmr_plot(parse=False)
+        plot = block.data["bokeh_plot_data"]
+        assert plot is None  # cannot plot MATPASS yet
