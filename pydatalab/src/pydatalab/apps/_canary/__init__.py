0a9a738e1a89adc649585feecb0e45c42f3dfc79
Matthew Evans <git@ml-evs.science>
2025-05-16 13:24:57 +0100

    Refactor app block loading to prevent optional dependencies from breaking dev builds

---
+++ pydatalab/src/pydatalab/apps/_canary/__init__.py
@@ -0,0 +1,6 @@
+from pydatalab.blocks.base import DataBlock
+
+raise ImportError("This canary block is only used for internal testing of dynamic block loading.")
+
+
+class CanaryBlock(DataBlock): ...
