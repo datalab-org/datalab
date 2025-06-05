fae15d6f8852bb7c22540cbb11badb7e85741ae9
Benjamin CHARMES <benjamin.charmes@gmail.com>
2025-06-05 15:08:03 +0200

    Display NotImplementedBlock when an unknown block type is encountered

--- webapp/src/components/datablocks/NotImplementedBlock.vue
+++ webapp/src/components/datablocks/NotImplementedBlock.vue
@@ -1,8 +1,10 @@
 <template>
-  <DataBlockBase>
-    <div class="alert alert-danger">
-      This block type is not implemented/installed for this <i>datalab</i> instance. Please contact
-      your <i>datalab</i> administrator.
+  <DataBlockBase :item_id="item_id" :block_id="block_id">
+    <div class="alert alert-danger d-flex align-items-center ml-3">
+      <font-awesome-icon icon="exclamation-triangle" class="mr-2" />
+      This block type ({{ blockType }}) is not implemented/installed for
+      this&nbsp;<i>datalab</i>&nbsp;instance. Please contact
+      your&nbsp;<i>datalab</i>&nbsp;administrator.
     </div>
   </DataBlockBase>
 </template>
@@ -13,6 +15,24 @@ import DataBlockBase from "@/components/datablocks/DataBlockBase.vue";
 export default {
   components: {
     DataBlockBase,
+  },
+  props: {
+    item_id: {
+      type: String,
+      required: true,
+    },
+    block_id: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    block() {
+      return this.$store.state.all_item_data[this.item_id]["blocks_obj"][this.block_id];
+    },
+    blockType() {
+      return this.block?.blocktype;
+    },
   },
 };
 </script>
