afc2480956b379341f22db003d605dc7c786fa8a
Matthew Evans <git@ml-evs.science>
2025-06-25 21:19:57 +0100

    Bump the uppy ecosystem to v4 and fix styling (#1255)

--- webapp/src/file_upload.js
+++ webapp/src/file_upload.js
@@ -1,5 +1,6 @@
 import "@uppy/core/dist/style.css";
 import "@uppy/dashboard/dist/style.css";
+import "@uppy/webcam/dist/style.min.css";
 import Uppy from "@uppy/core";
 import Dashboard from "@uppy/dashboard";
 import XHRUpload from "@uppy/xhr-upload";
