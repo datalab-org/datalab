f6d65fa08015362d8714a26879eccde8278da0b8
Benjamin CHARMES <benjamin.charmes@gmail.com>
2025-07-09 16:10:53 +0200

    Review fix

--- webapp/src/components/datablocks/NMRInsituBlock.vue
+++ webapp/src/components/datablocks/NMRInsituBlock.vue
@@ -9,9 +9,6 @@
       @change="onFileChange"
     />
     <div v-show="file_id">
-      <div v-if="folderNameError" class="alert alert-danger mt-2 mx-auto">
-        {{ folderNameError }}
-      </div>
       <div class="form-inline">
         <div class="form-group mb-2">
           <label class="mr-2"><b>NMR folder name</b></label>
@@ -21,7 +18,7 @@
             :reduce="(folder) => folder"
             class="folder-select mr-2"
             placeholder="Select a folder"
-            @update:model-value="onFolderSelected"
+            @update:model-value="onFolderChanged"
           />
           <label class="mr-2"><b>Echem folder name</b></label>
           <v-select
@@ -30,48 +27,68 @@
             :reduce="(folder) => folder"
             class="folder-select"
             placeholder="Select a folder"
-            @update:model-value="onFolderSelected"
+            @update:model-value="onFolderChanged"
           />
         </div>
       </div>
-    </div>
-    <div v-show="nmr_folder_name && echem_folder_name" class="form-inline mt-2">
-      <div class="form-group mb-2">
-        <label class="mr-2"><b>Spectrum range:</b></label>
-        <input
-          v-model.number="start_exp"
-          type="number"
-          class="form-control mr-2"
-          placeholder="Start"
-          min="1"
-          :max="maxExperiments"
-          style="width: 80px"
-          @change="onParameterChange"
-        />
-        <span class="mr-2">to</span>
-        <input
-          v-model.number="end_exp"
-          type="number"
-          class="form-control mr-2"
-          placeholder="End"
-          min="1"
-          :max="maxExperiments"
-          style="width: 80px"
-          @change="onParameterChange"
-        />
-        <label class="mr-2 ml-3"><b>Step:</b></label>
-        <input
-          v-model.number="step_exp"
-          type="number"
-          class="form-control mr-2"
-          placeholder="1"
-          min="1"
-          style="width: 60px"
-          @change="onParameterChange"
-        />
-        <small class="text-muted">
-          ({{ selectedExperimentsCount }} of {{ maxExperiments }} experiments selected)
-        </small>
+
+      <div v-show="nmr_folder_name && echem_folder_name" class="form-inline mt-2">
+        <div class="form-group mb-2">
+          <label class="mr-2"><b>Start Exp:</b></label>
+          <input
+            v-model.number="local_start_exp"
+            type="number"
+            class="form-control mr-2"
+            style="width: 80px"
+            min="1"
+            :max="maxExperiments"
+            :placeholder="maxExperiments ? `1-${maxExperiments}` : '1'"
+            @input="onParameterChanged"
+          />
+
+          <label class="mr-2"><b>End Exp:</b></label>
+          <input
+            v-model.number="local_end_exp"
+            type="number"
+            class="form-control mr-2"
+            style="width: 80px"
+            min="1"
+            :max="maxExperiments"
+            :placeholder="maxExperiments ? `1-${maxExperiments}` : '1'"
+            @input="onParameterChanged"
+          />
+
+          <label class="mr-2"><b>Step:</b></label>
+          <input
+            v-model.number="local_step_exp"
+            type="number"
+            class="form-control mr-2"
+            style="width: 80px"
+            min="1"
+            @input="onParameterChanged"
+          />
+
+          <label class="mr-2"><b>Exclude:</b></label>
+          <input
+            v-model="local_exclude_exp"
+            type="text"
+            class="form-control mr-2"
+            placeholder="e.g., 1,3,5"
+            @input="onParameterChanged"
+          />
+
+          <button
+            class="btn btn-primary"
+            :disabled="!hasParameterChanges || isUpdating || validationError"
+            @click="applyChanges"
+          >
+            {{ isUpdating ? "Updating..." : "Apply" }}
+          </button>
+        </div>
+      </div>
+
+      <div v-if="folderNameError || validationError" class="alert alert-danger mt-2 mx-auto">
+        {{ folderNameError || validationError }}
       </div>
     </div>
     <div
@@ -118,6 +135,12 @@ export default {
     return {
       folderNameError: "",
       isUpdating: false,
+      hasParameterChanges: false,
+      originalParameters: {},
+      local_start_exp: 1,
+      local_end_exp: null,
+      local_step_exp: 1,
+      local_exclude_exp: "",
     };
   },
   computed: {
@@ -137,8 +160,29 @@ export default {
     availableFolders() {
       return this.currentBlock.available_folders || [];
     },
+    validationError() {
+      if (this.local_start_exp < 1) {
+        return "Start experiment must be >= 1";
+      }
+      if (this.maxExperiments && this.local_start_exp > this.maxExperiments) {
+        return `Start experiment must be <= ${this.maxExperiments}`;
+      }
+      if (this.local_end_exp && this.local_start_exp && this.local_end_exp < this.local_start_exp) {
+        return "End experiment must be >= start experiment";
+      }
+      if (this.maxExperiments && this.local_end_exp && this.local_end_exp > this.maxExperiments) {
+        return `End experiment must be <= ${this.maxExperiments}`;
+      }
+      if (this.local_step_exp < 1) {
+        return "Step must be >= 1";
+      }
+      return "";
+    },
     maxExperiments() {
-      return this.currentBlock.metadata?.num_experiments;
+      return this.currentBlock.max_experiments || null;
+    },
+    defaultEndExp() {
+      return this.end_exp || this.maxExperiments;
     },
     nmr_folder_name: createComputedSetterForBlockField("nmr_folder_name"),
     echem_folder_name: createComputedSetterForBlockField("echem_folder_name"),
@@ -147,6 +191,26 @@ export default {
     start_exp: createComputedSetterForBlockField("start_exp"),
     end_exp: createComputedSetterForBlockField("end_exp"),
     step_exp: createComputedSetterForBlockField("step_exp"),
+    exclude_exp: createComputedSetterForBlockField("exclude_exp"),
+  },
+  watch: {
+    defaultEndExp(newVal) {
+      if (newVal && !this.hasParameterChanges) {
+        this.local_end_exp = newVal;
+      }
+    },
+  },
+  mounted() {
+    this.originalParameters = {
+      start_exp: this.start_exp,
+      end_exp: this.end_exp,
+      step_exp: this.step_exp,
+      exclude_exp: this.exclude_exp,
+    };
+    this.local_start_exp = this.start_exp || 1;
+    this.local_end_exp = this.defaultEndExp;
+    this.local_step_exp = this.step_exp || 1;
+    this.local_exclude_exp = this.exclude_exp || "";
   },
   methods: {
     onFileChange() {
@@ -155,48 +219,28 @@ export default {
 
       this.updateBlock();
     },
-    onFolderSelected() {
+    onFolderChanged() {
       if (this.nmr_folder_name && this.echem_folder_name) {
-        if (this.start_exp && this.start_exp < 1) {
-          this.folderNameError = "Start experiment must be >= 1";
-          return;
-        }
-        if (this.end_exp && this.start_exp && this.end_exp < this.start_exp) {
-          this.folderNameError = "End experiment must be >= start experiment";
-          return;
-        }
-        if (this.step_exp && this.step_exp < 1) {
-          this.folderNameError = "Step must be >= 1";
-          return;
-        }
-
         this.folderNameError = "";
         this.updateBlock();
       } else if (this.nmr_folder_name || this.echem_folder_name) {
         this.folderNameError = "Both NMR and Echem folder names are required";
       }
     },
-    onParameterChange() {
-      if (this.nmr_folder_name && this.echem_folder_name) {
-        if (this.start_exp && this.start_exp < 1) {
-          this.folderNameError = "Start experiment must be >= 1";
-          return;
-        }
-        if (this.end_exp && this.start_exp && this.end_exp < this.start_exp) {
-          this.folderNameError = "End experiment must be >= start experiment";
-          return;
-        }
-        if (this.step_exp && this.step_exp < 1) {
-          this.folderNameError = "Step must be >= 1";
-          return;
-        }
-
-        this.folderNameError = "";
-        this.updateBlock();
-      }
+    onParameterChanged() {
+      this.hasParameterChanges = true;
+    },
+    applyChanges() {
+      this.folderNameError = "";
+      this.start_exp = this.local_start_exp;
+      this.end_exp = this.local_end_exp;
+      this.step_exp = this.local_step_exp;
+      this.exclude_exp = this.local_exclude_exp;
+      this.hasParameterChanges = false;
+      this.updateBlock();
     },
     updateBlock() {
-      this.isLoading = true;
+      this.isUpdating = true;
       const foundFile = this.all_files.find((file) => file.immutable_id === this.file_id);
       this.folder_name = foundFile?.name || "Folder not found";
 
@@ -205,10 +249,10 @@ export default {
       };
       updateBlockFromServer(this.item_id, this.block_id, blockToUpdate)
         .then(() => {
-          this.isLoading = false;
+          this.isUpdating = false;
         })
         .catch((error) => {
-          this.isLoading = false;
+          this.isUpdating = false;
           console.error("Error updating block:", error);
         });
     },
