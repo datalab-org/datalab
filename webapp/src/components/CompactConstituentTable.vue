5295ef56c89db26cad84a8cd7fba882dbd5d040f
Ben Charmes <107116804+BenjaminCharmes@users.noreply.github.com>
2025-07-16 00:27:49 +0200

    Fix item selection dropdown missing scrollbar in synthesis info (#1280)

--- webapp/src/components/CompactConstituentTable.vue
+++ webapp/src/components/CompactConstituentTable.vue
@@ -33,6 +33,7 @@
             :item-type="constituent.item.type"
             :name="constituent.item.name"
             :chemform="constituent.item.chemform || ''"
+            :max-length="25"
             enable-click
             enable-modified-click
             @dblclick="turnOnRowSelect(index)"
