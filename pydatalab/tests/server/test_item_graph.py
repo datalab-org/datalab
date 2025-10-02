07804f2a9793deb88c9f80ed1695ac22d7441361
Benjamin CHARMES <benjamin.charmes@gmail.com>
2025-10-02 11:19:56 +0200

    Prevent matching items with null relationship item_ids
    Re-add old comments
    
    Re-add old comments
    
    Fix test_graph.py with right numbers of nodes and edges for each test (for items and collections)
    
    Add test for item graph difference between admins and users (#1360)
    
    Re-add get_graph_cy_format modification
    
    Fix pytest
    
    Re-add user_only=False
    
    Re-add user_only=False

--- pydatalab/tests/server/test_item_graph.py
+++ pydatalab/tests/server/test_item_graph.py
@@ -8,12 +8,12 @@ import json
 from pydatalab.models import Sample, StartingMaterial
 
 
-def test_single_starting_material(admin_client):
+def test_single_starting_material(admin_client, client):
     item_id = "material"
 
     material = StartingMaterial(item_id=item_id)
 
-    creation = admin_client.post(
+    creation = client.post(
         "/new-sample/",
         json={"new_sample_data": json.loads(material.json())},
     )
@@ -21,11 +21,11 @@ def test_single_starting_material(admin_client):
     assert creation.status_code == 201
 
     # A single material without connections should be ignored
-    graph = admin_client.get("/item-graph").json
+    graph = client.get("/item-graph").json
     assert len(graph["nodes"]) == 0
 
     # Unless it is asked for directly
-    graph = admin_client.get(f"/item-graph/{item_id}").json
+    graph = client.get(f"/item-graph/{item_id}").json
     assert len(graph["nodes"]) == 1
 
     # Now make a sample and connect it to the starting material; check that the
@@ -37,13 +37,90 @@ def test_single_starting_material(admin_client):
         ],
     )
 
-    creation = admin_client.post(
+    creation = client.post(
         "/new-sample/",
         json={"new_sample_data": json.loads(parent.json())},
     )
 
     assert creation.status_code == 201
 
-    graph = admin_client.get("/item-graph").json
+    graph = client.get("/item-graph").json
+    assert len(graph["nodes"]) == 2
+    assert len(graph["edges"]) == 1
+
+    # From both the starting material and the sample
+    graph = client.get(f"/item-graph/{item_id}").json
+    assert len(graph["nodes"]) == 2
+    assert len(graph["edges"]) == 1
+
+    # From both the starting material and the sample
+    graph = client.get("/item-graph/parent").json
+    assert len(graph["nodes"]) == 2
+    assert len(graph["edges"]) == 1
+
+    # Now add a few more samples in a chain and check that only the relevant ones are shown
+    child = Sample(
+        item_id="child",
+        synthesis_constituents=[
+            {"item": {"item_id": "parent", "type": "samples"}, "quantity": None}
+        ],
+    )
+
+    creation = client.post(
+        "/new-sample/",
+        json={"new_sample_data": json.loads(child.json())},
+    )
+
+    grandchild = Sample(
+        item_id="grandchild",
+        synthesis_constituents=[
+            {"item": {"item_id": "child", "type": "samples"}, "quantity": None}
+        ],
+    )
+
+    creation = client.post(
+        "/new-sample/",
+        json={"new_sample_data": json.loads(grandchild.json())},
+    )
+
+    great_grandchild = Sample(
+        item_id="great-grandchild",
+        synthesis_constituents=[
+            {"item": {"item_id": "grandchild", "type": "samples"}, "quantity": None}
+        ],
+    )
+
+    creation = client.post(
+        "/new-sample/",
+        json={"new_sample_data": json.loads(great_grandchild.json())},
+    )
+
+    graph = client.get("/item-graph").json
+    assert len(graph["nodes"]) == 5
+    assert len(graph["edges"]) == 4
+
+    # Check for bug where this behaviour was inconsistent between admin and non-admin users
+    graph = admin_client.get("/item-graph/great-grandchild").json
+    assert len(graph["nodes"]) == 2
+    assert len(graph["edges"]) == 1
+
+    # Add an admin only item and check that the non-admin user still sees the same graph
+    admin_great_great_grandchild = Sample(
+        item_id="admin-great-great-grandchild",
+        synthesis_constituents=[
+            {"item": {"item_id": "great-grandchild", "type": "samples"}, "quantity": None}
+        ],
+    )
+
+    creation = admin_client.post(
+        "/new-sample/", json={"new_sample_data": json.loads(admin_great_great_grandchild.json())}
+    )
+
+    graph = admin_client.get("/item-graph/great-grandchild").json
+    assert len(graph["nodes"]) == 3
+    assert len(graph["edges"]) == 2
+
+    # Current broken behaviour: non-admin users see the full graph, but not the things they don't have permission for
+    graph = client.get("/item-graph/great-grandchild").json
     assert len(graph["nodes"]) == 2
     assert len(graph["edges"]) == 1
