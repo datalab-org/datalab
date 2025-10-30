40b914f44dcf7b0ee35daaea1317c34fd6b414c8
Benjamin CHARMES <benjamin.charmes@gmail.com>
2025-10-30 12:08:51 +0100

    Dropdown for sample only or related sample + col

--- webapp/src/components/SampleInformation.vue
+++ webapp/src/components/SampleInformation.vue
@@ -46,7 +46,28 @@
             <TinyMceInline v-model="SampleDescription" aria-labelledby="samp-description-label" />
           </div>
           <div class="col">
-            <ExportButton :item-id="item_id" />
+            <div class="dropdown">
+              <button
+                class="btn btn-sm btn-outline-primary dropdown-toggle"
+                type="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                @click="isExportDropdownVisible = !isExportDropdownVisible"
+              >
+                <i class="fa fa-download"></i> Export (as .ELN)
+              </button>
+              <div v-show="isExportDropdownVisible" class="dropdown-menu" style="display: block">
+                <a class="dropdown-item" @click="handleSampleExport">
+                  <i class="fa fa-file"></i> Export Sample Only
+                </a>
+                <a class="dropdown-item" @click="handleGraphExport">
+                  <i class="fa fa-project-diagram"></i> Export Related Samples
+                </a>
+              </div>
+            </div>
+            <ExportButton ref="exportButton" :item-id="item_id" style="display: none" />
+            <SampleGraphExportModal ref="graphExportModal" :item-id="item_id" />
           </div>
         </div>
       </div>
@@ -72,9 +93,11 @@ import SynthesisInformation from "@/components/SynthesisInformation";
 import TableOfContents from "@/components/TableOfContents";
 import ItemRelationshipVisualization from "@/components/ItemRelationshipVisualization";
 import ExportButton from "@/components/ExportButton";
+import SampleGraphExportModal from "@/components/SampleGraphExportModal";
 
 export default {
   components: {
+    SampleGraphExportModal,
     ExportButton,
     ChemFormulaInput,
     TinyMceInline,
@@ -99,6 +122,7 @@ export default {
         { title: "Table of Contents", targetID: "table-of-contents" },
         { title: "Synthesis Information", targetID: "synthesis-information" },
       ],
+      isExportDropdownVisible: false,
     };
   },
   computed: {
@@ -110,6 +134,16 @@ export default {
     DateCreated: createComputedSetterForItemField("date"),
     ItemCreators: createComputedSetterForItemField("creators"),
     Collections: createComputedSetterForItemField("collections"),
+  },
+  methods: {
+    handleSampleExport() {
+      this.isExportDropdownVisible = false;
+      this.$refs.exportButton.handleExport();
+    },
+    handleGraphExport() {
+      this.isExportDropdownVisible = false;
+      this.$refs.graphExportModal.show();
+    },
   },
 };
 </script>
