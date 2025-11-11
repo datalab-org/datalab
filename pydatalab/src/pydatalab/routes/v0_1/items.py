94581fe5cc60779e6cfb43f03ca3843c8193f290
BenSmithGreyGroup <ben.ed.smith@mac.com>
2025-11-11 09:18:19 +0000

    Removed user from list versions route

--- pydatalab/src/pydatalab/routes/v0_1/items.py
+++ pydatalab/src/pydatalab/routes/v0_1/items.py
@@ -1075,7 +1075,7 @@ def list_versions(refcode):
             {
                 "_id": 1,
                 "timestamp": 1,
-                "user": 1,
+                "user_id": 1,
                 "datalab_version": 1,
                 "version": 1,
                 "action": 1,
