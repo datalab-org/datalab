b76e1155fe40448851b6fad4253d0eed69cdcd44
Benjamin Charmes <BenjaminCharmes@users.noreply.github.com>
2025-11-11 01:14:10 +0000

    Update various blocks for pydantic v2
    Chatblock patch; set default values in block
    
    More updates for blocks

--- pydatalab/src/pydatalab/blocks/base.py
+++ pydatalab/src/pydatalab/blocks/base.py
@@ -209,7 +209,7 @@ class DataBlock:
             for (f, s) in self.block_db_model.schema()["properties"].items()
             if s.get("datalab_exclude_from_db")
         }
-        return self.block_db_model(**self.data).dict(
+        return self.block_db_model(**self.data).model_dump(
             exclude=exclude_fields,
             exclude_unset=True,
             exclude_none=True,
@@ -261,7 +261,7 @@ class DataBlock:
         else:
             self.data.pop("warnings", None)
 
-        return self.block_db_model(**self.data).dict(exclude_unset=True, exclude_none=True)
+        return self.block_db_model(**self.data).model_dump(exclude_unset=True, exclude_none=True)
 
     def process_events(self, events: list[dict] | dict):
         """Handle any supported events passed to the block."""
@@ -357,5 +357,5 @@ class DataBlock:
             if s.get("datalab_exclude_from_load")
         }
         [data.pop(f, None) for f in exclude_fields]
-        self.data.update(self.block_db_model(**data).dict())
+        self.data.update(self.block_db_model(**data).model_dump(exclude_unset=True))
         return self
