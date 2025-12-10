94070c0dee79e5e675fcdd012a4424d82f1f829d
Matthew Evans <git@ml-evs.science>
2025-12-10 18:46:45 +0000

    Represent status with dot in tables but not in select view

--- webapp/src/components/FormattedItemStatus.vue
+++ webapp/src/components/FormattedItemStatus.vue
@@ -1,5 +1,10 @@
 <template>
-  <span v-if="alt" class="dot" :title="status" :class="'badge-' + badgeClass"></span>
+  <span
+    v-if="dotOnly"
+    class="dot"
+    :title="status.toUpperCase()"
+    :class="'badge-' + badgeClass"
+  ></span>
   <span v-else class="badge badge-pill text-uppercase" :class="'badge-' + badgeClass">
     {{ status }}
   </span>
@@ -8,8 +13,8 @@
 <script>
 export default {
   props: {
-    status: { type: String, required: false },
-    alt: { type: Boolean, default: false },
+    dotOnly: { type: Boolean, default: true },
+    status: { type: String, required: false, default: "" },
   },
   computed: {
     badgeClass() {
@@ -56,5 +61,81 @@ export default {
   display: inline-block;
   margin-left: auto;
   margin-right: auto;
+  position: relative;
+}
+
+.dot.badge-success {
+  border-color: #28a745;
+  background-color: transparent !important;
+  box-shadow:
+    0 0 2px #28a745,
+    0 0 4px #28a745,
+    0 0 6px #28a745;
+}
+
+.dot.badge-danger {
+  border-color: #dc3545;
+  background: repeating-linear-gradient(
+    45deg,
+    transparent,
+    transparent 1.5px,
+    #dc3545 1.5px,
+    #dc3545 2px
+  ) !important;
+}
+
+.dot.badge-warning {
+  border-color: #ffc107;
+  background: repeating-linear-gradient(
+    45deg,
+    transparent,
+    transparent 1.5px,
+    #ffc107 1.5px,
+    #ffc107 2px
+  ) !important;
+}
+
+.dot.badge-primary {
+  border-color: #007bff;
+  background: repeating-linear-gradient(
+    45deg,
+    transparent,
+    transparent 1.5px,
+    #007bff 1.5px,
+    #007bff 2px
+  ) !important;
+}
+
+.dot.badge-secondary {
+  border-color: #6c757d;
+  background: repeating-linear-gradient(
+    45deg,
+    transparent,
+    transparent 1.5px,
+    #6c757d 1.5px,
+    #6c757d 2px
+  ) !important;
+}
+
+.dot.badge-info {
+  border-color: #17a2b8;
+  background: repeating-linear-gradient(
+    45deg,
+    transparent,
+    transparent 1.5px,
+    #17a2b8 1.5px,
+    #17a2b8 2px
+  ) !important;
+}
+
+.dot.badge-dark {
+  border-color: #343a40;
+  background: repeating-linear-gradient(
+    45deg,
+    transparent,
+    transparent 1.5px,
+    #343a40 1.5px,
+    #343a40 2px
+  ) !important;
 }
 </style>
