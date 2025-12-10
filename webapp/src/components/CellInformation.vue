60ee4274755cdf888b51cff76ea4a04eed17f1dc
Benjamin Charmes <BenjaminCharmes@users.noreply.github.com>
2025-12-10 18:37:12 +0000

    Add status field to edit pages for each item type

--- webapp/src/components/CellInformation.vue
+++ webapp/src/components/CellInformation.vue
@@ -28,8 +28,14 @@
           <div class="form-group col-md-3 col-sm-3 col-6 pb-3">
             <ToggleableCreatorsFormGroup v-model="ItemCreators" :refcode="Refcode" />
           </div>
-          <div class="col-md-6 col-sm-7 pr-2">
+          <div class="form-group col-md-3 col-sm-7 pr-2">
             <ToggleableCollectionFormGroup v-model="Collections" />
+          </div>
+          <div class="form-group col-md-3 col-sm-7 pr-2">
+            <ToggleableItemStatusFormGroup
+              v-model="Status"
+              :possible-item-statuses="possibleItemStatuses"
+            />
           </div>
         </div>
         <div class="form-row">
@@ -105,6 +111,7 @@
 
 <script>
 import { createComputedSetterForItemField } from "@/field_utils.js";
+
 import ChemFormulaInput from "@/components/ChemFormulaInput";
 import TiptapInline from "@/components/TiptapInline";
 import CellPreparationInformation from "@/components/CellPreparationInformation";
@@ -113,6 +120,7 @@ import ItemRelationshipVisualization from "@/components/ItemRelationshipVisualiz
 import FormattedRefcode from "@/components/FormattedRefcode";
 import ToggleableCollectionFormGroup from "@/components/ToggleableCollectionFormGroup";
 import ToggleableCreatorsFormGroup from "@/components/ToggleableCreatorsFormGroup";
+import ToggleableItemStatusFormGroup from "@/components/ToggleableItemStatusFormGroup";
 import { cellFormats } from "@/resources.js";
 
 export default {
@@ -125,6 +133,7 @@ export default {
     FormattedRefcode,
     ToggleableCollectionFormGroup,
     ToggleableCreatorsFormGroup,
+    ToggleableItemStatusFormGroup,
   },
   props: {
     item_id: {
@@ -143,6 +152,9 @@ export default {
     };
   },
   computed: {
+    item() {
+      return this.$store.state.all_item_data[this.item_id];
+    },
     Refcode: createComputedSetterForItemField("refcode"),
     ItemID: createComputedSetterForItemField("item_id"),
     SampleDescription: createComputedSetterForItemField("description"),
@@ -155,6 +167,13 @@ export default {
     CellFormatDescription: createComputedSetterForItemField("cell_format_description"),
     CharacteristicMass: createComputedSetterForItemField("characteristic_mass"),
     Collections: createComputedSetterForItemField("collections"),
+    Status: createComputedSetterForItemField("status"),
+    schema() {
+      return this.$store.state.schemas[this.item?.type];
+    },
+    possibleItemStatuses() {
+      return this.schema?.attributes?.schema?.definitions?.CellStatus?.enum;
+    },
   },
 };
 </script>
