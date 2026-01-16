3be8a3f7632293e28dfed3167367e1fe57ee67c6
Matthew Evans <git@ml-evs.science>
2026-01-16 13:11:30 +0000

    More progress vibing a collapsible substance model

--- webapp/src/components/SubstanceInformation.vue
+++ webapp/src/components/SubstanceInformation.vue
@@ -1,21 +1,70 @@
 <template>
-  <div id="substance-information" class="data-block" data-testid="substance-block">
-    <div class="datablock-header collapsible" :class="{ expanded: isExpanded }">
-      <font-awesome-icon
-        :icon="['fas', 'chevron-right']"
-        fixed-width
-        class="collapse-arrow"
-        data-testid="collapse-arrow"
-        @click="toggleExpandBlock"
-      />
-      <label class="block-title">Substance Information</label>
-    </div>
+  <div id="substance-information" ref="outerdiv" class="data-block" data-testid="substance-block">
     <div
-      ref="contentContainer"
-      class="content-container"
-      :style="{ 'max-height': contentMaxHeight }"
+      class="datablock-header"
+      :class="{ clickable: !isEditing }"
+      @click="!isEditing && enterEditMode()"
     >
-      <div id="substance-information">
+      <font-awesome-icon :icon="['fas', 'flask']" fixed-width class="header-icon" />
+      <label class="block-title">Substance Information</label>
+      <font-awesome-icon
+        v-if="!isEditing"
+        id="edit-icon"
+        class="ml-auto mr-2"
+        icon="pen"
+        size="xs"
+      />
+    </div>
+
+    <!-- COMPACT DISPLAY MODE -->
+    <div v-if="!isEditing" class="substance-card" @click="enterEditMode">
+      <div v-if="hasAnyData" class="substance-card-content">
+        <div class="substance-display-row">
+          <!-- Molecule structure from InChIKey/InChI/SMILES -->
+          <div v-if="hasStructureIdentifier && !moleculeImageError" class="molecule-display">
+            <img
+              :src="pubchemImageUrl"
+              alt="Molecule structure"
+              class="molecule-image-compact"
+              @error="moleculeImageError = true"
+            />
+          </div>
+
+          <!-- Text info items -->
+          <div class="info-items">
+            <!-- Chemical formula -->
+            <div v-if="chemform" class="info-item">
+              <span class="display-label">Formula</span>
+              <ChemicalFormula :formula="chemform" class="formula-value" />
+            </div>
+
+            <!-- Molar mass -->
+            <div v-if="molar_mass" class="info-item">
+              <span class="display-label">Molar mass</span>
+              <span class="info-value">{{ molar_mass }} g/mol</span>
+            </div>
+
+            <!-- GHS Hazard pictograms (inline with other info) -->
+            <div v-if="GHS_codes && hasPictograms" class="info-item ghs-item">
+              <span class="display-label">Hazards</span>
+              <GHSHazardPictograms :model-value="GHS_codes" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Empty state placeholder -->
+      <div v-else class="substance-empty-state">
+        <span class="text-muted">
+          <font-awesome-icon :icon="['fas', 'plus-circle']" class="mr-1" />
+          Click to add substance information
+        </span>
+      </div>
+    </div>
+
+    <!-- EDIT MODE -->
+    <OnClickOutside v-if="isEditing" :options="{ ignore: [outerDivRef] }" @trigger="exitEditMode">
+      <div class="substance-edit-form">
         <div class="form-row">
           <div class="form-group col-sm-4 col-6 pr-2">
             <label for="substance-chemform">Chemical formula</label>
@@ -25,6 +74,19 @@
             <label for="substance-smiles">SMILES</label>
             <input id="substance-smiles" v-model="smiles" class="form-control" type="text" />
           </div>
+          <div v-if="hasStructureIdentifier" class="form-group col-sm-4 col-6 pr-2">
+            <label>Structure</label>
+            <div class="molecule-image-container">
+              <img
+                v-if="!moleculeImageError"
+                :src="pubchemImageUrl"
+                alt="Molecule structure"
+                class="molecule-image"
+                @error="moleculeImageError = true"
+              />
+              <span v-else class="molecule-image-error">Unable to load structure image</span>
+            </div>
+          </div>
         </div>
         <div class="form-row">
           <div class="form-group col-sm-4 col-6 pr-2">
@@ -32,8 +94,8 @@
             <input id="substance-inchi" v-model="inchi" class="form-control" type="text" />
           </div>
           <div class="form-group col-sm-4 col-6 pr-2">
-            <label for="substance-inchi">InChI Key</label>
-            <input id="substance-inchi" v-model="inchi_key" class="form-control" type="text" />
+            <label for="substance-inchi-key">InChI Key</label>
+            <input id="substance-inchi-key" v-model="inchi_key" class="form-control" type="text" />
           </div>
           <div class="form-group col-sm-4 col-6 pr-2">
             <label for="substance-mass">Molar mass / molecular weight</label>
@@ -41,35 +103,50 @@
           </div>
         </div>
         <div class="form-row">
-          <div class="form-row col-sm-4 col-6 pr-2">
+          <div class="form-group col-sm-6 col-12 pr-2">
             <label for="substance-ghs">GHS codes</label>
-            <GHSHazardInformationid v-model="GHS_codes" />
+            <GHSHazardInformation id="substance-ghs" v-model="GHS_codes" :editable="true" />
+          </div>
+        </div>
+
+        <!-- Done button -->
+        <div class="form-row">
+          <div class="col-12 text-right">
+            <button class="btn btn-sm btn-outline-secondary" @click.stop="exitEditMode">
+              Done
+            </button>
           </div>
         </div>
       </div>
-    </div>
+    </OnClickOutside>
   </div>
 </template>
 
 <script>
 import { createComputedSetterForItemField } from "@/field_utils.js";
-import { GHSHazardInformation } from "@/components/GHSHazardInformation.vue";
+import GHSHazardInformation from "@/components/GHSHazardInformation.vue";
+import GHSHazardPictograms from "@/components/GHSHazardPictograms.vue";
 import ChemFormulaInput from "@/components/ChemFormulaInput";
+import ChemicalFormula from "@/components/ChemicalFormula";
+import { OnClickOutside } from "@vueuse/components";
+import { getPictogramsFromHazardInformation } from "@/resources.js";
 
 export default {
   components: {
     ChemFormulaInput,
+    ChemicalFormula,
     GHSHazardInformation,
+    GHSHazardPictograms,
+    OnClickOutside,
   },
   props: {
     item_id: { type: String, required: true },
   },
   data() {
     return {
-      // isExpanded is used to toggle the visibility of the content-container starts as false then will expand when clicked or if it is filled
-      isExpanded: false,
-      contentMaxHeight: "0px", // "none", Start collapsed so 0px, if expanded set to none in mounted
-      padding_height: 18,
+      isEditing: false,
+      outerDivRef: null,
+      moleculeImageError: false,
     };
   },
   computed: {
@@ -79,55 +156,64 @@ export default {
     inchi_key: createComputedSetterForItemField("inchi_key"),
     GHS_codes: createComputedSetterForItemField("GHS_codes"),
     molar_mass: createComputedSetterForItemField("molar_mass"),
+
+    hasStructureIdentifier() {
+      return !!(this.inchi_key || this.inchi || this.smiles);
+    },
+
+    pubchemImageUrl() {
+      // Prefer InChIKey > InChI > SMILES (InChI/InChIKey are more robust)
+      if (this.inchi_key) {
+        return `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/${encodeURIComponent(this.inchi_key)}/PNG?image_size=200x200`;
+      }
+      if (this.inchi) {
+        // InChI contains "/" characters, so must be passed as query parameter
+        return `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchi/PNG?inchi=${encodeURIComponent(this.inchi)}&image_size=200x200`;
+      }
+      if (this.smiles) {
+        return `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/${encodeURIComponent(this.smiles)}/PNG?image_size=200x200`;
+      }
+      return null;
+    },
+
+    hasAnyData() {
+      return !!(
+        this.chemform ||
+        this.smiles ||
+        this.inchi ||
+        this.inchi_key ||
+        this.GHS_codes ||
+        this.molar_mass
+      );
+    },
+
+    hasPictograms() {
+      if (!this.GHS_codes) return false;
+      const pictograms = getPictogramsFromHazardInformation(this.GHS_codes);
+      return pictograms.size > 0;
+    },
   },
   watch: {
-    // Added initialization check to prevent firing on mount - this seemed to trigger an unsaved check when loading the sample for the second time
-    constituents: {
-      handler() {
-        this.$store.commit("setItemSaved", { item_id: this.item_id, isSaved: false });
-      },
-      deep: true,
+    // Reset error state when any structure identifier changes
+    inchi_key() {
+      this.moleculeImageError = false;
+    },
+    inchi() {
+      this.moleculeImageError = false;
+    },
+    smiles() {
+      this.moleculeImageError = false;
     },
   },
   mounted() {
-    var content = this.$refs.contentContainer;
-
-    // Auto-collapsed when initialised empty
-    this.isExpanded = true;
-
-    // If expanded set height to none, otherwise set to 0px
-    if (this.isExpanded) {
-      this.contentMaxHeight = "none";
-      content.style.overflow = "visible";
-    } else {
-      this.contentMaxHeight = "0px";
-    }
-
-    content.addEventListener("transitionend", () => {
-      if (this.isExpanded) {
-        this.contentMaxHeight = "none";
-      }
-    });
+    this.outerDivRef = this.$refs.outerdiv;
   },
   methods: {
-    toggleExpandBlock() {
-      var content = this.$refs.contentContainer;
-      console.log(this.contentMaxHeight);
-      if (!this.isExpanded) {
-        this.contentMaxHeight = content.scrollHeight + 2 * this.padding_height + "px";
-        this.isExpanded = true;
-        content.style.overflow = "visible";
-      } else {
-        content.style.overflow = "hidden";
-        requestAnimationFrame(() => {
-          //must be an arrow function so that 'this' is still accessible!
-          this.contentMaxHeight = content.scrollHeight + "px";
-          requestAnimationFrame(() => {
-            this.contentMaxHeight = "0px";
-            this.isExpanded = false;
-          });
-        });
-      }
+    enterEditMode() {
+      this.isEditing = true;
+    },
+    exitEditMode() {
+      this.isEditing = false;
     },
   },
 };
@@ -146,58 +232,154 @@ export default {
   margin: auto;
 }
 
-.collapsible {
-  /* background-color: #eee; */
-  color: white;
-  /* color: #444; */
-  /*cursor: pointer;*/
-  /*padding: 6px;*/
-  width: 100%;
-  /* border: 1px solid #ccc; */
-  text-align: left;
-  outline: none;
-  /* border-radius: 3px; */
+.datablock-header.clickable {
+  cursor: pointer;
+}
+
+.datablock-header.clickable:hover {
+  background-color: #f8f9fa;
+}
+
+.header-icon {
+  color: #004175;
+  margin-right: 0.5rem;
 }
 
 .block-title {
   display: flex;
   align-items: center;
-  margin-left: 1em;
   font-size: large;
   font-weight: 500;
   margin: auto 0;
 }
 
-.collapse-arrow {
-  font-size: large;
-  margin-left: 10px;
-  margin-right: 10px;
-  color: #004175;
-  transition: all 0.4s;
+#edit-icon {
+  color: grey;
 }
 
-.collapse-arrow:hover {
-  color: #7ca7ca;
+/* Compact Card Display Styles */
+.substance-card {
+  border: 1px solid #dee2e6;
+  border-radius: 0.375rem;
+  padding: 0.75rem 1rem;
+  background-color: #fff;
+  cursor: pointer;
+  transition: all 0.2s ease;
 }
 
-/* expanded is on the parent (the header) */
-.expanded .collapse-arrow {
-  -webkit-transform: rotate(90deg);
-  -moz-transform: rotate(90deg);
-  transform: rotate(90deg);
+.substance-card:hover {
+  border-color: #adb5bd;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
 }
 
-.subheading {
-  color: darkslategrey;
-  font-size: small;
-  font-weight: 600;
+.substance-card-content {
+  display: flex;
+  flex-direction: column;
+}
+
+.substance-display-row {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 1rem;
+}
+
+.molecule-display {
+  flex-shrink: 0;
+}
+
+.molecule-image-compact {
+  width: 100px;
+  height: 100px;
+  object-fit: contain;
+  background-color: white;
+  border: 1px solid #e9ecef;
+  border-radius: 0.25rem;
+  padding: 0.25rem;
+}
+
+.info-items {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  gap: 1.5rem;
+  flex: 1;
+}
+
+.info-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.ghs-item {
+  flex-basis: 100%;
+}
+
+.ghs-item :deep(.ghs-display) {
+  margin: 0;
+}
+
+/* Smaller GHS icons for compact view */
+.ghs-item :deep(.ghs-icon) {
+  width: 2.5rem;
+  height: 2.5rem;
+  margin-right: 4px;
+}
+
+.display-label {
+  font-size: 0.75rem;
+  color: #6c757d;
   text-transform: uppercase;
-  margin-bottom: 0px;
+  letter-spacing: 0.05em;
+  margin-bottom: 0.125rem;
 }
 
-.content-container {
-  overflow: hidden;
-  max-height: none;
-  transition: max-height 0.4s ease-in-out;
+.formula-value {
+  font-size: 1.25rem;
+  font-weight: 500;
+}
+
+.info-value {
+  font-size: 1rem;
+}
+
+.substance-empty-state {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  color: #6c757d;
+}
+
+/* Edit Form Styles */
+.substance-edit-form {
+  border: 1px solid #dee2e6;
+  border-radius: 0.375rem;
+  padding: 1rem;
+  background-color: #fff;
+}
+
+.molecule-image-container {
+  background-color: white;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100px;
+}
+
+.molecule-image {
+  max-width: 100%;
+  max-height: 200px;
+  object-fit: contain;
+}
+
+.molecule-image-error {
+  color: #6c757d;
+  font-size: 0.875rem;
+  font-style: italic;
 }
 </style>
