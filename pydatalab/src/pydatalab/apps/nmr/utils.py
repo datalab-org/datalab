90e3b7e991d8f124358a1bbaf4642a3fa6ca79f5
Matthew Evans <git@ml-evs.science>
2026-02-13 18:43:47 +0000

    Add metadata model for NMR block and adjust some unit conversions

--- pydatalab/src/pydatalab/apps/nmr/utils.py
+++ pydatalab/src/pydatalab/apps/nmr/utils.py
@@ -67,6 +67,7 @@ def read_bruker_1d(
             warnings.warn(
                 "No frequency-domain data found in Bruker project. Attempting best guess at processing time-domain data with FFT and ACME autophase."
             )
+            p_data = ng.bruker.remove_digital_filter(a_data, a_dic)
             p_data = ng.process.proc_base.fft(a_data)
 
         p_data = ng.process.proc_base.rev(p_data)
