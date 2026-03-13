c19e1aa469ed8bd582f1a7bf5cb1b030594c97fe
BenSmithGreyGroup <ben.ed.smith@mac.com>
2026-03-13 16:23:07 +0000

    Don't expose internal functions in init.py

--- pydatalab/src/pydatalab/apps/nexus/__init__.py
+++ pydatalab/src/pydatalab/apps/nexus/__init__.py
@@ -7,21 +7,11 @@ which are commonly used in neutron, X-ray, and muon science.
 from .utils import (
     ColumnMapping,
     NeXusValidationError,
-    NeXusValidator,
-    extract_nexus_metadata,
-    extract_plottable_data,
-    find_all_nxdata_groups,
-    list_nexus_data_groups,
     load_nexus_file,
 )
 
 __all__ = (
     "load_nexus_file",
-    "list_nexus_data_groups",
-    "find_all_nxdata_groups",
-    "extract_plottable_data",
-    "extract_nexus_metadata",
-    "NeXusValidator",
     "ColumnMapping",
     "NeXusValidationError",
 )
