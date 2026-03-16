f9e200973810d28bd89e46c9938df830a753d0a8
Matthew Evans <git@ml-evs.science>
2026-03-16 14:30:24 +0000

    Add scaffolding to create custom about pages

---
+++ webapp/src/components/CustomAbout.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- Deployment-specific about page content. Override this component by
+       placing a CustomAbout.vue in public/custom/ which will be copied
+       into src/components/ at build time. -->
+</template>
+
+<script>
+export default {
+  name: "CustomAbout",
+  hasContent: false,
+};
+</script>
