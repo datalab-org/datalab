058f557b4310229c22ad3876ef2041a9c749f4b6
BenSmithGreyGroup <ben.ed.smith@mac.com>
2026-04-22 13:14:57 +0100

    refactored block a bit

--- pydatalab/src/pydatalab/apps/eis/__init__.py
+++ pydatalab/src/pydatalab/apps/eis/__init__.py
@@ -64,6 +64,17 @@ def parse_pstrace_eis_txt(filename: Path):
     return eis
 
 
+def parse_biologic_mpr(filename: Path):
+    mpr_file = MPRfile(str(filename))
+    return pd.DataFrame(
+        {
+            "Frequency [Hz]": mpr_file.data["freq/Hz"],
+            "Re(Z) [Ω]": mpr_file.data["Re(Z)/Ohm"],
+            "-Im(Z) [Ω]": mpr_file.data["-Im(Z)/Ohm"],
+        }
+    )
+
+
 class EISBlock(DataBlock):
     accepted_file_extensions = (".txt", ".mpr")
     blocktype = "eis"
@@ -81,66 +92,52 @@ This block can plot electrochemical impedance spectroscopy (EIS) data from:
         return (self.generate_eis_plot,)
 
     def generate_eis_plot(self):
-        file_info = None
-        # all_files = None
-        eis_data = None
-
         if "file_id" not in self.data:
             LOGGER.warning("No file set in the DataBlock")
             return
-        else:
-            file_info = get_file_info_by_id(self.data["file_id"], update_if_live=True)
-            ext = os.path.splitext(file_info["location"].split("/")[-1])[-1].lower()
-            if ext not in self.accepted_file_extensions:
-                LOGGER.warning(
-                    "Unsupported file extension (must be one of %s, not %s)",
-                    self.accepted_file_extensions,
-                    ext,
-                )
-                return
 
-            errors = []
-            eis_data = None
+        file_info = get_file_info_by_id(self.data["file_id"], update_if_live=True)
+        ext = os.path.splitext(file_info["location"].split("/")[-1])[-1].lower()
+        if ext not in self.accepted_file_extensions:
+            LOGGER.warning(
+                "Unsupported file extension (must be one of %s, not %s)",
+                self.accepted_file_extensions,
+                ext,
+            )
+            return
 
-            if ext == ".mpr":
+        errors = []
+        eis_data = None
+
+        if ext == ".mpr":
+            try:
+                eis_data = parse_biologic_mpr(Path(file_info["location"]))
+            except RuntimeError as exc:
+                errors = [exc]
+        elif ext == ".txt":
+            for parser in (parse_ivium_eis_txt, parse_pstrace_eis_txt):
                 try:
-                    mpr_file = MPRfile(file_info["location"])
-                    eis_data = pd.DataFrame(
-                        {
-                            "Frequency [Hz]": mpr_file.data["freq/Hz"],
-                            "Re(Z) [Ω]": mpr_file.data["Re(Z)/Ohm"],
-                            "-Im(Z) [Ω]": mpr_file.data["-Im(Z)/Ohm"],
-                        }
-                    )
-
-                except RuntimeError as exc:
-                    errors = [exc]
-            elif ext == ".txt":
-                try:
-                    eis_data = parse_ivium_eis_txt(Path(file_info["location"]))
-                except RuntimeError as exc:
-                    errors = [exc]
-
-                try:
-                    eis_data = parse_pstrace_eis_txt(Path(file_info["location"]))
+                    eis_data = parser(Path(file_info["location"]))
+                    break
                 except RuntimeError as exc:
                     errors.append(exc)
 
-            if eis_data is None:
-                raise RuntimeError(
-                    f"Could not parse EIS data from uploaded file with implemented parsers. Errors: {errors}"
-                )
-
-        if eis_data is not None:
-            plot = selectable_axes_plot(
-                eis_data,
-                x_options=["Re(Z) [Ω]"],
-                y_options=["-Im(Z) [Ω]"],
-                color_options=["Frequency [Hz]"],
-                color_mapper=LogColorMapper("Cividis256"),
-                plot_points=True,
-                plot_line=False,
-                tools=HoverTool(tooltips=[("Frequency [Hz]", "@{Frequency [Hz]}")]),
+        if eis_data is None:
+            raise RuntimeError(
+                f"Could not parse EIS data from uploaded file with implemented parsers. Errors: {errors}"
             )
 
-            self.data["bokeh_plot_data"] = bokeh.embed.json_item(plot, theme=DATALAB_BOKEH_THEME)
+        numeric_cols = list(eis_data.select_dtypes("number").columns)
+        plot = selectable_axes_plot(
+            eis_data,
+            x_options=["Re(Z) [Ω]"] + [c for c in numeric_cols if c != "Re(Z) [Ω]"],
+            y_options=["-Im(Z) [Ω]"] + [c for c in numeric_cols if c != "-Im(Z) [Ω]"],
+            y_default="-Im(Z) [Ω]",
+            color_options=["Frequency [Hz]"],
+            color_mapper=LogColorMapper("Cividis256"),
+            plot_points=True,
+            plot_line=False,
+            tools=HoverTool(tooltips=[("Frequency [Hz]", "@{Frequency [Hz]}")]),
+        )
+
+        self.data["bokeh_plot_data"] = bokeh.embed.json_item(plot, theme=DATALAB_BOKEH_THEME)
