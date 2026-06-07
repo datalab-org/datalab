50b55fe4247362eafc93e8a2b01967e726566d18
Matthew Evans <git@ml-evs.science>
2026-06-08 00:16:59 +0100

    Final wave of model_dump/dict changes

--- pydatalab/tests/server/test_samples.py
+++ pydatalab/tests/server/test_samples.py
@@ -360,7 +360,7 @@ def test_item_regex_search(
 def test_new_sample_with_relationships(
     client, complicated_sample, insert_complicated_sample_constituents
 ):
-    complicated_sample_json = json.loads(complicated_sample.json())
+    complicated_sample_json = json.loads(complicated_sample.model_dump_json())
     response = client.post("/new-sample/", json=complicated_sample_json)
     # Test that 201: Created is emitted
     assert response.status_code == 201, response.json
@@ -424,7 +424,7 @@ def test_new_sample_with_relationships(
             description="This is a new relationship",
         )
     )
-    derived_sample_json = json.loads(derived_sample.json())
+    derived_sample_json = json.loads(derived_sample.model_dump_json())
 
     response = client.post("/new-sample/", json=derived_sample_json)
     # Test that 201: Created is emitted
@@ -528,7 +528,7 @@ def test_copy_from_sample(client, complicated_sample):
 
     """
     complicated_sample.item_id = "new_complicated_sample"
-    complicated_sample_json = json.loads(complicated_sample.json())
+    complicated_sample_json = json.loads(complicated_sample.model_dump_json())
     response = client.post("/new-sample/", json=complicated_sample_json)
 
     # Test that 201: Created is emitted
@@ -558,12 +558,13 @@ def test_copy_from_sample(client, complicated_sample):
 
 @pytest.mark.dependency(depends=["test_copy_from_sample"])
 def test_create_multiple_samples(client, complicated_sample):
-    samples = [complicated_sample, complicated_sample.copy()]
+    samples = [complicated_sample, complicated_sample.model_copy()]
     samples[0].item_id = "another_new_complicated_sample"
     samples[1].item_id = "additional_new_complicated_sample"
 
     response = client.post(
-        "/new-samples/", json={"new_sample_datas": [json.loads(s.json()) for s in samples]}
+        "/new-samples/",
+        json={"new_sample_datas": [json.loads(s.model_dump_json()) for s in samples]},
     )
     assert response.status_code == 207, response.json
     assert response.json["nsuccess"] == 2, response.json
@@ -577,7 +578,7 @@ def test_create_multiple_samples(client, complicated_sample):
     response = client.post(
         "/new-samples/",
         json={
-            "new_sample_datas": [json.loads(s.json()) for s in samples],
+            "new_sample_datas": [json.loads(s.model_dump_json()) for s in samples],
             "copy_from_item_ids": [
                 "another_new_complicated_sample",
                 "additional_new_complicated_sample",
@@ -599,7 +600,7 @@ def test_create_multiple_samples(client, complicated_sample):
 
 @pytest.mark.dependency(depends=["test_create_multiple_samples"])
 def test_create_cell(client, default_cell):
-    response = client.post("/new-sample/", json=json.loads(default_cell.json()))
+    response = client.post("/new-sample/", json=json.loads(default_cell.model_dump_json()))
     assert response.status_code == 201, response.json
     assert response.json["status"] == "success"
 
@@ -672,7 +673,9 @@ def test_create_collections(client, default_collection, database):
     assert response.status_code == 200
 
     # Create an empty collection
-    response = client.put("/collections", json={"data": json.loads(default_collection.json())})
+    response = client.put(
+        "/collections", json={"data": json.loads(default_collection.model_dump_json())}
+    )
     assert response.status_code == 201, response.json
     assert response.json["status"] == "success"
     assert response.json["data"]["collection_id"] == "test_collection"
@@ -691,7 +694,7 @@ def test_create_collections(client, default_collection, database):
     new_collection = copy.deepcopy(default_collection)
     new_collection.collection_id = "test_collection_2"
 
-    data = json.loads(new_collection.json())
+    data = json.loads(new_collection.model_dump_json())
     data.update(
         {
             "starting_members": [
