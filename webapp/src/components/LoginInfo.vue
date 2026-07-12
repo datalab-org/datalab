9f975be02aebf5e48b376bfc6c21026f841e80be
David Waroquiers <david.waroquiers@matgenix.com>
2026-07-12 15:10:50 +0200

    Adding missing files for login info.

---
+++ webapp/src/components/LoginInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 style="font-size: 4rem">Welcome to Datalab</h1>
+  <p>datalab is a place to store experimental data and the connections between them.</p>
+  <p>
+    datalab is open source (MIT license) and development occurs on GitHub at
+    <a href="https://github.com/datalab-org/datalab"
+      ><font-awesome-icon :icon="['fab', 'github']" />&nbsp;datalab-org/datalab</a
+    >
+    with documentation available on
+    <a href="https://the-datalab.readthedocs.io"
+      ><font-awesome-icon icon="book" />&nbsp;ReadTheDocs</a
+    >.
+  </p>
+  <router-link to="/about" class="btn btn-default">Learn More</router-link>
+</template>
+
+<script>
+export default {
+  name: "LoginInfo",
+};
+</script>
