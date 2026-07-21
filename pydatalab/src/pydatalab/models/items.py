fc2e5c3af8425561bd886fb84658b1fc2770c141
Guido Petretto <g.petretto@gmail.com>
2026-07-21 15:34:20 +0200

    separate blocks from items

--- pydatalab/src/pydatalab/models/items.py
+++ pydatalab/src/pydatalab/models/items.py
@@ -2,6 +2,7 @@ import abc
 
 from pydantic import field_validator
 
+from pydatalab.models.blocks import HasBlocks
 from pydatalab.models.entries import Entry
 from pydatalab.models.files import HasFiles
 from pydatalab.models.traits import (
