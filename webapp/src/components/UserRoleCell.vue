0251f9c2b578ea1fb140de143ee9726da65d29e7
David Waroquiers <david.waroquiers@matgenix.com>
2026-08-05 20:43:39 +0200

    Fixed editing of roles (UI) (#1924)
    Co-authored-by: Matthew Evans <git@ml-evs.science>

--- webapp/src/components/UserRoleCell.vue
+++ webapp/src/components/UserRoleCell.vue
@@ -80,18 +80,11 @@ export default {
       }
     },
     async handleRoleChange(newRole) {
-      const originalRole = this.localRole;
+      const originalRole = this.user?.role || "user";
 
-      if (originalRole === "admin" && newRole !== "admin") {
-        const confirmed = await DialogService.confirm({
-          title: "Change Admin Role",
-          message: `Are you sure you want to remove admin privileges from ${this.user.display_name}?`,
-          type: "warning",
-        });
-        if (!confirmed) {
-          this.localRole = originalRole;
-          return;
-        }
+      if (newRole === originalRole) {
+        this.localRole = originalRole;
+        return;
       }
 
       const confirmed = await DialogService.confirm({
