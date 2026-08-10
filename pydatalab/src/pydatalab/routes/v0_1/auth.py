f233520447e29a3431a3e3331b8710e0b328f0da
OMWalmsley <oliver.m.walmsley@gmail.com>
2026-08-10 13:26:04 +0100

    Fix: reformatted file

--- pydatalab/src/pydatalab/routes/v0_1/auth.py
+++ pydatalab/src/pydatalab/routes/v0_1/auth.py
@@ -1129,15 +1129,15 @@ def delete_api_key(api_id):
     if not doc:
         # Deal with potential legacy key
         if ObjectId(api_id) == ObjectId(current_user.id):
-	        doc = flask_mongo.db.api_keys.find_one(
-	            {
-	                "_id": ObjectId(api_id),
-	                "user": {"$exists": False},
-	                "digest": {"$exists": False},
-	                "name": {"$exists": False},
-	                "type": {"$exists": False},
-	            },
-			)
+            doc = flask_mongo.db.api_keys.find_one(
+                {
+                    "_id": ObjectId(api_id),
+                    "user": {"$exists": False},
+                    "digest": {"$exists": False},
+                    "name": {"$exists": False},
+                    "type": {"$exists": False},
+                },
+            )
     if not doc:
         raise NotFound(description="API key not found.")
     result = flask_mongo.db.api_keys.delete_one({"_id": ObjectId(api_id)})
