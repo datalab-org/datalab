c029337eaa2308ef041e4d6d948f7b9de41781e8
OMWalmsley <oliver.m.walmsley@gmail.com>
2026-08-12 10:01:24 +0100

    Chore: simplified nested location logic

--- pydatalab/tests/server/test_items.py
+++ pydatalab/tests/server/test_items.py
@@ -441,3 +441,102 @@ def test_location_endpoint_with_single_non_nested_location(client, item_creator,
     assert set(response.json["flat_locations"]).issuperset({"Lab 1"})
     assert "nested_locations" in response.json
     assert "Lab 1" in response.json["nested_locations"]
+
+
+def test_regression_test_against_overwriting_problem(client, item_creator, user_id):
+    # test ensures that items don't get over (i.e. Position A should not get overwritten)
+    item_creator(
+        Cell(
+            **{
+                "item_id": "test_cell_4",
+                "name": "test cell",
+                "date": "1970-02-01",
+                "negative_electrode": [
+                    {
+                        "item": {"item_id": "test", "type": "starting_materials"},
+                        "quantity": 5.0,
+                        "unit": "mg",
+                    },
+                    {
+                        "item": {"item_id": "test_carbon", "chemform": "C", "type": "samples"},
+                        "quantity": 1.0,
+                        "unit": "mg",
+                    },
+                ],
+                "positive_electrode": [
+                    {
+                        "item": {
+                            "item_id": "test_cathode",
+                            "chemform": "LiCoO2",
+                            "type": "samples",
+                        },
+                        "quantity": 500,
+                        "unit": "kg",
+                    }
+                ],
+                "electrolyte": [
+                    {"item": {"name": "inlined reference"}, "quantity": 10, "unit": "ml"}
+                ],
+                "cell_format": "swagelok",
+                "type": "cells",
+                "creator_ids": [user_id],
+                "Location": "Cambridge > Lab 6 > Shelf 2 > Position A",
+            }
+        )
+    )
+    item_creator(
+        Cell(
+            **{
+                "item_id": "test_cell0",
+                "name": "test cell",
+                "date": "1970-02-01",
+                "negative_electrode": [
+                    {
+                        "item": {"item_id": "test", "type": "starting_materials"},
+                        "quantity": 2.0,
+                        "unit": "mg",
+                    },
+                    {
+                        "item": {"item_id": "test_carbon", "chemform": "C", "type": "samples"},
+                        "quantity": 2.0,
+                        "unit": "mg",
+                    },
+                ],
+                "positive_electrode": [
+                    {
+                        "item": {
+                            "item_id": "test_cathode",
+                            "chemform": "LiCoO2",
+                            "type": "samples",
+                        },
+                        "quantity": 2000,
+                        "unit": "kg",
+                    }
+                ],
+                "electrolyte": [
+                    {"item": {"name": "inlined reference"}, "quantity": 100, "unit": "ml"}
+                ],
+                "cell_format": "swagelok",
+                "type": "cells",
+                "creator_ids": [user_id],
+                "Location": "Cambridge > Lab 6 > Shelf 1",
+            }
+        )
+    )
+
+    response = client.get("/locations")
+    assert response.status_code == 200
+    assert "flat_locations" in response.json
+
+    assert set(response.json["flat_locations"]).issuperset(
+        {
+            "Cambridge > Lab 6 > Shelf 1",
+            "Cambridge > Lab 6 > Shelf 2 > Position A",
+        }
+    )
+    assert "nested_locations" in response.json
+    assert "Cambridge" in response.json["nested_locations"]
+    assert "Lab 6" in response.json["nested_locations"]["Cambridge"]
+    assert "Shelf 1" in response.json["nested_locations"]["Cambridge"]["Lab 6"]
+    assert "Shelf 2" in response.json["nested_locations"]["Cambridge"]["Lab 6"]
+    assert "Position A" in response.json["nested_locations"]["Cambridge"]["Lab 6"]["Shelf 2"]
