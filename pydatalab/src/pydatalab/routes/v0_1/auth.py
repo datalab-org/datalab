53c6afbf4b3eb6dbf82ce9f64427415599e23402
Matthew Evans <git@ml-evs.science>
2026-09-25 14:18:20 +0100

    Prevent "registration" magic links from attaching new email to existing user (#2143)

--- pydatalab/src/pydatalab/routes/v0_1/auth.py
+++ pydatalab/src/pydatalab/routes/v0_1/auth.py
@@ -15,7 +15,7 @@ import jwt
 from bson import ObjectId
 from flask import Blueprint, Response, g, jsonify, redirect, request, session
 from flask_dance.consumer import OAuth2ConsumerBlueprint, oauth_authorized, oauth_before_login
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from flask_login.utils import LocalProxy
 from werkzeug.exceptions import BadRequest, Forbidden, NotFound
 
@@ -873,6 +873,11 @@ def email_logged_in():
     email = data["email"]
     if not email:
         raise BadRequest("No email found; please request a new token.")
+
+    # A magic link always logs in as the owner of the email; log out any current user
+    # so that the email is never attached to their account as a new identity
+    if current_user.is_authenticated:
+        logout_user()
 
     if data.get("remember"):
         session[REMEMBER_ME_SESSION_KEY] = True
