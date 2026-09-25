0cd59dd5859a31829083be2735ab413dc9eed5ac
Matthew Evans <git@ml-evs.science>
2026-09-25 22:19:39 +0100

    Remove leftover rebase canaries

--- webapp/src/components/InventoryAccess.vue
+++ webapp/src/components/InventoryAccess.vue
@@ -1,4 +1,3 @@
-<!-- This file was edited with the assistance of an AI model and requires human review from the contributor. -->
 <template>
   <div class="d-flex align-items-center">
     <Creators :creators="creators" :groups="groups" :show-names="false" show-bubble />
--- webapp/src/utils/tableColumns.js
+++ webapp/src/utils/tableColumns.js
@@ -1,4 +1,3 @@
-// This file was edited with the assistance of an AI model and requires human review from the contributor.
 import { FilterOperator, FilterMatchMode } from "@primevue/core/api";
 
 import { formatRelativeDate } from "@/field_utils.js";
